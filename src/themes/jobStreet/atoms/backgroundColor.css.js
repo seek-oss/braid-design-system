87ff238d593351be63518e6d52d84074d28e065a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-09-14 17:05:29 +1000

    Update jobStreet highlight background color

--- src/themes/jobStreet/atoms/backgroundColor.css.js
+++ src/themes/jobStreet/atoms/backgroundColor.css.js
@@ -3,7 +3,7 @@ export default {
   '.inputDisabled': { background: '#eee' },
   '.formAccent': { background: '#1c3f94' },
   '.formAccentDisabled': { background: '#ccc' },
-  '.highlight': { background: '#F8F8FA' },
+  '.highlight': { background: '#E8EBF3' },
   '.info': { background: '#1C3F94' },
   '.card': { background: '#fff' }
 };
