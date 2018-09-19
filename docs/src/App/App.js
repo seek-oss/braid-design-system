388e444edc120ec6223c750ce9658a85f0216192
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-09-19 13:00:08 +1000

    Fix seekAnz info colour, add theme switching to demo

--- docs/src/App/App.js
+++ docs/src/App/App.js
@@ -1,5 +1,5 @@
-import jobStreetTheme from '../../../src/themes/jobStreet';
-import React from 'react';
+import * as themes from '../../../src/themes';
+import React, { Component } from 'react';
 import {
   ThemeProvider,
   Text,
@@ -8,34 +8,56 @@ import {
   Checkbox
 } from '../../../src/components';
 
-export default () => (
-  <ThemeProvider theme={jobStreetTheme}>
-    <div style={{ maxWidth: 500, margin: '0 auto' }}>
-      <Text marginBottom="largest">Coming soon.</Text>
-      <Alert tone="info" marginBottom="larger">
-        This is a piece of information that we'd like to bring to your
-        attention. If you miss it, it's not the end of the world.
-      </Alert>
-      <Checkbox
-        label="I agree to the terms and conditions"
-        tone="critical"
-        message="You must agree to continue"
-      />
-      <ChecklistCard borderWidth="standard" marginTop="large">
-        <Checkbox label="Hello world" message={false} checked>
-          <Text>Hello!</Text>
-        </Checkbox>
-        <Checkbox
-          label="Hello world"
-          message="Oops, something went wrong!"
-          tone="critical"
-        >
-          <Text>Hello!</Text>
-        </Checkbox>
-        <Checkbox label="Hello world" message={false}>
-          <Text>Hello!</Text>
-        </Checkbox>
-      </ChecklistCard>
-    </div>
-  </ThemeProvider>
-);
+const themeNames = Object.keys(themes);
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      themeIndex: 0
+    };
+  }
+
+  toggleTheme = () => {
+    this.setState(state => ({
+      themeIndex: (state.themeIndex + 1) % themeNames.length
+    }));
+  };
+
+  render() {
+    const { themeIndex } = this.state;
+
+    return (
+      <ThemeProvider theme={themes[themeNames[themeIndex]]}>
+        <div style={{ maxWidth: 600, margin: '0 auto' }}>
+          <button onClick={this.toggleTheme}>Toggle theme</button>
+          <Alert tone="info" marginBottom="larger" marginTop="large">
+            This is a piece of information that we'd like to bring to your
+            attention. If you miss it, it's not the end of the world.
+          </Alert>
+          <Checkbox
+            label="I agree to the terms and conditions"
+            tone="critical"
+            message="You must agree to continue"
+          />
+          <ChecklistCard borderWidth="standard" marginTop="large">
+            <Checkbox label="Hello world" message={false} checked>
+              <Text>Hello!</Text>
+            </Checkbox>
+            <Checkbox
+              label="Hello world"
+              message="Oops, something went wrong!"
+              tone="critical"
+            >
+              <Text>Hello!</Text>
+            </Checkbox>
+            <Checkbox label="Hello world" message={false}>
+              <Text>Hello!</Text>
+            </Checkbox>
+          </ChecklistCard>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
--- src/themes/seekAnz/atoms/backgroundColor.css.js
+++ src/themes/seekAnz/atoms/backgroundColor.css.js
@@ -4,6 +4,6 @@ export default {
   '.formAccent': { background: '#2765cf' },
   '.formAccentDisabled': { background: '#ccc' },
   '.selection': { background: '#f5f8ff' },
-  '.info': { background: '#2765cf' },
+  '.info': { background: '#9556b7' },
   '.card': { background: '#fff' }
 };
