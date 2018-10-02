c6dea899fcfd7ef7255250b085809083e4f6a086
Jahred Hope <jahredhope@gmail.com>
2018-10-03 08:18:06 +1000

    build: Seperate out and document release configuration

---
+++ release.config.js
@@ -0,0 +1,6 @@
+// semantic-release configuration
+module.exports = {
+  // Avoid all default success plugins to avoid commenting on PRs
+  // https://github.com/semantic-release/semantic-release/blob/caribou/docs/usage/configuration.md#success
+  success: false
+};
