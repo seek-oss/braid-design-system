60eb27bc38c33d2de1fa6934110e27f687547de7
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-10-17 16:55:53 +1100

    ci: Post commit status

---
+++ scripts/postCommitStatus.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+(async () => {
+  try {
+    console.log('Posting commit status to GitHub...');
+
+    const { GH_TOKEN, TRAVIS_PULL_REQUEST_SHA } = process.env;
+
+    if (!GH_TOKEN && !TRAVIS_PULL_REQUEST_SHA) {
+      throw new Error(
+        'GH_TOKEN and TRAVIS_PULL_REQUEST_SHA environment variables must be present'
+      );
+    }
+
+    const octokit = require('@octokit/rest')();
+
+    octokit.authenticate({
+      type: 'token',
+      token: GH_TOKEN
+    });
+
+    await octokit.repos.createStatus({
+      owner: 'seek-oss',
+      repo: 'braid-design-system',
+      sha: TRAVIS_PULL_REQUEST_SHA,
+      state: 'success',
+      context: 'Preview deployment',
+      target_url: `https://braid-design-system--${TRAVIS_PULL_REQUEST_SHA}.surge.sh`
+    });
+
+    console.log('Successfully posted commit status to GitHub');
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // eslint-disable-line no-process-exit
+  }
+})();
