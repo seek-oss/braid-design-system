e2e45fb31ebb760a301dfcca5baa190b089c86d1
michaeltaranto <michael.taranto@gmail.com>
2018-11-09 15:34:43 +1100

    fix(theme): Migrate to TypeScript

---
+++ lib/themes/seekAsia/index.ts
@@ -0,0 +1,11 @@
+import tokens from './tokens/tokens';
+import { Theme } from '../theme';
+import * as atoms from './atoms/atoms';
+
+const theme: Theme = {
+  name: 'seekAsia',
+  tokens,
+  atoms
+};
+
+export default theme;
