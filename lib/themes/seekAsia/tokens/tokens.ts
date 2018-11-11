c70faf95344a38a19ee15e65011ed5705c315477
Michael Taranto <michaeltaranto@users.noreply.github.com>
2018-11-12 10:56:03 +1100

    fix(theme): Migrate to TypeScript (#27)

---
+++ lib/themes/seekAsia/tokens/tokens.ts
@@ -0,0 +1,55 @@
+import { Tokens } from '../../theme';
+
+const tokens: Tokens = {
+  rowHeight: 4,
+  columnWidth: 4,
+  interactionRows: 11,
+  responsiveBreakpoint: 768,
+  descenderHeightScale: 0.13,
+  text: {
+    standard: {
+      mobile: {
+        size: 16,
+        rows: 6
+      },
+      desktop: {
+        size: 16,
+        rows: 6
+      }
+    },
+    large: {
+      mobile: {
+        size: 18,
+        rows: 6
+      },
+      desktop: {
+        size: 20,
+        rows: 7
+      }
+    }
+  },
+  rowSpacing: {
+    smallest: 1,
+    smaller: 2,
+    small: 3,
+    medium: 4,
+    large: 5,
+    larger: 8,
+    largest: 12
+  },
+  columnSpacing: {
+    gutter: 5,
+    smallest: 1,
+    smaller: 2,
+    small: 3,
+    medium: 4,
+    large: 5,
+    larger: 8,
+    largest: 12
+  },
+  borderWidth: {
+    standard: 1
+  }
+};
+
+export default tokens;
