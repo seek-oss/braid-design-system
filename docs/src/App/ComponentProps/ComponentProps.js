6b47fb20a4c1f9dfe0e9e4d22874476b3f71d508
Michael Taranto <michaeltaranto@users.noreply.github.com>
2018-11-19 10:23:18 +1100

    docs(site): Document props with react-docgen (#41)

--- docs/src/App/ComponentProps/ComponentProps.js
+++ docs/src/App/ComponentProps/ComponentProps.js
@@ -1,47 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import parsePropTypes from 'parse-prop-types';
 import { Box, Text } from '../../../../lib/components';
-
-const typeValueToString = option => {
-  if (option.type.name === 'oneOfType') {
-    return option.type.value
-      .map(x => {
-        if (x.name === 'oneOf') {
-          return x.value
-            .map(y => {
-              return JSON.stringify(y);
-            })
-            .join(' | ');
-        }
-
-        return x.name;
-      })
-      .join(' | ');
-  }
-
-  if (option.type.name === 'oneOf') {
-    return option.type.value
-      .map(x => {
-        return JSON.stringify(x);
-      })
-      .join(' | ');
-  }
-
-  return option.type.name;
-};
+import componentDocs from '../../../../lib/componentDocs/componentDocs.json';
 
 export default class ComponentProps extends Component {
   static propTypes = {
-    component: PropTypes.node.isRequired
+    componentName: PropTypes.string.isRequired
   };
 
   render() {
-    const { component } = this.props;
-    const propTypes = parsePropTypes(component);
-    const options = Object.keys(propTypes).map(propName => ({
+    const { componentName } = this.props;
+    const { props = {} } = componentDocs[componentName] || {};
+
+    const options = Object.keys(props).map(propName => ({
       name: propName,
-      ...propTypes[propName]
+      ...props[propName]
     }));
 
     return options.length === 0 ? null : (
@@ -53,7 +26,7 @@ export default class ComponentProps extends Component {
           <Box key={i} paddingBottom="small">
             <Text>{option.name}</Text>
             <Text color="secondary">
-              Type: {typeValueToString(option)}
+              Type: {option.type.name}
               {option.required && !option.defaultValue ? ' (Required)' : ''}
             </Text>
             {option.defaultValue ? (
