ef5f5967c1e629e8b8892490cb2905765c7282ab
michaeltaranto <michael.taranto@gmail.com>
2018-11-26 16:21:06 +1100

    fix(Divider): Migrate to TypeScript

---
+++ lib/components/Divider/Divider.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import styles from './Divider.css.js';
+import withTheme, { WithThemeProps } from '../private/withTheme';
+import Box from '../Box/Box';
+import { BorderColorVariants, BorderWidthVariants } from '../../themes/theme';
+
+interface Props extends WithThemeProps {
+  borderColor: BorderColorVariants;
+  borderWidth: BorderWidthVariants;
+  className?: string;
+}
+
+export default withTheme(
+  class Divider extends Component<Props> {
+    static displayName = 'Divider';
+
+    render() {
+      const {
+        theme,
+        borderColor,
+        borderWidth,
+        className,
+        ...restProps
+      } = this.props;
+
+      return (
+        <Box className={classnames(styles.root, className)} {...restProps}>
+          <div
+            className={classnames(
+              styles.divider,
+              theme.atoms.borderWidth[borderWidth || 'standard'],
+              theme.atoms.borderColor[borderColor || 'standard']
+            )}
+          />
+        </Box>
+      );
+    }
+  }
+);
