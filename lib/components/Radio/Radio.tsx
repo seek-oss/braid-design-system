ac5651b6fd929e7706207f9fa55573e11f02ee11
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-02-08 16:33:32 +1100

    fix(ChecklistCard): Show error border on entire row (#90)

--- lib/components/Radio/Radio.tsx
+++ lib/components/Radio/Radio.tsx
@@ -90,12 +90,16 @@ export default class Radio extends Component<RadioProps, State> {
           const radioSize = getCheckboxRadioSize(theme);
 
           return (
-            <div
-              className={classnames(className, {
-                [theme.atoms.paddingBottom.xsmall]: inChecklistCard,
-                [theme.atoms.backgroundColor.selection]:
-                  inChecklistCard && (checked || hovered) && !disabled
-              })}
+            <Box
+              {...(inChecklistCard && tone === 'critical'
+                ? { borderWidth: 'standard', borderColor: 'critical' }
+                : {})}
+              backgroundColor={
+                inChecklistCard && (checked || hovered) && !disabled
+                  ? 'selection'
+                  : undefined
+              }
+              paddingBottom={inChecklistCard ? 'xsmall' : undefined}
               style={style}
             >
               <input
@@ -175,16 +179,18 @@ export default class Radio extends Component<RadioProps, State> {
                         theme.atoms.transition.fast
                       )}
                     />
-                    <Box
-                      borderColor="critical"
-                      borderWidth="standard"
-                      style={{ opacity: tone === 'critical' ? 1 : 0 }}
-                      className={classnames(
-                        styles.radio,
-                        styles.radioCritical,
-                        theme.atoms.transition.fast
-                      )}
-                    />
+                    {!inChecklistCard ? (
+                      <Box
+                        borderColor="critical"
+                        borderWidth="standard"
+                        style={{ opacity: tone === 'critical' ? 1 : 0 }}
+                        className={classnames(
+                          styles.radio,
+                          styles.radioCritical,
+                          theme.atoms.transition.fast
+                        )}
+                      />
+                    ) : null}
                     <Box
                       backgroundColor={
                         disabled ? 'formAccentDisabled' : 'formAccent'
@@ -231,7 +237,7 @@ export default class Radio extends Component<RadioProps, State> {
                   />
                 </Box>
               ) : null}
-            </div>
+            </Box>
           );
         }}
       </ThemeConsumer>
