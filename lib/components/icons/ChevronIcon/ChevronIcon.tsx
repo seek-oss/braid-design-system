5426058e923e05403cabfd8683b4f54899d40808
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-02-15 15:13:56 +1100

    fix(ChevronIcon): Support inline prop

--- lib/components/icons/ChevronIcon/ChevronIcon.tsx
+++ lib/components/icons/ChevronIcon/ChevronIcon.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import { Omit } from 'utility-types';
 import Box from '../../Box/Box';
 import Icon, { IconProps } from '../Icon/Icon';
 import ChevronSvg from './ChevronSvg';
 import styles from './ChevronIcon.css.js';
 
-export interface ChevronIconProps extends Omit<IconProps, 'svgComponent'> {
+export interface ChevronIconProps
+  extends Pick<IconProps, 'svgComponent' | 'inline'> {
   direction?: 'up' | 'down' | 'left' | 'right';
 }
 
@@ -14,17 +14,18 @@ export default class ChevronIcon extends Component<ChevronIconProps> {
   static displayName = 'ChevronIcon';
 
   render() {
-    const { direction = 'down' } = this.props;
+    const { direction = 'down', inline } = this.props;
 
     return (
       <Box
         className={classnames(styles.root, {
+          [styles.inline]: inline,
           [styles.up]: direction === 'up',
           [styles.left]: direction === 'left',
           [styles.right]: direction === 'right'
         })}
       >
-        <Icon svgComponent={ChevronSvg} />
+        <Icon svgComponent={ChevronSvg} inline={inline} />
       </Box>
     );
   }
