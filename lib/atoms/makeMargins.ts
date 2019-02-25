40046b2fe148617697f06a7eca3045173ada4379
Michael Taranto <mtaranto@seek.com.au>
2019-02-25 15:18:17 +1100

    Revert "refactor: Fix spacing properties"
    This reverts commit da654634e8436b41c86ee36f4fdb3aed0d6077c4.

--- lib/atoms/makeMargins.ts
+++ lib/atoms/makeMargins.ts
@@ -3,17 +3,17 @@ import rowSpacingForCssRule from './utils/rowSpacingForCssRule';
 import columnSpacingForCssRule from './utils/columnSpacingForCssRule';
 import makeDesktopRules from './utils/makeDesktopRules';
 
-const makeMarginRules = (tokens: Tokens) => ({
-  ...rowSpacingForCssRule('marginTop', tokens),
-  ...columnSpacingForCssRule('marginRight', tokens),
-  ...rowSpacingForCssRule('marginBottom', tokens),
-  ...columnSpacingForCssRule('marginLeft', tokens)
+const makeMarginRules = (tokens: Tokens, suffix = '') => ({
+  ...rowSpacingForCssRule(`marginTop${suffix}`, tokens),
+  ...columnSpacingForCssRule(`marginRight${suffix}`, tokens),
+  ...rowSpacingForCssRule(`marginBottom${suffix}`, tokens),
+  ...columnSpacingForCssRule(`marginLeft${suffix}`, tokens)
 });
 
 export default (tokens: Tokens) => ({
   ...makeMarginRules(tokens),
   ...makeDesktopRules({
     tokens,
-    css: makeMarginRules(tokens)
+    css: makeMarginRules(tokens, 'Desktop')
   })
 });
