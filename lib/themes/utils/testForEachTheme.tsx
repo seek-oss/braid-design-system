668b37b92e84573c10eb4da5d425ef7e216e8a46
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-02-26 06:57:50 +1100

    test: Remove component snapshot tests in favour of Chromatic tests

--- lib/themes/utils/testForEachTheme.tsx
+++ lib/themes/utils/testForEachTheme.tsx
@@ -1,20 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import each from 'lodash/each';
 import { Omit } from 'utility-types';
-import * as themes from '../../themes';
-import { Theme } from '../../themes/theme';
+import * as themes from '..';
+import { Theme } from '../theme';
 import ThemeProvider, {
   ThemeProviderProps
 } from '../../components/ThemeProvider/ThemeProvider';
 
-type ForEachThemeCallback = (
+type TestCallback = (
   args: {
     theme: Theme;
     ThemeProvider: FunctionComponent<Omit<ThemeProviderProps, 'theme'>>;
   }
 ) => void;
 
-export default (callback: ForEachThemeCallback) => {
+export default (callback: TestCallback) => {
   each(themes, theme => {
     describe(`Theme: ${theme.name}`, () => {
       callback({
