da654634e8436b41c86ee36f4fdb3aed0d6077c4
Michael Taranto <mtaranto@seek.com.au>
2019-02-25 15:07:29 +1100

    refactor: Fix spacing properties

--- lib/atoms/makeMargins.ts
+++ lib/atoms/makeMargins.ts
@@ -3,17 +3,17 @@ import rowSpacingForCssRule from './utils/rowSpacingForCssRule';
 import columnSpacingForCssRule from './utils/columnSpacingForCssRule';
 import makeDesktopRules from './utils/makeDesktopRules';
 
-const makeMarginRules = (tokens: Tokens, suffix = '') => ({
-  ...rowSpacingForCssRule(`marginTop${suffix}`, tokens),
-  ...columnSpacingForCssRule(`marginRight${suffix}`, tokens),
-  ...rowSpacingForCssRule(`marginBottom${suffix}`, tokens),
-  ...columnSpacingForCssRule(`marginLeft${suffix}`, tokens)
+const makeMarginRules = (tokens: Tokens) => ({
+  ...rowSpacingForCssRule('marginTop', tokens),
+  ...columnSpacingForCssRule('marginRight', tokens),
+  ...rowSpacingForCssRule('marginBottom', tokens),
+  ...columnSpacingForCssRule('marginLeft', tokens)
 });
 
 export default (tokens: Tokens) => ({
   ...makeMarginRules(tokens),
   ...makeDesktopRules({
     tokens,
-    css: makeMarginRules(tokens, 'Desktop')
+    css: makeMarginRules(tokens)
   })
 });
--- lib/atoms/makePadding.ts
+++ lib/atoms/makePadding.ts
@@ -3,17 +3,17 @@ import rowSpacingForCssRule from './utils/rowSpacingForCssRule';
 import columnSpacingForCssRule from './utils/columnSpacingForCssRule';
 import makeDesktopRules from './utils/makeDesktopRules';
 
-const makePaddingRules = (tokens: Tokens, suffix = '') => ({
-  ...rowSpacingForCssRule(`paddingTop${suffix}`, tokens),
-  ...columnSpacingForCssRule(`paddingRight${suffix}`, tokens),
-  ...rowSpacingForCssRule(`paddingBottom${suffix}`, tokens),
-  ...columnSpacingForCssRule(`paddingLeft${suffix}`, tokens)
+const makePaddingRules = (tokens: Tokens) => ({
+  ...rowSpacingForCssRule('paddingTop', tokens),
+  ...columnSpacingForCssRule('paddingRight', tokens),
+  ...rowSpacingForCssRule('paddingBottom', tokens),
+  ...columnSpacingForCssRule('paddingLeft', tokens)
 });
 
 export default (tokens: Tokens) => ({
   ...makePaddingRules(tokens),
   ...makeDesktopRules({
     tokens,
-    css: makePaddingRules(tokens, 'Desktop')
+    css: makePaddingRules(tokens)
   })
 });
