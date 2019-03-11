2941eb74307c05a1f3a1701e8a3b1e4d1039085c
Michael Taranto <mtaranto@seek.com.au>
2019-03-11 21:27:11 +1100

    ci(release): Formalise patch releases pre v1

--- release.config.js
+++ release.config.js
@@ -3,4 +3,30 @@ module.exports = {
   // Avoid all default success plugins to avoid commenting on PRs
   // https://github.com/semantic-release/semantic-release/blob/caribou/docs/usage/configuration.md#success
   success: false,
+
+  // Formalise our current alpha release strategy — ensuring every release
+  // is a patch at this stage. Allows developers to semantically convey
+  // the scope and type of change pre v1.0.0
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {
+            breaking: true,
+            release: 'patch',
+          },
+          {
+            type: 'feat',
+            release: 'patch',
+          },
+          {
+            type: 'fix',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+  ],
 };
