5b26c3127695297abbfbc9b2e40c5ac0838777a1
Michael Taranto <michaeltaranto@users.noreply.github.com>
2019-03-20 22:15:53 +1100

    docs: Fix playroom link (#145)

--- docs/src/App/Home/Home.css.js
+++ docs/src/App/Home/Home.css.js
@@ -27,4 +27,11 @@ export default {
       cursor: 'pointer',
     },
   },
+  '.subtitle': {
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    '> *': {
+      whiteSpace: 'nowrap',
+    },
+  },
 };
