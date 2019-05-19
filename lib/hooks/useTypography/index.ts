f75b89d62199b983ac0624df87e546fed8370708
Michael Taranto <mtaranto@seek.com.au>
2019-05-20 09:15:14 +1000

    refactor: Rename Type to TypographyParams

--- lib/hooks/useTypography/index.ts
+++ lib/hooks/useTypography/index.ts
@@ -3,16 +3,16 @@ import { fontFamily, fontWeight, fontSize, transform } from './index.treat';
 
 export type FontWeight = 'regular' | 'medium' | 'strong';
 
-interface Type {
+interface TypographyParams {
   weight?: FontWeight;
   size: keyof typeof fontSize;
   baseline: boolean;
 }
 
-export default ({ weight, size, baseline }: Type) =>
+export default ({ weight, size, baseline }: TypographyParams) =>
   useClassNames(fontFamily, useWeight(weight), fontSize[size], {
     [transform[size]]: baseline,
   });
 
-export const useWeight = (weight: Type['weight'] = 'regular') =>
+export const useWeight = (weight: TypographyParams['weight'] = 'regular') =>
   useClassNames(fontWeight[weight]);
