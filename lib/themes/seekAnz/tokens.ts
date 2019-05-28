38eb7e876df0a45f02cd56f3bb679fceeb5913f2
Jye Nicolson <jyenicolson@gmail.com>
2019-05-28 13:31:16 +1000

    feat(Text): Add small text size (#204)

--- lib/themes/seekAnz/tokens.ts
+++ lib/themes/seekAnz/tokens.ts
@@ -45,6 +45,16 @@ const tokens: Tokens = {
     },
   },
   text: {
+    small: {
+      mobile: {
+        size: 14,
+        rows: 3,
+      },
+      desktop: {
+        size: 14,
+        rows: 3,
+      },
+    },
     standard: {
       mobile: {
         size: 16,
