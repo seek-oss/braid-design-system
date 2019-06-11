218d13c81913d7cdfed199b87bb2fa09f9915e81
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-06-11 16:39:47 +1000

    feat(Text): Default Text to ‘span’, support id prop, add Paragraph (#226)

--- lib/components/FieldLabel/FieldLabel.tsx
+++ lib/components/FieldLabel/FieldLabel.tsx
@@ -31,16 +31,14 @@ export const FieldLabel = ({
     <Box paddingBottom="xsmall">
       <Box component="span" display="flex" className={styles.spaceBetween}>
         <label htmlFor={id}>
-          <Text component="span">
+          <Text>
             <Strong>{label}</Strong>
             {secondaryLabel ? (
               <Secondary>&nbsp;({secondaryLabel})</Secondary>
             ) : null}
           </Text>
         </label>
-        {tertiaryLabel ? (
-          <Text component="span">&nbsp;{tertiaryLabel}</Text>
-        ) : null}
+        {tertiaryLabel ? <Text>&nbsp;{tertiaryLabel}</Text> : null}
       </Box>
       {description ? (
         <Box paddingTop="xxsmall" paddingBottom="xxsmall">
