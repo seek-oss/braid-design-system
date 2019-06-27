1094b226bbb20209e278b2dcbbb46e744e7263bc
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-06-27 21:13:47 +1000

    chore: Fix Box usage in docs site

--- site/src/App/Components/ComponentRoute.tsx
+++ site/src/App/Components/ComponentRoute.tsx
@@ -82,7 +82,7 @@ export const ComponentRoute = ({
             <Text tone="secondary">Code:</Text>
           </Box>
           <Box
-            backgroundColor="formAccent"
+            background="formAccent"
             paddingLeft="small"
             paddingRight="small"
             paddingTop="xxsmall"
