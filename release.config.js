1fbf207dfaddf0fb182bfe38b27e4daa07dc8ce2
Michael Taranto <mtaranto@seek.com.au>
2019-06-28 13:41:13 +1000

    fix: Reinstate disabling of success notifications

---
+++ release.config.js
@@ -0,0 +1,7 @@
+// semantic-release configuration
+
+module.exports = {
+  // Avoid all default success plugins to avoid commenting on PRs
+  // https://github.com/semantic-release/semantic-release/blob/caribou/docs/usage/configuration.md#success
+  success: false,
+};
