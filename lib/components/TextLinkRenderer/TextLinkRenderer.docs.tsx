0f892e4be6343f9e33da0304b1154d9a9bf2e1a3
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-07-12 15:22:57 +1000

    test: Fix TextLinkRender example

--- lib/components/TextLinkRenderer/TextLinkRenderer.docs.tsx
+++ lib/components/TextLinkRenderer/TextLinkRenderer.docs.tsx
@@ -2,33 +2,40 @@ import React from 'react';
 import { ComponentDocs } from '../../../site/src/types';
 import { Link } from 'react-router-dom';
 import { TextLinkRenderer } from './TextLinkRenderer';
+import { Text } from '../Text/Text';
 
 const docs: ComponentDocs = {
   examples: [
     {
-      label: 'Standard TextLink with Custom Renderer',
+      label: 'TextLink with Custom Renderer',
       render: () => (
-        <TextLinkRenderer>
-          {textLinkProps => (
-            <Link to="" {...textLinkProps}>
-              Text Link
-            </Link>
-          )}
-        </TextLinkRenderer>
+        <Text>
+          The last word of this sentence is a{' '}
+          <TextLinkRenderer>
+            {textLinkProps => (
+              <Link to="" {...textLinkProps}>
+                link.
+              </Link>
+            )}
+          </TextLinkRenderer>
+        </Text>
       ),
       code: `
         import React from 'react';
         import { Link } from 'react-router-dom';
-        import { TextLinkRenderer } from 'braid-design-system';
+        import { TextLinkRenderer, Text } from 'braid-design-system';
 
         export default () => (
-          <TextLinkRenderer>
-            {textLinkProps => (
-              <Link to="" {...textLinkProps}>
-                Text Link
-              </Link>
-            )}
-          </TextLinkRenderer>
+          <Text>
+            The last word of this sentence is a{' '}
+            <TextLinkRenderer>
+              {textLinkProps => (
+                <Link to="" {...textLinkProps}>
+                  link.
+                </Link>
+              )}
+            </TextLinkRenderer>
+          </Text>
         );
       `,
     },
