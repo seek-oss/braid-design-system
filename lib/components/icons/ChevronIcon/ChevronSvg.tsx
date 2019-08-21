84cfbc88d4fe61981208bd719e7ff500397e4f8f
Michael Taranto <mtaranto@seek.com.au>
2019-08-21 15:54:18 +1000

    fix: Apply currentColor to paths in svgs

--- lib/components/icons/ChevronIcon/ChevronSvg.tsx
+++ lib/components/icons/ChevronIcon/ChevronSvg.tsx
@@ -1,13 +1,10 @@
 import React, { AllHTMLAttributes } from 'react';
 
 export const ChevronSvg = (props: AllHTMLAttributes<SVGElement>) => (
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 1024 1024"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M945 266l78 67-510 524-510-524 75-69 435 451 432-449z" />
+  <svg width="16" height="16" viewBox="0 0 1024 1024" {...props}>
+    <path
+      d="M945 266l78 67-510 524-510-524 75-69 435 451 432-449z"
+      fill="currentColor"
+    />
   </svg>
 );
--- lib/components/icons/TickIcon/TickSvg.tsx
+++ lib/components/icons/TickIcon/TickSvg.tsx
@@ -1,13 +1,10 @@
 import React, { AllHTMLAttributes } from 'react';
 
 export const TickSvg = (props: AllHTMLAttributes<SVGElement>) => (
-  <svg
-    viewBox="90.471 302.809 823 823"
-    width="16"
-    height="16"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M365.407 1022.185L90.677 747.457l67.883-67.882L365.406 886.42l479.977-479.988 67.883 67.882" />
+  <svg viewBox="90.471 302.809 823 823" width="16" height="16" {...props}>
+    <path
+      d="M365.407 1022.185L90.677 747.457l67.883-67.882L365.406 886.42l479.977-479.988 67.883 67.882"
+      fill="currentColor"
+    />
   </svg>
 );
