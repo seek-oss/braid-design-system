1eb99bae07f920c40252817e5810ac94a39510fc
Michael Taranto <mtaranto@seek.com.au>
2019-08-29 15:09:41 +1000

    refactor: Add snapshots

--- lib/components/Column/Column.docs.tsx
+++ lib/components/Column/Column.docs.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ComponentDocs } from '../../../site/src/types';
 import { Columns } from '../Columns/Columns';
 import { Column } from './Column';
 import { Box } from '../Box/Box';
 import { Text } from '../Text/Text';
 import { HideCode } from '../private/HideCode';
+import { width as columnWidths } from '../Column/Column.treat';
+
+const widths = Object.keys(columnWidths) as Array<keyof typeof columnWidths>;
 
 const Content = ({ children = 'Column' }) => (
   <Box background="selection" padding="small">
@@ -73,6 +76,137 @@ const docs: ComponentDocs = {
             </HideCode>
           </Column>
         </Columns>
+      ),
+    },
+    {
+      label: 'All widths',
+      docsSite: false,
+      render: () => (
+        <Fragment>
+          {widths.map(width => (
+            <Box marginBottom="medium">
+              <Columns>
+                <Column width={width}>
+                  <HideCode>
+                    <Content>{width}</Content>
+                  </HideCode>
+                </Column>
+                <Column>
+                  <HideCode>
+                    <Content>Fluid</Content>
+                  </HideCode>
+                </Column>
+              </Columns>
+            </Box>
+          ))}
+        </Fragment>
+      ),
+    },
+    {
+      label: 'Gutter align',
+      docsSite: false,
+      render: () => (
+        <Fragment>
+          <Box marginBottom="medium">
+            <Columns>
+              <Column>
+                <HideCode>
+                  <Content>Fluid</Content>
+                </HideCode>
+              </Column>
+              <Column>
+                <HideCode>
+                  <Content>Fluid</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+          <Box marginBottom="medium">
+            <Columns>
+              <Column width="1/2">
+                <HideCode>
+                  <Content>&#189;</Content>
+                </HideCode>
+              </Column>
+              <Column>
+                <HideCode>
+                  <Content>Fluid</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+          <Box marginBottom="medium">
+            <Columns>
+              <Column width="1/2">
+                <HideCode>
+                  <Content>&#189;</Content>
+                </HideCode>
+              </Column>
+              <Column width="1/2">
+                <HideCode>
+                  <Content>&#189;</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+          <Box marginBottom="medium">
+            <Columns>
+              <Column width="1/4">
+                <HideCode>
+                  <Content>&#188;</Content>
+                </HideCode>
+              </Column>
+              <Column width="1/4">
+                <HideCode>
+                  <Content>&#188;</Content>
+                </HideCode>
+              </Column>
+              <Column>
+                <HideCode>
+                  <Content>Fluid</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+          <Box marginBottom="medium">
+            <Columns>
+              <Column width="1/3">
+                <HideCode>
+                  <Content>&#8531;</Content>
+                </HideCode>
+              </Column>
+              <Column width="1/4">
+                <HideCode>
+                  <Content>&#188;</Content>
+                </HideCode>
+              </Column>
+              <Column width="content">
+                <HideCode>
+                  <Content>Content</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+          <Box marginBottom="medium">
+            <Columns reverse>
+              <Column width="1/3">
+                <HideCode>
+                  <Content>&#8531;</Content>
+                </HideCode>
+              </Column>
+              <Column width="1/4">
+                <HideCode>
+                  <Content>&#188;</Content>
+                </HideCode>
+              </Column>
+              <Column width="content">
+                <HideCode>
+                  <Content>Content</Content>
+                </HideCode>
+              </Column>
+            </Columns>
+          </Box>
+        </Fragment>
       ),
     },
   ],
