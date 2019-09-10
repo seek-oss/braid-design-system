ce60f498d9b608696a04c1528244927527ffa52a
Matt Jones <mattjones701@gmail.com>
2019-09-11 08:41:25 +1000

    refactor: Move reset code to lib

--- reset/index.ts
+++ reset/index.ts
@@ -1,7 +1,1 @@
-import { markResetImported } from '../lib/reset/resetTracker';
-
-if (process.env.NODE_ENV === 'development') {
-  markResetImported();
-}
-
-export * from '../lib/reset/reset.treat';
+export * from '../lib/reset';
