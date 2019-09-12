ee2838084386a70581188a65667c537cd0512c43
Ashin Starr <astarr@seek.com.au>
2019-09-12 18:02:16 +1000

    fix: Bumped SeekAsias font weight to 600 for medium

--- lib/themes/seekAsia/makeTokens.ts
+++ lib/themes/seekAsia/makeTokens.ts
@@ -43,7 +43,7 @@ export default ({
       descenderHeightScale: 0.13,
       fontWeight: {
         regular: 400,
-        medium: 500,
+        medium: 600,
         strong: 700,
       },
       heading: {
