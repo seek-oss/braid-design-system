5b2fb651bba2f9f58b53175ea66b157720284e1c
patrickliu-sa <patrickliu@seekasia.com>
2019-09-24 15:24:03 +0800

    fix(Theme): add jobsDbRebrand

---
+++ lib/themes/jobsDbRebrand/index.ts
@@ -0,0 +1,10 @@
+import treatTheme from './theme.treat';
+import { Theme } from '../theme';
+
+const theme: Theme = {
+  name: 'jobsDbRebrand',
+  background: '#eee',
+  treatTheme,
+};
+
+export default theme;
---
+++ lib/themes/jobsDbRebrand/tokens.ts
@@ -0,0 +1,11 @@
+import makeTokens from '../seekAsia/makeTokens';
+
+const teal = '#0e7e8b';
+const blue = '#0c4b85';
+
+export default makeTokens({
+  name: 'jobsDbRebrand',
+  brand: `linear-gradient(90deg, ${teal}, ${blue})`,
+  formAccent: blue,
+  brandAccent: '#ff9000',
+});
