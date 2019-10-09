31ca89140234c42e9b7391055e4193a249f70ad8
Michael Taranto <mtaranto@seek.com.au>
2019-10-10 09:26:16 +1100

    refactor: Remove explicit casting

--- lib/components/Columns/Columns.treat.ts
+++ lib/components/Columns/Columns.treat.ts
@@ -2,24 +2,17 @@ import { style, styleMap } from 'sku/treat';
 import { mapToStyleProperty } from '../../utils';
 
 export const gutterOffset = styleMap(theme =>
-  mapToStyleProperty(
-    theme.space,
-    'marginLeft',
-    (space: number, propertyName) => ({
-      [propertyName]: -(space * theme.grid),
-    }),
-  ),
+  mapToStyleProperty(theme.space, 'marginLeft', (space, propertyName) => ({
+    [propertyName]: -(space * theme.grid),
+  })),
 );
 
 export const gutterOffsetDesktop = styleMap(theme => ({
   none: theme.utils.desktopStyles({ marginLeft: 0 }),
-  ...mapToStyleProperty(
-    theme.space,
-    'marginLeft',
-    (space: number, propertyName) =>
-      theme.utils.desktopStyles({
-        [propertyName]: -(space * theme.grid),
-      }),
+  ...mapToStyleProperty(theme.space, 'marginLeft', (space, propertyName) =>
+    theme.utils.desktopStyles({
+      [propertyName]: -(space * theme.grid),
+    }),
   ),
 }));
 
