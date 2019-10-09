7d5c575c7dcdaa8539577cc909b2d1cf8f9fdd87
Michael Taranto <mtaranto@seek.com.au>
2019-10-09 15:44:04 +1100

    refactor: Remove redundant change

--- lib/components/Columns/Columns.treat.ts
+++ lib/components/Columns/Columns.treat.ts
@@ -1,15 +1,15 @@
 import { style, styleMap } from 'sku/treat';
 import { mapToStyleProperty } from '../../utils';
 
-export const gutterOffset = styleMap(theme => ({
-  ...mapToStyleProperty(
+export const gutterOffset = styleMap(theme =>
+  mapToStyleProperty(
     theme.space,
     'marginLeft',
     (space: number, propertyName) => ({
       [propertyName]: -(space * theme.grid),
     }),
   ),
-}));
+);
 
 export const gutterOffsetDesktop = styleMap(theme => ({
   none: theme.utils.desktopStyles({ marginLeft: 0 }),
