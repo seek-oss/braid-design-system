8bfa4ee357ac8df4e80b51596145ccc0629bda59
Michael Taranto <mtaranto@seek.com.au>
2019-10-25 15:36:09 +1100

    feat(IconOverflow): Add component

---
+++ lib/components/icons/IconOverflow/IconOverflow.docs.tsx
@@ -0,0 +1,10 @@
+import { ComponentDocs } from '../../../../site/src/types';
+import examplesForIcon from '../../private/examplesForIcon';
+import { IconOverflow } from './IconOverflow';
+
+const docs: ComponentDocs = {
+  migrationGuide: true,
+  examples: examplesForIcon(IconOverflow),
+};
+
+export default docs;
