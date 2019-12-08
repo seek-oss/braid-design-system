f6327e093e9785d85306c8f56b2ea4b09a44de43
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-12-09 09:33:28 +1100

    refactor: Clean up negative margin styles

--- lib/hooks/useNegativeMargin/useNegativeMarginTop.treat.ts
+++ lib/hooks/useNegativeMargin/useNegativeMarginTop.treat.ts
@@ -1,38 +1,29 @@
 import { style, styleMap } from 'sku/treat';
 import { mapToStyleProperty } from '../../utils';
 
-export const preventCollapse = 1;
+const negativeMarginTop = (grid: number, rows: number) => ({
+  ':before': { marginTop: -(grid * rows) },
+});
+
 export const base = style({
-  paddingTop: preventCollapse,
-  ...{
-    ':before': { content: '""', display: 'block' },
-  },
+  paddingTop: 0.05, // Prevent margin collapse
+  ':before': { content: '""', display: 'block' },
 });
 
 export const mobile = styleMap(({ space, grid }) =>
-  mapToStyleProperty({ none: 0, ...space }, 'marginTop', (x, propertyName) => ({
-    ':before': {
-      [propertyName]: -(x * grid) - preventCollapse,
-    },
-  })),
+  mapToStyleProperty({ none: 0, ...space }, 'marginTop', rows =>
+    negativeMarginTop(grid, rows),
+  ),
 );
 
 export const tablet = styleMap(({ space, grid, utils }) =>
-  mapToStyleProperty({ none: 0, ...space }, 'marginTop', (x, propertyName) =>
-    utils.responsiveStyle({
-      tablet: {
-        ':before': { [propertyName]: -(x * grid) - preventCollapse },
-      },
-    }),
+  mapToStyleProperty({ none: 0, ...space }, 'marginTop', rows =>
+    utils.responsiveStyle({ tablet: negativeMarginTop(grid, rows) }),
   ),
 );
 
 export const desktop = styleMap(({ space, grid, utils }) =>
-  mapToStyleProperty({ none: 0, ...space }, 'marginTop', (x, propertyName) =>
-    utils.responsiveStyle({
-      desktop: {
-        ':before': { [propertyName]: -(x * grid) - preventCollapse },
-      },
-    }),
+  mapToStyleProperty({ none: 0, ...space }, 'marginTop', rows =>
+    utils.responsiveStyle({ desktop: negativeMarginTop(grid, rows) }),
   ),
 );
