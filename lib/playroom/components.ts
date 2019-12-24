4e5fc530344f5e8340c6f34d291f721638b42486
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-12-24 11:49:25 +1100

    docs(Playroom): Provide fallback behaviour for TextLink (#434)

--- lib/playroom/components.ts
+++ lib/playroom/components.ts
@@ -6,6 +6,7 @@ export { PlayroomCheckbox as Checkbox } from '../components/Checkbox/Checkbox.pl
 export { PlayroomDropdown as Dropdown } from '../components/Dropdown/Dropdown.playroom';
 export { PlayroomMonthPicker as MonthPicker } from '../components/MonthPicker/MonthPicker.playroom';
 export { PlayroomRadio as Radio } from '../components/Radio/Radio.playroom';
-export { PlayroomTextField as TextField } from '../components/TextField/TextField.playroom';
 export { PlayroomTextarea as Textarea } from '../components/Textarea/Textarea.playroom';
+export { PlayroomTextField as TextField } from '../components/TextField/TextField.playroom';
+export { PlayroomTextLink as TextLink } from '../components/TextLink/TextLink.playroom';
 export { PlayroomToggle as Toggle } from '../components/Toggle/Toggle.playroom';
