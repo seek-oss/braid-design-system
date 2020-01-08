95d25bd0cc4a9ff37cb82c40fbe5ad8de0842910
Matt Jones <mattjones701@gmail.com>
2020-01-09 10:22:47 +1100

    feat: Add BackgroundProvider for handling custom background colors

---
+++ lib/components/BackgroundProvider/BackgroundProvider.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react';
+import { renderBackgroundProvider } from '../Box/BackgroundContext';
+
+export interface BackgroundProviderProps {
+  type: 'light' | 'dark';
+  children: ReactElement;
+}
+
+export const BackgroundProvider = ({
+  type,
+  children,
+}: BackgroundProviderProps) => {
+  return renderBackgroundProvider(
+    type === 'dark' ? 'UNKNOWN_DARK' : 'UNKNOWN_LIGHT',
+    children,
+  );
+};
