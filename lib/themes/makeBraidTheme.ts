4debc6195f9b834b07b516f4caf237c789474f99
Matt Jones <mattjones701@gmail.com>
2020-01-15 14:08:08 +1100

    chore: Stuff

--- lib/themes/makeBraidTheme.ts
+++ lib/themes/makeBraidTheme.ts
@@ -181,6 +181,11 @@ const makeRuntimeTokens = (tokens: TreatTheme) => ({
   name: tokens.name,
   background: tokens.color.background.body,
   webFonts: makeWebFonts(tokens),
+  space: {
+    grid: tokens.grid,
+    space: tokens.space,
+  },
+  color: tokens.color,
   backgroundLightness: mapValues(
     tokens.color.background,
     (background, name) => {
@@ -226,11 +231,6 @@ export function makeBraidTheme(treatTokens: TreatTokens) {
 
   return {
     treatTheme: createTheme(decoratedTokens),
-    space: {
-      grid: treatTokens.grid,
-      space: treatTokens.space,
-    },
-    color: treatTokens.color,
     ...makeRuntimeTokens(decoratedTokens),
   };
 }
