20c031448eecbbe43476590d85a4454e0a6b1d2c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-01-16 10:51:40 +1300

    feat(BulletList): Support secondary tone (#457)

--- lib/components/BulletList/BulletList.tsx
+++ lib/components/BulletList/BulletList.tsx
@@ -5,33 +5,48 @@ import { UseStackProps } from '../Stack/Stack';
 
 const defaultSize = 'standard';
 const defaultSpace = 'medium';
+const defaultTone = 'neutral';
 interface BulletListContextValue {
-  size: NonNullable<UseTextProps['size']>;
-  space: NonNullable<UseStackProps['space']>;
+  size: NonNullable<BulletListProps['size']>;
+  space: NonNullable<BulletListProps['space']>;
+  tone: NonNullable<BulletListProps['tone']>;
 }
 export const BulletListContext = createContext<BulletListContextValue>({
   size: defaultSize,
   space: defaultSpace,
+  tone: defaultTone,
 });
+
+const validTones = ['neutral', 'secondary'] as const;
 
 export interface BulletListProps {
   children: ReactNode;
   size?: UseTextProps['size'];
   space?: UseStackProps['space'];
+  tone?: typeof validTones[number];
 }
 
 export const BulletList = ({
   children,
   size = defaultSize,
   space = defaultSpace,
+  tone = defaultTone,
 }: BulletListProps) => {
   const bulletListContextValue = useMemo(
     () => ({
       size,
       space,
+      tone,
     }),
-    [size, space],
+    [size, space, tone],
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (!validTones.includes(tone)) {
+      throw new Error(`Invalid tone: ${tone}`);
+    }
+  }
+
   return (
     <BulletListContext.Provider value={bulletListContextValue}>
       <Box component="ul">{children}</Box>
