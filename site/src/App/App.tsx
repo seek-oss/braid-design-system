7d06c5cc70d484007f4c65d55653140b4482c05d
Michael Taranto <michaeltaranto@users.noreply.github.com>
2020-01-16 09:37:34 +1100

    docs: Scroll to top on page change (#458)

--- site/src/App/App.tsx
+++ site/src/App/App.tsx
@@ -1,7 +1,8 @@
 // Import all themes up front so CSS overrides work
 import * as themes from '../../../lib/themes';
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useEffect } from 'react';
 import { Route } from 'react-router';
+import { useLocation } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import '../../../lib/reset';
@@ -20,27 +21,46 @@ const routes = [
   },
 ];
 
-export const App = () => (
-  <BraidProvider theme={themes.wireframe}>
-    <div>
-      {routes.map(({ path, exact, Component }) => (
-        <Route key={path} exact={exact} path={path}>
-          {({ match }) => (
-            <CSSTransition
-              in={match != null}
-              timeout={350}
-              classNames={{ ...styles }}
-              unmountOnExit
-            >
-              <StrictMode>
-                <Box position="absolute" className={styles.transitionContainer}>
-                  <Component />
-                </Box>
-              </StrictMode>
-            </CSSTransition>
-          )}
-        </Route>
-      ))}
-    </div>
-  </BraidProvider>
-);
+export const App = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if ('scrollBehavior' in window.document.documentElement.style) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
+  return (
+    <BraidProvider theme={themes.wireframe}>
+      <div>
+        {routes.map(({ path, exact, Component }) => (
+          <Route key={path} exact={exact} path={path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={350}
+                classNames={{ ...styles }}
+                unmountOnExit
+              >
+                <StrictMode>
+                  <Box
+                    position="absolute"
+                    className={styles.transitionContainer}
+                  >
+                    <Component />
+                  </Box>
+                </StrictMode>
+              </CSSTransition>
+            )}
+          </Route>
+        ))}
+      </div>
+    </BraidProvider>
+  );
+};
