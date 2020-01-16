043d07f4fc013e1bfb84a7c46051cac83a154afe
Michael Taranto <michaeltaranto@users.noreply.github.com>
2020-01-17 08:18:44 +1100

    feat(Box): Add flex wrap support (#461)

--- lib/components/Inline/Inline.tsx
+++ lib/components/Inline/Inline.tsx
@@ -1,6 +1,4 @@
 import React, { Children } from 'react';
-import classnames from 'classnames';
-import { useStyles } from 'sku/treat';
 import { Box } from '../Box/Box';
 import { ResponsiveSpace } from '../Box/useBoxStyles';
 import {
@@ -9,7 +7,6 @@ import {
 } from '../../hooks/useNegativeMargin/useNegativeMargin';
 import { ResponsiveProp } from '../../utils/responsiveProp';
 import { Align, alignToFlexAlign } from '../../utils/align';
-import * as styleRefs from './Inline.treat';
 import { ReactNodeNoStrings } from '../private/ReactNodeNoStrings';
 
 export interface InlineProps {
@@ -19,16 +16,16 @@ export interface InlineProps {
 }
 
 export const Inline = ({ space = 'none', align, children }: InlineProps) => {
-  const styles = useStyles(styleRefs);
   const negativeMarginLeft = useNegativeMarginLeft(space);
   const negativeMarginTop = useNegativeMarginTop(space);
 
   return (
-    <Box className={classnames(negativeMarginTop)}>
+    <Box className={negativeMarginTop}>
       <Box
         display="flex"
         justifyContent={alignToFlexAlign(align)}
-        className={classnames(styles.flexWrap, negativeMarginLeft)}
+        flexWrap="wrap"
+        className={negativeMarginLeft}
       >
         {Children.map(children, child =>
           child !== null && child !== undefined ? (
