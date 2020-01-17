a3464f4515a76ce0419b22b8d0cf036f1fa9f41a
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-01-17 17:54:43 +1300

    docs: Manage scroll position with 'react-router-scroll-memory' (#464)

--- site/src/client.tsx
+++ site/src/client.tsx
@@ -2,6 +2,8 @@ import 'focus-visible';
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// @ts-ignore
+import ScrollMemory from 'react-router-scroll-memory';
 import { App } from './App/App';
 import { RenderContext } from './types';
 import { ConfigProvider } from './App/ConfigContext';
@@ -10,6 +12,7 @@ export default (app: RenderContext) => {
   hydrate(
     <BrowserRouter basename={app.routerBasename}>
       <ConfigProvider value={app.appConfig}>
+        <ScrollMemory />
         <App />
       </ConfigProvider>
     </BrowserRouter>,
