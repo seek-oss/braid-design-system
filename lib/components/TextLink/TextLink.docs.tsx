51ca1a5a4228d6458651784364b27210f68c9833
Matt Jones <mattjones701@gmail.com>
2020-01-28 15:56:17 +1100

    docs: Fix docs links

--- lib/components/TextLink/TextLink.docs.tsx
+++ lib/components/TextLink/TextLink.docs.tsx
@@ -18,6 +18,14 @@ const docs: ComponentDocs = {
       label: 'Text Link',
       Example: () => (
         <Text>
+          <TextLink href="">Text link</TextLink>
+        </Text>
+      ),
+    },
+    {
+      label: 'Text Link in a sentence',
+      Example: () => (
+        <Text>
           The last word of a sentence is a{' '}
           <TextLink href="">text link.</TextLink>
         </Text>
@@ -32,14 +40,6 @@ const docs: ComponentDocs = {
             visited link.
           </TextLink>
         </Text>
-      ),
-    },
-    {
-      label: 'Block Text Link',
-      Example: () => (
-        <TextLink href="">
-          <Text>Text Link</Text>
-        </TextLink>
       ),
     },
     {
@@ -61,28 +61,12 @@ const docs: ComponentDocs = {
       ),
     },
     {
-      label: 'Large Block Text Link',
-      Example: () => (
-        <TextLink href="">
-          <Text size="large">Text Link</Text>
-        </TextLink>
-      ),
-    },
-    {
       label: 'Small Text Link',
       Example: () => (
         <Text size="small">
           The last word of a sentence is a{' '}
           <TextLink href="">text link.</TextLink>
         </Text>
-      ),
-    },
-    {
-      label: 'Small Block Text Link',
-      Example: () => (
-        <TextLink href="">
-          <Text size="small">Text Link</Text>
-        </TextLink>
       ),
     },
     {
@@ -95,23 +79,7 @@ const docs: ComponentDocs = {
       ),
     },
     {
-      label: 'Xsmall Block Text Link',
-      Example: () => (
-        <TextLink href="">
-          <Text size="xsmall">Text Link</Text>
-        </TextLink>
-      ),
-    },
-    {
       label: 'Heading Level 1 Link',
-      Example: () => (
-        <TextLink href="">
-          <Heading level="1">Heading link.</Heading>
-        </TextLink>
-      ),
-    },
-    {
-      label: 'Heading Level 1 Link (Inline)',
       Example: () => (
         <Heading level="1">
           The last word of this heading is a <TextLink href="">link.</TextLink>
@@ -121,14 +89,6 @@ const docs: ComponentDocs = {
     {
       label: 'Heading Level 2 Link',
       Example: () => (
-        <TextLink href="">
-          <Heading level="2">Heading link.</Heading>
-        </TextLink>
-      ),
-    },
-    {
-      label: 'Heading Level 2 Link (Inline)',
-      Example: () => (
         <Heading level="2">
           The last word of this heading is a <TextLink href="">link.</TextLink>
         </Heading>
@@ -137,14 +97,6 @@ const docs: ComponentDocs = {
     {
       label: 'Heading Level 3 Link',
       Example: () => (
-        <TextLink href="">
-          <Heading level="3">Heading link.</Heading>
-        </TextLink>
-      ),
-    },
-    {
-      label: 'Heading Level 3 Link (Inline)',
-      Example: () => (
         <Heading level="3">
           The last word of this heading is a <TextLink href="">link.</TextLink>
         </Heading>
@@ -152,14 +104,6 @@ const docs: ComponentDocs = {
     },
     {
       label: 'Heading Level 4 Link',
-      Example: () => (
-        <TextLink href="">
-          <Heading level="4">Heading link.</Heading>
-        </TextLink>
-      ),
-    },
-    {
-      label: 'Heading Level 4 Link (Inline)',
       Example: () => (
         <Heading level="4">
           The last word of this heading is a <TextLink href="">link.</TextLink>
@@ -178,17 +122,6 @@ const docs: ComponentDocs = {
           </TextLink>
           .
         </Text>
-      ),
-    },
-    {
-      label: 'Block Text Link with icon',
-      docsSite: false,
-      Example: () => (
-        <TextLink href="">
-          <Text>
-            Text Link <IconChevron direction="right" />
-          </Text>
-        </TextLink>
       ),
     },
     {
