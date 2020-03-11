c4bdcb5ee2303849ee8791e259192cac99de98f5
Matt Jones <mattjones701@gmail.com>
2020-03-11 13:05:39 +1100

    Improve changelog format

---
+++ .changeset/changelog.js
@@ -0,0 +1,55 @@
+const { getInfo } = require('@changesets/get-github-info');
+
+const repo = 'seek-oss/braid-design-system';
+
+const changelogFunctions = {
+  getDependencyReleaseLine: async (changesets, dependenciesUpdated) => {
+    if (dependenciesUpdated.length === 0) return '';
+
+    const changesetLink = `- Updated dependencies [${(
+      await Promise.all(
+        changesets.map(async cs => {
+          if (cs.commit) {
+            let { links } = await getInfo({
+              repo,
+              commit: cs.commit,
+            });
+            return links.commit;
+          }
+        }),
+      )
+    )
+      .filter(_ => _)
+      .join(', ')}]:`;
+
+    const updatedDepenenciesList = dependenciesUpdated.map(
+      dependency => `  - ${dependency.name}@${dependency.newVersion}`,
+    );
+
+    return [changesetLink, ...updatedDepenenciesList].join('\n');
+  },
+  getReleaseLine: async changeset => {
+    const [firstLine, ...futureLines] = changeset.summary
+      .split('\n')
+      .map(l => l.trimRight());
+
+    if (changeset.commit) {
+      let { links } = await getInfo({
+        repo,
+        commit: changeset.commit,
+      });
+
+      const versionInfo = `${changeset.commit} ${
+        links.pull === null ? '' : links.pull
+      }`;
+
+      const summary = `- ${firstLine}: ${versionInfo}`;
+
+      return `${summary}\n${futureLines.map(l => `  ${l}`).join('\n')}`;
+    } else {
+      return `\n\n- ${firstLine}\n${futureLines.map(l => `  ${l}`).join('\n')}`;
+    }
+  },
+};
+
+module.exports = changelogFunctions;
