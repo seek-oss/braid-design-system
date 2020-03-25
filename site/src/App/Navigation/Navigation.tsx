f2cc9adc4da81337141055e2ecc099b15d7336e9
Michael Taranto <mtaranto@seek.com.au>
2020-03-26 08:39:08 +1100

    wip

--- site/src/App/Navigation/Navigation.tsx
+++ site/src/App/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, ReactNode } from 'react';
+import React, { useState, useRef, ReactNode, useEffect } from 'react';
 import { useStyles } from 'sku/react-treat';
 import { ContentBlock, Text, Box, Hidden } from '../../../../lib/components';
 import { useIsolatedScroll } from '../../../../lib/components/Autosuggest/useIsolatedScroll';
@@ -8,6 +8,7 @@ import { Link } from '../Link/Link';
 import { MenuButton } from '../MenuButton/MenuButton';
 import { SubNavigation } from '../SubNavigation/SubNavigation';
 import * as styleRefs from './Navigation.treat';
+import { useWindowScroll } from 'react-use';
 
 const Header = ({
   menuOpen,
@@ -35,62 +36,129 @@ const Header = ({
 interface NavigationProps {
   children: ReactNode;
 }
+const HEADER_HEIGHT = 96;
+const LARGE_SPACE = 32;
+const SUBNAV_WIDTH = 300;
 export const Navigation = ({ children }: NavigationProps) => {
-  const styles = useStyles(styleRefs);
-  const [isMenuOpen, setMenuOpen] = useState(false);
+  const { y: scrollTop } = useWindowScroll();
+  const [showStickyHeader, setShowStickyHeader] = useState(false);
+
+  useEffect(() => {
+    if (scrollTop > HEADER_HEIGHT - LARGE_SPACE) {
+      setShowStickyHeader(true);
+    } else {
+      setShowStickyHeader(false);
+    }
+  }, [scrollTop]);
 
   const menuRef = useRef<HTMLElement | null>(null);
   useIsolatedScroll(menuRef.current);
 
-  const bottomSpace = 'xxlarge';
+  // const bottomSpace = 'xxlarge';
 
   return (
-    <Box
-      paddingX={['gutter', 'large']}
-      paddingBottom={bottomSpace}
-      overflow={isMenuOpen ? 'hidden' : undefined}
-      className={isMenuOpen ? styles.container : undefined}
-    >
-      <ContentBlock width="large">
-        <Box
-          position={isMenuOpen ? 'fixed' : 'absolute'}
-          top={0}
-          bottom={0}
-          width={isMenuOpen ? 'full' : undefined}
-          className={[styles.header, isMenuOpen ? styles.isOpen : '']}
-        >
-          <Header
-            menuOpen={isMenuOpen}
-            menuClick={() => setMenuOpen(!isMenuOpen)}
-          />
+    <Box>
+      <Box
+        position="relative"
+        background="positiveLight"
+        padding="large"
+        style={{ zIndex: 1 }}
+      >
+        <Text baseline={false}>
+          <Link to="/">
+            <Logo iconOnly height={32} />
+          </Link>
+        </Text>
+      </Box>
 
-          <Box
-            ref={menuRef}
-            position="absolute"
-            bottom={0}
-            overflow="auto"
-            paddingBottom={bottomSpace}
-            width="full"
-            transition="fast"
-            className={[styles.menu, isMenuOpen ? styles.isOpen : '']}
-            role={isMenuOpen ? 'menu' : undefined}
-          >
-            <SubNavigation onSelect={() => setMenuOpen(false)} />
-          </Box>
+      <Box position="fixed" bottom={0} top={0} style={{ width: SUBNAV_WIDTH }}>
+        <Box
+          padding="large"
+          background="cautionLight"
+          transition="fast"
+          style={{ opacity: showStickyHeader ? 1 : 0 }}
+        >
+          <Text baseline={false}>
+            <Link to="/">
+              <Logo iconOnly height={32} />
+            </Link>
+          </Text>
         </Box>
         <Box
-          display="flex"
-          transition="fast"
-          paddingLeft={['none', 'gutter', 'large']}
-          pointerEvents={isMenuOpen ? 'none' : undefined}
-          className={[styles.content, isMenuOpen ? styles.isOpen : '']}
+          ref={menuRef}
+          background="criticalLight"
+          padding="large"
+          overflow="scroll"
         >
-          <Box position="relative" width="full">
-            {children}
-            <Overlay visible={isMenuOpen} />
-          </Box>
+          <SubNavigation />
         </Box>
-      </ContentBlock>
+      </Box>
+
+      <Box
+        background="infoLight"
+        padding="large"
+        style={{ marginLeft: SUBNAV_WIDTH }}
+      >
+        {children}
+      </Box>
     </Box>
   );
+
+  // const styles = useStyles(styleRefs);
+  // const [isMenuOpen, setMenuOpen] = useState(false);
+
+  // const menuRef = useRef<HTMLElement | null>(null);
+  // useIsolatedScroll(menuRef.current);
+
+  // const bottomSpace = 'xxlarge';
+
+  // return (
+  //   <Box
+  //     paddingX={['gutter', 'large']}
+  //     paddingBottom={bottomSpace}
+  //     overflow={isMenuOpen ? 'hidden' : undefined}
+  //     className={isMenuOpen ? styles.container : undefined}
+  //   >
+  //     <ContentBlock width="large">
+  //       <Box
+  //         position={isMenuOpen ? 'fixed' : 'absolute'}
+  //         top={0}
+  //         bottom={0}
+  //         width={isMenuOpen ? 'full' : undefined}
+  //         className={[styles.header, isMenuOpen ? styles.isOpen : '']}
+  //       >
+  //         <Header
+  //           menuOpen={isMenuOpen}
+  //           menuClick={() => setMenuOpen(!isMenuOpen)}
+  //         />
+
+  //         <Box
+  //           ref={menuRef}
+  //           position="absolute"
+  //           bottom={0}
+  //           overflow="auto"
+  //           paddingBottom={bottomSpace}
+  //           width="full"
+  //           transition="fast"
+  //           className={[styles.menu, isMenuOpen ? styles.isOpen : '']}
+  //           role={isMenuOpen ? 'menu' : undefined}
+  //         >
+  //           <SubNavigation onSelect={() => setMenuOpen(false)} />
+  //         </Box>
+  //       </Box>
+  //       <Box
+  //         display="flex"
+  //         transition="fast"
+  //         paddingLeft={['none', 'gutter', 'large']}
+  //         pointerEvents={isMenuOpen ? 'none' : undefined}
+  //         className={[styles.content, isMenuOpen ? styles.isOpen : '']}
+  //       >
+  //         <Box position="relative" width="full">
+  //           {children}
+  //           <Overlay visible={isMenuOpen} />
+  //         </Box>
+  //       </Box>
+  //     </ContentBlock>
+  //   </Box>
+  // );
 };
