b800c4c8e087ac79709f778b502381380b80df3e
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-03-31 16:17:56 +1100

    Use standard border width token to define divider height

--- lib/components/Divider/Divider.treat.ts
+++ lib/components/Divider/Divider.treat.ts
@@ -1,6 +1,8 @@
 import { style, styleMap } from 'sku/treat';
 
-export const base = style({ height: 1 });
+export const base = style(theme => ({
+  height: theme.border.width.standard,
+}));
 
 export const weight = styleMap(theme => ({
   standard: { background: theme.border.color.standard },
