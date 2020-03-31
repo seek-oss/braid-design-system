35b9b559be8a853cd86f8a4be25b6fb1835cd0e0
Michael Taranto <mtaranto@seek.com.au>
2020-03-31 11:25:17 +1100

    Docs: Update Navigation & sticky header on mobile

--- site/src/App/Navigation/Navigation.treat.ts
+++ site/src/App/Navigation/Navigation.treat.ts
@@ -1,108 +1,92 @@
 import { globalStyle, style } from 'sku/treat';
+import { pageOverlay } from '../../../../lib/components/private/zIndex';
 
 const menuWidth = 300;
-const headerHeight = 96;
 
 export const isOpen = style({});
 
-export const preventContentScroll = style({
-  maxHeight: '100vh',
+const bodyBackground = style(({ color }) => ({
+  background: color.background.body,
+}));
+
+const headerOffset = style(({ utils }) =>
+  utils.responsiveStyle({
+    mobile: {
+      top: 96,
+    },
+    tablet: {
+      top: 120,
+    },
+  }),
+);
+
+const fixedWidthAboveMobile = style(({ utils, space, grid }) =>
+  utils.responsiveStyle({
+    tablet: {
+      width: menuWidth - grid * space.medium,
+    },
+  }),
+);
+
+const overlay = style({
+  zIndex: pageOverlay,
 });
 
-export const sideBar = style(({ breakpoint }) => ({
-  zIndex: 1,
+const hideOnMobileWhenOpen = style(({ breakpoint }) => ({
   '@media': {
     [`screen and (max-width: ${breakpoint.tablet - 1}px)`]: {
-      maxWidth: 400,
       selectors: {
-        [`&:not(${isOpen})`]: {
-          transform: 'translateX(-10%)',
+        [`&${isOpen}`]: {
           opacity: 0,
         },
       },
     },
-    [`screen and (min-width: ${breakpoint.tablet}px)`]: {
-      maxWidth: menuWidth,
+  },
+}));
+
+const hideOnMobileWhenClosed = style(({ breakpoint }) => ({
+  '@media': {
+    [`screen and (max-width: ${breakpoint.tablet - 1}px)`]: {
+      selectors: {
+        [`&:not(${isOpen})`]: {
+          opacity: 0,
+        },
+      },
     },
   },
 }));
 
-export const subNavigationContainer = style(({ utils }) => ({
-  top: headerHeight,
-  zIndex: 2,
-}));
-
-export const header = style({
-  zIndex: 2,
-});
-
-export const mockHeader = style({
-  opacity: 0,
-  transform: 'translateY(-16px)',
-});
-
-export const content = style(({ utils }) =>
+const subNavOffsetAboveMobile = style(({ utils }) =>
   utils.responsiveStyle({
-    mobile: {
-      top: headerHeight,
-    },
     tablet: {
       marginLeft: menuWidth,
     },
   }),
 );
 
-// export const isOpen = style({});
+export const scrollLock = style({
+  overflow: 'hidden',
+});
 
-// export const header = style(({ utils }) =>
-//   utils.responsiveStyle({
-//     mobile: {
-//       zIndex: 1,
-//     },
-//     tablet: {
-//       position: 'fixed',
-//     },
-//   }),
-// );
+export const hide = style({
+  opacity: 0,
+});
 
-// export const container = style({
-//   maxHeight: '100vh',
-// });
+export const subNavigationContainer = [
+  headerOffset,
+  bodyBackground,
+  overlay,
+  fixedWidthAboveMobile,
+  hideOnMobileWhenClosed,
+];
 
-// export const menu = style(({ breakpoint, space, grid }) => ({
-//   '@media': {
-//     [`screen and (max-width: ${breakpoint.tablet - 1}px)`]: {
-//       top: headerHeight + space.medium * grid,
-//       selectors: {
-//         [`&:not(${isOpen})`]: {
-//           opacity: 0,
-//         },
-//       },
-//     },
-//     [`screen and (min-width: ${breakpoint.tablet}px)`]: {
-//       top: headerHeight + space.large * grid,
-//       width: menuWidth,
-//     },
-//   },
-// }));
+export const pageContent = [
+  headerOffset,
+  subNavOffsetAboveMobile,
+  hideOnMobileWhenOpen,
+];
 
-// export const content = style(({ breakpoint, space, grid }) => ({
-//   '@media': {
-//     [`screen and (max-width: ${breakpoint.tablet - 1}px)`]: {
-//       paddingTop: headerHeight + space.medium * grid,
-//       selectors: {
-//         [`&${isOpen}`]: {
-//           transform: `translateX(${menuWidth + space.gutter * grid}px)`,
-//           opacity: 0.4,
-//         },
-//       },
-//     },
-//     [`screen and (min-width: ${breakpoint.tablet}px)`]: {
-//       paddingTop: headerHeight + space.large * grid,
-//       marginLeft: `${menuWidth}px`,
-//     },
-//   },
-// }));
+export const stickyHeader = [bodyBackground];
 
 globalStyle('html, body', {
   margin: 0,
--- site/src/App/Navigation/Navigation.tsx
+++ site/src/App/Navigation/Navigation.tsx
@@ -1,14 +1,23 @@
-import React, { useState, useRef, ReactNode, useEffect } from 'react';
+import React, {
+  useState,
+  useRef,
+  ReactNode,
+  useEffect,
+  forwardRef,
+} from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useWindowScroll, useInterval } from 'react-use';
 import { useStyles } from 'sku/react-treat';
 import { ContentBlock, Text, Box, Hidden } from '../../../../lib/components';
 import { useIsolatedScroll } from '../../../../lib/components/Autosuggest/useIsolatedScroll';
-import { Overlay } from '../../../../lib/components/private/Overlay/Overlay';
-import { Logo } from '../Logo/Logo';
-import { Link } from '../Link/Link';
-import { MenuButton } from '../MenuButton/MenuButton';
+import { useBoxStyles } from '../../../../lib/components/Box/useBoxStyles';
+import { BoxProps } from '../../../../lib/components/Box/Box';
 import { SubNavigation } from '../SubNavigation/SubNavigation';
+import { MenuButton } from '../MenuButton/MenuButton';
+import { Logo } from '../Logo/Logo';
 import * as styleRefs from './Navigation.treat';
-import { useWindowScroll } from 'react-use';
+
+const space = 'medium';
 
 const Header = ({
   menuOpen,
@@ -17,167 +26,137 @@ const Header = ({
   menuOpen: boolean;
   menuClick: () => void;
 }) => (
-  <Text baseline={false}>
-    <Box display="flex" alignItems="center">
-      <Hidden print above="mobile">
-        <Box paddingRight="medium" display="flex" alignItems="center">
-          <MenuButton open={menuOpen} onClick={menuClick} />
-        </Box>
-      </Hidden>
-      <Link to="/" tabIndex={menuOpen ? -1 : undefined}>
-        <Logo iconOnly height={32} />
-      </Link>
+  <Box paddingY="large" paddingX={space}>
+    <Text component="div" baseline={false}>
+      <Box display="flex" alignItems="center">
+        <Hidden print above="mobile">
+          <Box paddingRight="medium" display="flex" alignItems="center">
+            <MenuButton open={menuOpen} onClick={menuClick} />
+          </Box>
+        </Hidden>
+        <Link
+          to="/"
+          tabIndex={menuOpen ? -1 : undefined}
+          className={useBoxStyles({ component: 'a' })}
+        >
+          <Logo iconOnly height={32} />
+        </Link>
+      </Box>
+    </Text>
+  </Box>
+);
+
+const FixedContentBlock = forwardRef<HTMLElement, BoxProps>(
+  ({ children, ...props }, forwardedRef) => (
+    <Box transition="fast" {...props} position="fixed" ref={forwardedRef}>
+      <ContentBlock width="large">{children}</ContentBlock>
     </Box>
-  </Text>
+  ),
 );
 
 interface NavigationProps {
   children: ReactNode;
 }
-const HEADER_HEIGHT = 96;
-const LARGE_SPACE = 32;
-const SUBNAV_WIDTH = 300;
+
 export const Navigation = ({ children }: NavigationProps) => {
   const styles = useStyles(styleRefs);
   const { y: scrollTop } = useWindowScroll();
   const [showStickyHeader, setShowStickyHeader] = useState(false);
   const [isMenuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    if (scrollTop > HEADER_HEIGHT - LARGE_SPACE) {
-      setShowStickyHeader(true);
-    } else {
-      setShowStickyHeader(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [direction, setDirection] = useState<'up' | 'down' | null>(null);
+
+  useInterval(() => {
+    if (lastScrollTop !== scrollTop) {
+      if (Math.abs(lastScrollTop - scrollTop) > 0) {
+        setDirection(scrollTop > lastScrollTop ? 'down' : 'up');
+      }
+      setLastScrollTop(scrollTop);
     }
-  }, [scrollTop]);
+  }, 250);
+
+  const location = useLocation();
+  useEffect(() => {
+    setDirection(null);
+  }, [location]);
+
+  useEffect(() => {
+    setShowStickyHeader(scrollTop > 300 && direction === 'up');
+  }, [direction, scrollTop]);
+
+  useEffect(() => {
+    document.body.classList[isMenuOpen ? 'add' : 'remove'](styles.scrollLock);
+  }, [isMenuOpen, styles.scrollLock]);
 
   const menuRef = useRef<HTMLElement | null>(null);
   useIsolatedScroll(menuRef.current);
 
   return (
-    <Box
-      overflow={isMenuOpen ? 'hidden' : undefined}
-      className={isMenuOpen ? styles.preventContentScroll : undefined}
-    >
-      <Box
-        position="absolute"
-        width="full"
-        background="positiveLight"
-        padding="large"
-        className={styles.header}
-      >
-        <Header
-          menuOpen={isMenuOpen}
-          menuClick={() => setMenuOpen(!isMenuOpen)}
-        />
-      </Box>
+    <ContentBlock width="large">
+      <Box paddingRight={['none', space]}>
+        <FixedContentBlock top={0} left={0} right={0}>
+          <Header
+            menuOpen={isMenuOpen}
+            menuClick={() => setMenuOpen(!isMenuOpen)}
+          />
+        </FixedContentBlock>
 
-      <Box
-        position="fixed"
-        width="full"
-        bottom={0}
-        top={0}
-        transition="fast"
-        className={[styles.sideBar, isMenuOpen ? styles.isOpen : undefined]}
-      >
+        <FixedContentBlock
+          ref={menuRef}
+          overflow="scroll"
+          bottom={0}
+          paddingY="small"
+          paddingX={space}
+          paddingBottom="xxlarge"
+          width="full"
+          display={[isMenuOpen ? 'block' : 'none', 'block']}
+          className={[
+            styles.subNavigationContainer,
+            isMenuOpen ? styles.isOpen : undefined,
+          ]}
+        >
+          <SubNavigation onSelect={() => setMenuOpen(false)} />
+        </FixedContentBlock>
+
         <Box
-          display={['none', 'block']}
-          padding="large"
-          background="cautionLight"
+          position="relative"
+          background="card"
+          borderRadius="standard"
+          boxShadow="medium"
+          paddingY={['large', 'large', 'xlarge']}
+          paddingX={['medium', 'large', 'xlarge']}
+          marginBottom="xxlarge"
           transition="fast"
-          className={showStickyHeader ? undefined : styles.mockHeader}
+          pointerEvents={isMenuOpen ? 'none' : undefined}
+          className={[
+            styles.pageContent,
+            isMenuOpen ? styles.isOpen : undefined,
+          ]}
+        >
+          {children}
+        </Box>
+
+        <FixedContentBlock
+          top={0}
+          left={0}
+          right={0}
+          boxShadow="small"
+          display={['block', 'none']}
+          pointerEvents={showStickyHeader ? undefined : 'none'}
+          className={[
+            styles.stickyHeader,
+            showStickyHeader ? undefined : styles.hide,
+          ]}
+          tabIndex={-1}
+          aria-hidden={true}
         >
           <Header
             menuOpen={isMenuOpen}
             menuClick={() => setMenuOpen(!isMenuOpen)}
           />
-        </Box>
-
-        <Overlay visible={isMenuOpen} boxShadow="large" />
-
-        <Box
-          ref={menuRef}
-          position="absolute"
-          left={0}
-          right={0}
-          bottom={0}
-          background="criticalLight"
-          paddingY="large"
-          paddingLeft="large"
-          overflow="scroll"
-          transition="fast"
-          className={styles.subNavigationContainer}
-        >
-          <SubNavigation onSelect={() => setMenuOpen(false)} />
-        </Box>
+        </FixedContentBlock>
       </Box>
-
-      <Box
-        position="relative"
-        background="infoLight"
-        padding="large"
-        className={styles.content}
-      >
-        {children}
-      </Box>
-    </Box>
+    </ContentBlock>
   );
-
-  // const styles = useStyles(styleRefs);
-  // const [isMenuOpen, setMenuOpen] = useState(false);
-
-  // const menuRef = useRef<HTMLElement | null>(null);
-  // useIsolatedScroll(menuRef.current);
-
-  // const bottomSpace = 'xxlarge';
-
-  // return (
-  //   <Box
-  //     paddingX={['gutter', 'large']}
-  //     paddingBottom={bottomSpace}
-  //     overflow={isMenuOpen ? 'hidden' : undefined}
-  //     className={isMenuOpen ? styles.container : undefined}
-  //   >
-  //     <ContentBlock width="large">
-  //       <Box
-  //         position={isMenuOpen ? 'fixed' : 'absolute'}
-  //         top={0}
-  //         bottom={0}
-  //         width={isMenuOpen ? 'full' : undefined}
-  //         className={[styles.header, isMenuOpen ? styles.isOpen : '']}
-  //       >
-  //         <Header
-  //           menuOpen={isMenuOpen}
-  //           menuClick={() => setMenuOpen(!isMenuOpen)}
-  //         />
-
-  //         <Box
-  //           ref={menuRef}
-  //           position="absolute"
-  //           bottom={0}
-  //           overflow="auto"
-  //           paddingBottom={bottomSpace}
-  //           width="full"
-  //           transition="fast"
-  //           className={[styles.menu, isMenuOpen ? styles.isOpen : '']}
-  //           role={isMenuOpen ? 'menu' : undefined}
-  //         >
-  //           <SubNavigation onSelect={() => setMenuOpen(false)} />
-  //         </Box>
-  //       </Box>
-  //       <Box
-  //         display="flex"
-  //         transition="fast"
-  //         paddingLeft={['none', 'gutter', 'large']}
-  //         pointerEvents={isMenuOpen ? 'none' : undefined}
-  //         className={[styles.content, isMenuOpen ? styles.isOpen : '']}
-  //       >
-  //         <Box position="relative" width="full">
-  //           {children}
-  //           <Overlay visible={isMenuOpen} />
-  //         </Box>
-  //       </Box>
-  //     </ContentBlock>
-  //   </Box>
-  // );
 };
