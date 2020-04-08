4e7700ad858721f697e34d639926fdfe661338aa
Matt Jones <mattjones701@gmail.com>
2020-04-09 09:47:43 +1000

    Use for loop instead for buildDataAttributes

--- lib/components/private/buildDataAttributes.ts
+++ lib/components/private/buildDataAttributes.ts
@@ -1,9 +1,12 @@
 export type DataAttributeMap = Record<string, string | number>;
 
-export default (
-  data: DataAttributeMap = {},
-): { [dataAttribute: string]: string } =>
-  Object.assign(
-    {},
-    ...Object.keys(data).map((key) => ({ [`data-${key}`]: data[key] })),
-  );
+export default (data: DataAttributeMap = {}): DataAttributeMap => {
+  const keys = Object.keys(data);
+  const dataAttributes: DataAttributeMap = {};
+
+  for (const key of keys) {
+    dataAttributes[`data-${key}`] = data[key];
+  }
+
+  return dataAttributes;
+};
