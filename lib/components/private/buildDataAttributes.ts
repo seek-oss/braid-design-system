47c637e34fef8cea3bdec9c90c730621b67ef947
Matt Jones <mattjones701@gmail.com>
2020-04-08 10:48:53 +1000

    Remove lodash from buildDataAttributes

--- lib/components/private/buildDataAttributes.ts
+++ lib/components/private/buildDataAttributes.ts
@@ -1,6 +1,9 @@
-import mapKeys from 'lodash/mapKeys';
-
 export type DataAttributeMap = Record<string, string | number>;
 
-export default (data: DataAttributeMap = {}) =>
-  mapKeys(data, (_, attrName) => `data-${attrName}`);
+export default (
+  data: DataAttributeMap = {},
+): { [dataAttribute: string]: string } =>
+  Object.assign(
+    {},
+    ...Object.keys(data).map((key) => ({ [`data-${key}`]: data[key] })),
+  );
