425e761c458d8e7cd81c7bb97e86c1cd8a119b2b
Michael Taranto <mtaranto@seek.com.au>
2020-04-17 12:12:32 +1000

    Add TextDropdown

--- lib/playroom/components.ts
+++ lib/playroom/components.ts
@@ -5,7 +5,6 @@ export { AccordionItem } from '../components/AccordionItem/AccordionItem.playroo
 export { Autosuggest } from '../components/Autosuggest/Autosuggest.playroom';
 export { ButtonLink } from '../components/ButtonLink/ButtonLink.playroom';
 export { Checkbox } from '../components/Checkbox/Checkbox.playroom';
-export { Combobox } from '../components/Combobox/Combobox.playroom';
 export { Dropdown } from '../components/Dropdown/Dropdown.playroom';
 export { FieldLabel } from '../components/FieldLabel/FieldLabel.playroom';
 export { FieldMessage } from '../components/FieldMessage/FieldMessage.playroom';
@@ -13,6 +12,7 @@ export { Link } from '../components/Link/Link.playroom';
 export { MonthPicker } from '../components/MonthPicker/MonthPicker.playroom';
 export { Radio } from '../components/Radio/Radio.playroom';
 export { Textarea } from '../components/Textarea/Textarea.playroom';
+export { TextDropdown } from '../components/TextDropdown/TextDropdown.playroom';
 export { TextField } from '../components/TextField/TextField.playroom';
 export { TextLink } from '../components/TextLink/TextLink.playroom';
 export { Toggle } from '../components/Toggle/Toggle.playroom';
