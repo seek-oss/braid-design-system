553f511dd903ff6e07a1842df9b22278924aad5c
Michael Taranto <mtaranto@seek.com.au>
2020-05-06 10:21:44 +1000

    Fix build

--- lib/components/Dropdown/Dropdown.tsx
+++ lib/components/Dropdown/Dropdown.tsx
@@ -19,46 +19,55 @@ export interface DropdownProps
 }
 
 const NamedDropdown = forwardRef<HTMLSelectElement, DropdownProps>(
-  (
-    { children, value, onChange, onBlur, onFocus, placeholder, ...restProps },
-    ref,
-  ) => (
-    <Field
-      {...restProps}
-      labelId={undefined}
-      secondaryMessage={null}
-      value={value}
-      actionButton={
-        <Text baseline={false}>
-          <IconChevron />
-        </Text>
-      }
-    >
-      {(overlays, fieldProps, actionButton, icon) => (
-        <Fragment>
-          {icon}
-          <Box
-            component="select"
-            value={value}
-            defaultValue={typeof value === 'undefined' ? '' : undefined}
-            onChange={onChange}
-            onBlur={onBlur}
-            onFocus={onFocus}
-            placeholder={placeholder}
-            {...fieldProps}
-            ref={ref}
-          >
-            <option value="" disabled={true}>
-              {placeholder}
-            </option>
-            <Fragment>{children}</Fragment>
-          </Box>
-          {overlays}
-          <Box pointerEvents="none">{actionButton}</Box>
-        </Fragment>
-      )}
-    </Field>
-  ),
+  (props, ref) => {
+    const {
+      children,
+      value,
+      onChange,
+      onBlur,
+      onFocus,
+      placeholder,
+      ...restProps
+    } = props;
+
+    return (
+      <Field
+        {...restProps}
+        labelId={undefined}
+        secondaryMessage={null}
+        value={value}
+        actionButton={
+          <Text baseline={false}>
+            <IconChevron />
+          </Text>
+        }
+      >
+        {(overlays, fieldProps, actionButton, icon) => (
+          <Fragment>
+            {icon}
+            <Box
+              component="select"
+              value={value}
+              defaultValue={typeof value === 'undefined' ? '' : undefined}
+              onChange={onChange}
+              onBlur={onBlur}
+              onFocus={onFocus}
+              placeholder={placeholder}
+              {...fieldProps}
+              ref={ref}
+            >
+              <option value="" disabled={true}>
+                {placeholder}
+              </option>
+              {children}
+            </Box>
+            {overlays}
+            <Box pointerEvents="none">{actionButton}</Box>
+          </Fragment>
+        )}
+      </Field>
+    );
+  },
 );
 
 NamedDropdown.displayName = 'Dropdown';
