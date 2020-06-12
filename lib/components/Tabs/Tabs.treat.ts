7bc019effa28e2639f8e6655f493c920988b7f7b
Matt Jones <mattjones701@gmail.com>
2020-06-12 13:35:58 +1000

    Hover styling

--- lib/components/Tabs/Tabs.treat.ts
+++ lib/components/Tabs/Tabs.treat.ts
@@ -1,7 +1,18 @@
 import { style, styleMap } from 'sku/treat';
 
+export const tab = style({});
+
 export const tabFocusRing = style({
   zIndex: 1,
+});
+
+export const tabHover = style({
+  opacity: 0,
+  selectors: {
+    [`${tab}:hover &`]: {
+      opacity: 1,
+    },
+  },
 });
 
 export const tabPanel = style({});
@@ -17,10 +28,12 @@ export const tabPanelFocusRing = style({
 
 export const divider = styleMap((theme) => ({
   horizontal: {
+    zIndex: 1,
     background: theme.border.color.standard,
     height: theme.border.width.standard,
   },
   vertical: {
+    zIndex: 1,
     background: theme.border.color.standard,
     width: theme.border.width.standard,
   },
