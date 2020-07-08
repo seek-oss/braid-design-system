462ac95ee37d2c80a3a987e620afdb0e8d2be512
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-07-08 12:06:21 +1000

    Remove Card from Tag examples

--- lib/components/Tag/Tag.docs.tsx
+++ lib/components/Tag/Tag.docs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentDocs } from '../../../site/src/types';
-import { Tag, Card, Inline } from '../';
+import { Tag, Inline } from '../';
 
 const docs: ComponentDocs = {
   category: 'Content',
@@ -9,49 +9,43 @@ const docs: ComponentDocs = {
   examples: [
     {
       label: 'Standard Tag',
-      Example: () => (
-        <Card>
-          <Tag>Tag</Tag>
-        </Card>
-      ),
+      background: 'card',
+      Example: () => <Tag>Tag</Tag>,
     },
     {
       label: 'Clearable Tag',
+      background: 'card',
       Example: ({ handler }) => (
-        <Card>
-          <Tag onClear={handler} clearLabel="Clear tag">
-            Tag
-          </Tag>
-        </Card>
+        <Tag onClear={handler} clearLabel="Clear tag">
+          Tag
+        </Tag>
       ),
     },
     {
       label: 'Truncated Tag',
       docsSite: false,
+      background: 'card',
       Example: ({ handler }) => (
-        <Card>
-          <Tag onClear={handler} clearLabel="Clear tag">
-            The quick brown fox jumps over the lazy dog. The quick brown fox
-            jumps over the lazy dog. The quick brown fox jumps over the lazy
-            dog. The quick brown fox jumps over the lazy dog. The quick brown
-            fox jumps over the lazy dog. The quick brown fox jumps over the lazy
-            dog. The quick brown fox jumps over the lazy dog.
-          </Tag>
-        </Card>
+        <Tag onClear={handler} clearLabel="Clear tag">
+          The quick brown fox jumps over the lazy dog. The quick brown fox jumps
+          over the lazy dog. The quick brown fox jumps over the lazy dog. The
+          quick brown fox jumps over the lazy dog. The quick brown fox jumps
+          over the lazy dog. The quick brown fox jumps over the lazy dog. The
+          quick brown fox jumps over the lazy dog.
+        </Tag>
       ),
     },
     {
       label: 'Test: Standard and clearable tags should be equal height',
       docsSite: false,
+      background: 'card',
       Example: ({ handler }) => (
-        <Card>
-          <Inline space="small">
-            <Tag>Tag</Tag>
-            <Tag onClear={handler} clearLabel="Clear tag">
-              Tag
-            </Tag>
-          </Inline>
-        </Card>
+        <Inline space="small">
+          <Tag>Tag</Tag>
+          <Tag onClear={handler} clearLabel="Clear tag">
+            Tag
+          </Tag>
+        </Inline>
       ),
     },
   ],
