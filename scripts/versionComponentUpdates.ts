66b2711fc0ee7e7e7e93c537f4729fe6a5d25748
mattcompiles <mattjones701@gmail.com>
2020-08-13 11:23:41 +1000

    Add custom frontmatter support to changesets (#720)

---
+++ scripts/versionComponentUpdates.ts
@@ -0,0 +1,30 @@
+import fs from 'fs-extra';
+import glob from 'fast-glob';
+
+import { version } from '../package.json';
+
+(async () => {
+  const componentUpdateFiles = await glob('.changeset/*-data.json', {
+    absolute: true,
+  });
+
+  const updates = [];
+
+  for (const updateFile of componentUpdateFiles) {
+    const update = await fs.readJSON(updateFile);
+
+    updates.push(update);
+
+    await fs.remove(updateFile);
+  }
+
+  const componentUpdatesFile = require.resolve(
+    '../site/src/componentUpdates.json',
+  );
+
+  const existingComponentUpdates = await fs.readJSON(componentUpdatesFile);
+
+  existingComponentUpdates.push({ version, updates });
+
+  await fs.writeJSON(componentUpdatesFile, existingComponentUpdates);
+})();
