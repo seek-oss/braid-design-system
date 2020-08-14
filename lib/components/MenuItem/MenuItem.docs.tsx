5c5a9c224579e4634f5a516f173de314364d2749
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-08-14 14:08:03 +1000

    Mark MenuItem page as new

--- lib/components/MenuItem/MenuItem.docs.tsx
+++ lib/components/MenuItem/MenuItem.docs.tsx
@@ -15,6 +15,7 @@ import {
 const docs: ComponentDocs = {
   category: 'Content',
   subComponents: ['MenuItemLink'],
+  added: new Date('14 August 2020'),
   screenshotWidths: [],
   migrationGuide: true,
   description: (
