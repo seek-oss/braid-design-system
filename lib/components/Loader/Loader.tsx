aa50273e8ee293d6364204c17afefcd2d542ffef
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-08-14 15:31:08 +1000

    Loader: Fix rendering issues due to browser rounding errors (#728)

--- lib/components/Loader/Loader.tsx
+++ lib/components/Loader/Loader.tsx
@@ -7,7 +7,7 @@ import * as styleRefs from './Loader.treat';
 const indicators = [...Array(3)];
 
 interface LoaderProps {
-  size?: keyof typeof styleRefs.size;
+  size?: keyof typeof styleRefs.rootSize;
   delayVisibility?: boolean;
 }
 
@@ -19,13 +19,19 @@ export const Loader = ({
   const parentBackgroundColor = useBackgroundLightness();
 
   return (
-    <Box display="flex" className={delayVisibility ? styles.delay : undefined}>
+    <Box
+      display="flex"
+      className={[
+        styles.rootSize[size],
+        delayVisibility ? styles.delay : undefined,
+      ]}
+    >
       {indicators.map((_, index) => (
         <Box
           key={index}
           borderRadius="full"
           background={parentBackgroundColor === 'dark' ? 'card' : 'neutral'}
-          className={[styles.indicator, styles.size[size]]}
+          className={[styles.circle, styles.circleSize[size]]}
         />
       ))}
     </Box>
