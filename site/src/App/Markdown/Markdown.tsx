aaa6576c4cd27cdfd1c2fc8a647316835b3e4a1f
Matt Jones <mattjones701@gmail.com>
2020-08-18 13:50:40 +1000

    Fix ListItem rendering in Markdown

--- site/src/App/Markdown/Markdown.tsx
+++ site/src/App/Markdown/Markdown.tsx
@@ -17,14 +17,29 @@ const Code = ({ language, value }: { language: string; value: string }) => (
   <CodeBlock language={language}>{value}</CodeBlock>
 );
 
+const paragraph = ({ children }: any) => <Text component="p">{children}</Text>;
+
 const renderers = {
   root: ({ children }: any) => <Stack space="large">{children}</Stack>,
   heading: ({ level, children }: any) => (
     <Heading level={level}>{children}</Heading>
   ),
-  paragraph: Text,
+  paragraph,
   list: List,
-  listItem: Text,
+  listItem: ({ children }: { children: any }) => {
+    const childList = React.Children.toArray(children);
+
+    if (childList.length > 1) {
+      return <Stack space="medium">{children}</Stack>;
+    }
+
+    // @ts-expect-error
+    if (childList[0]?.type?.name === 'paragraph') {
+      return children;
+    }
+
+    return <Text>{children}</Text>;
+  },
   strong: Strong,
   emphasis: Strong,
   inlineCode: ({ children }: any) => (
