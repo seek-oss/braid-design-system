d3374722c9143fe05232097b9993ea8a9d3e0fb1
Matt Jones <mattjones701@gmail.com>
2020-09-11 15:27:45 +1000

    Move back to protal implementation

--- lib/components/Announcement/Announcement.tsx
+++ lib/components/Announcement/Announcement.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { useStyles } from 'sku/react-treat';
-import { Box } from '../Box/Box';
 
+import { useBoxStyles } from '../Box/useBoxStyles';
 import * as styleRefs from '../HiddenVisually/HiddenVisually.treat';
+
+let announcementCounter = 0;
 
 interface AnnouncementProps {
   children: string | undefined | null;
 }
 
 export const Announcement = ({ children }: AnnouncementProps) => {
+  const [announcementElement, setElement] = useState<HTMLElement | null>(null);
   const styles = useStyles(styleRefs);
+  const className = useBoxStyles({
+    component: 'div',
+    position: 'absolute',
+    overflow: 'hidden',
+    className: styles.root,
+  });
 
-  return (
-    <Box
-      position="absolute"
-      overflow="hidden"
-      aria-live="polite"
-      aria-atomic="true"
-      className={styles.root}
-    >
-      {children}
-    </Box>
-  );
+  useEffect(() => {
+    const announcementContainerId = `braid-announcement-container-${announcementCounter++}`;
+
+    const element = document.createElement('div');
+    element.setAttribute('id', announcementContainerId);
+    element.setAttribute('class', className);
+    element.setAttribute('aria-live', 'polite');
+    element.setAttribute('aria-atomic', 'true');
+
+    document.body.appendChild(element);
+
+    setElement(element);
+
+    return () => {
+      document.body.removeChild(element);
+    };
+  }, [className]);
+
+  if (!announcementElement) {
+    return null;
+  }
+
+  return createPortal(children, announcementElement);
 };
