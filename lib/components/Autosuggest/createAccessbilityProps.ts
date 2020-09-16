36306e31687fd71ee726d5724c7555e125ee482e
Matt Jones <mattjones701@gmail.com>
2020-09-16 12:36:25 +1000

    Fix typo

--- lib/components/Autosuggest/createAccessbilityProps.ts
+++ lib/components/Autosuggest/createAccessbilityProps.ts
@@ -12,7 +12,7 @@ export const createAccessbilityProps = ({
 }: AutosuggestProps) => {
   const menuId = `${id}-menu`;
   const labelId = `${id}-label`;
-  const assistiveDecriptionId = `${id}-aria-description`;
+  const assistiveDescriptionId = `${id}-aria-description`;
 
   return {
     labelProps: {
@@ -31,12 +31,12 @@ export const createAccessbilityProps = ({
         typeof highlightedIndex === 'number'
           ? getItemId(id, highlightedIndex)
           : undefined,
-      'aria-describedby': assistiveDecriptionId,
+      'aria-describedby': assistiveDescriptionId,
       autoComplete: 'off', // https://stackoverflow.com/questions/47775041/disable-autofill-in-chrome-63/47822599
       spellCheck: false,
     } as const,
     assistiveDescriptionProps: {
-      id: assistiveDecriptionId,
+      id: assistiveDescriptionId,
     } as const,
     menuProps: {
       id: menuId,
