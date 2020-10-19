5b4bb78b39e78fc88b7fe06d0df2c300a390a9c5
Michael Taranto <mtaranto@seek.com.au>
2020-10-20 10:46:57 +1100

    Apply aria-describedby only when needed

---
+++ lib/components/Radio/Radio.test.tsx
@@ -0,0 +1,59 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BraidTestProvider, Radio } from '..';
+
+describe('Radio', () => {
+  it('associates field with label correctly', () => {
+    const { getByLabelText } = render(
+      <BraidTestProvider>
+        <Radio
+          id="field"
+          label="My field"
+          value=""
+          onChange={() => {}}
+          checked={false}
+        />
+      </BraidTestProvider>,
+    );
+
+    expect(getByLabelText('My field').tagName).toBe('INPUT');
+  });
+
+  it('associates field with description correctly', () => {
+    const { getByLabelText } = render(
+      <BraidTestProvider>
+        <Radio
+          id="field"
+          label="My field"
+          description="More detail about field"
+          value=""
+          onChange={() => {}}
+          checked={false}
+        />
+      </BraidTestProvider>,
+    );
+
+    expect(getByLabelText('My field')).toHaveDescription(
+      'More detail about field',
+    );
+  });
+
+  it('field is not marked as having a description without a message or description', () => {
+    const { getByLabelText } = render(
+      <BraidTestProvider>
+        <Radio
+          id="field"
+          label="My field"
+          value=""
+          onChange={() => {}}
+          checked={false}
+        />
+      </BraidTestProvider>,
+    );
+
+    expect(
+      getByLabelText('My field').getAttribute('aria-describedby'),
+    ).toBeNull();
+  });
+});
