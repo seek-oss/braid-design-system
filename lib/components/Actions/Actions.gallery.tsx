58706a2c4fca3dafe1ed29a268a7482e916273d9
Michael Taranto <mtaranto@seek.com.au>
2020-11-24 15:49:13 +1100

    Source the gallery examples

--- lib/components/Actions/Actions.gallery.tsx
+++ lib/components/Actions/Actions.gallery.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
+import source from '../../utils/source.macro';
 import { ComponentExample } from '../../../site/src/types';
 import { Actions, Button, TextLink } from '../';
 
 export const galleryItems: ComponentExample[] = [
   {
     label: 'Actions with Strong Button and TextLink',
-    Example: () => (
-      <Actions>
-        <Button weight="strong">Strong</Button>
-        <TextLink href="#">TextLink</TextLink>
-      </Actions>
-    ),
+    Example: () =>
+      source(
+        <Actions>
+          <Button weight="strong">Strong</Button>
+          <TextLink href="#">TextLink</TextLink>
+        </Actions>,
+      ),
   },
   {
     label: 'Actions with Regular Button and Weak Button',
-    Example: () => (
-      <Actions>
-        <Button weight="regular">Regular</Button>
-        <Button weight="weak">Weak</Button>
-      </Actions>
-    ),
+    Example: () =>
+      source(
+        <Actions>
+          <Button weight="regular">Regular</Button>
+          <Button weight="weak">Weak</Button>
+        </Actions>,
+      ),
   },
   {
     label: 'Actions with Weak Buttons and Regular Button',
-    Example: () => (
-      <Actions>
-        <Button weight="weak">Weak</Button>
-        <Button weight="weak">Weak</Button>
-        <Button weight="regular">Regular</Button>
-      </Actions>
-    ),
+    Example: () =>
+      source(
+        <Actions>
+          <Button weight="weak">Weak</Button>
+          <Button weight="weak">Weak</Button>
+          <Button weight="regular">Regular</Button>
+        </Actions>,
+      ),
   },
 ];
