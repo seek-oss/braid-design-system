6521dea0ce8b37ee072dfa80a4a44e25ade94c74
Michael Taranto <mtaranto@seek.com.au>
2020-11-25 09:36:48 +1100

    More sourcery

--- lib/components/Alert/Alert.snippets.tsx
+++ lib/components/Alert/Alert.snippets.tsx
@@ -6,42 +6,42 @@ import { Snippets } from '../private/Snippets';
 export const snippets: Snippets = [
   {
     name: 'Critical',
-    code: (
+    code: source(
       <Alert tone="critical">
         <Text>Critical Alert</Text>
-      </Alert>
+      </Alert>,
     ),
   },
   {
     name: 'Caution',
-    code: (
+    code: source(
       <Alert tone="caution">
         <Text>Caution Alert</Text>
-      </Alert>
+      </Alert>,
     ),
   },
   {
     name: 'Positive',
-    code: (
+    code: source(
       <Alert tone="positive">
         <Text>Positive Alert</Text>
-      </Alert>
+      </Alert>,
     ),
   },
   {
     name: 'Info',
-    code: (
+    code: source(
       <Alert tone="info">
         <Text>Info Alert</Text>
-      </Alert>
+      </Alert>,
     ),
   },
   {
     name: 'Promote',
-    code: (
+    code: source(
       <Alert tone="promote">
         <Text>Promote Alert</Text>
-      </Alert>
+      </Alert>,
     ),
   },
   {
