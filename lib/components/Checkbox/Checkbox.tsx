928cff92607eab31b90dc3d55649ed5f6ff28551
Michael Taranto <mtaranto@seek.com.au>
2020-11-26 08:27:58 +1100

    Refactor check state resolution function

--- lib/components/Checkbox/Checkbox.tsx
+++ lib/components/Checkbox/Checkbox.tsx
@@ -6,21 +6,7 @@ import {
 } from '../private/InlineField/InlineField';
 
 const resolveCheckedGroup = (values: Array<CheckboxChecked>) =>
-  values.reduce((acc, val, index) => {
-    if (acc === 'mixed') {
-      return 'mixed';
-    }
-
-    if (acc === true) {
-      return val || 'mixed';
-    }
-
-    if (acc === false && index > 0) {
-      return val ? 'mixed' : false;
-    }
-
-    return val;
-  }, false);
+  values.some((value) => value !== values[0]) ? 'mixed' : values[0] ?? false;
 
 export interface CheckboxProps extends Omit<InlineFieldProps, 'checked'> {
   checked: CheckboxChecked | Array<boolean>;
