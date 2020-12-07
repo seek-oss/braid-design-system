55865dbaf7737a03625af28c5d6e4789f56c9451
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-12-07 11:43:18 +1100

    Update Strong and Secondary docs

--- lib/components/Secondary/Secondary.docs.tsx
+++ lib/components/Secondary/Secondary.docs.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { ComponentDocs } from '../../../site/src/types';
-import { Secondary, Text } from '../';
+import source from '../../utils/source.macro';
+import { ComponentDetail } from '../../../site/src/types';
+import { Secondary, Text, TextLink } from '../';
 
-const docs: ComponentDocs = {
+const docs: ComponentDetail = {
   category: 'Content',
   migrationGuide: true,
-  examples: [
+  Example: () =>
+    source(
+      <Text size="large">
+        The last word of this sentence is <Secondary>secondary.</Secondary>
+      </Text>,
+    ),
+  alternatives: [
+    { name: 'Strong', description: 'For a stronger text treatment.' },
+  ],
+  additional: [
     {
-      Example: () => (
+      label: 'Note',
+      description: (
         <Text>
-          The word in the <Secondary>middle</Secondary> is secondary text.
+          This component must be nested within a{' '}
+          <TextLink href="/components/Text">Text</TextLink> or{' '}
+          <TextLink href="/components/Heading">Heading</TextLink> component.
         </Text>
       ),
     },
