d8a9ef759af0468f2a6b69f92de19c71c37e0898
Michael Taranto <mtaranto@seek.com.au>
2020-12-09 14:22:12 +1100

    Update Tag

--- lib/components/Tag/Tag.docs.tsx
+++ lib/components/Tag/Tag.docs.tsx
@@ -1,24 +1,67 @@
 import React from 'react';
-import { ComponentDocs } from '../../../site/src/types';
-import { Tag } from '../';
+import { ComponentDetail } from '../../../site/src/types';
+import { Card, Inline, Tag, Strong, Text, TextLinkButton } from '../';
+import source from '../../utils/source.macro';
 
-const docs: ComponentDocs = {
+const docs: ComponentDetail = {
   category: 'Content',
   migrationGuide: true,
-  examples: [
+  Example: () =>
+    source(
+      <Card>
+        <Inline space="small">
+          <Tag>One</Tag>
+          <Tag>Two</Tag>
+          <Tag>Three</Tag>
+        </Inline>
+      </Card>,
+    ),
+  alternatives: [{ name: 'Badge', description: 'For static labels.' }],
+  additional: [
     {
-      label: 'Standard Tag',
-      background: 'card',
-      Example: () => <Tag>Tag</Tag>,
-    },
-    {
-      label: 'Clearable Tag',
-      background: 'card',
-      Example: ({ handler }) => (
-        <Tag onClear={handler} clearLabel="Clear tag">
-          Tag
-        </Tag>
+      label: 'Clearable',
+      description: (
+        <Text>
+          Tags can be made clearable, by providing an <Strong>onClear</Strong>{' '}
+          handler and a <Strong>clearLabel</Strong> to describe what clicking it
+          will do.
+        </Text>
       ),
+      background: 'card',
+      Example: ({ getState, setState }) =>
+        source(
+          <Inline space="small" alignY="center">
+            <Tag>One</Tag>
+            {!getState('clearTwo') ? (
+              <Tag
+                onClear={() => setState('clearTwo', true)}
+                clearLabel={'Clear "Two"'}
+              >
+                Two
+              </Tag>
+            ) : null}
+            {!getState('clearThree') ? (
+              <Tag
+                onClear={() => setState('clearThree', true)}
+                clearLabel={'Clear "Three"'}
+              >
+                Three
+              </Tag>
+            ) : null}
+            <Text tone="secondary">
+              <TextLinkButton
+                weight="weak"
+                hitArea="large"
+                onClick={() => {
+                  setState('clearTwo', false);
+                  setState('clearThree', false);
+                }}
+              >
+                Reset
+              </TextLinkButton>
+            </Text>
+          </Inline>,
+        ),
     },
   ],
 };
--- lib/components/Tag/Tag.gallery.tsx
+++ lib/components/Tag/Tag.gallery.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { ComponentExample } from '../../../site/src/types';
-import { Tag } from '../';
+import { Tag, Inline, Text, TextLinkButton } from '../';
+import source from '../../utils/source.macro';
 
 export const galleryItems: ComponentExample[] = [
   {
-    label: 'Standard Tag',
+    label: 'Standard',
     background: 'card',
-    Example: () => <Tag>Tag</Tag>,
+    Example: () => source(<Tag>Tag</Tag>),
   },
   {
-    label: 'Clearable Tag',
+    label: 'Clearable',
     background: 'card',
-    Example: ({ handler }) => (
-      <Tag onClear={handler} clearLabel="Clear tag">
-        Tag
-      </Tag>
-    ),
+    Example: ({ getState, setState }) =>
+      source(
+        <Inline space="small" alignY="center">
+          <Tag>One</Tag>
+          {!getState('clearTwo') ? (
+            <Tag
+              onClear={() => setState('clearTwo', true)}
+              clearLabel={'Clear "Two"'}
+            >
+              Two
+            </Tag>
+          ) : null}
+          {!getState('clearThree') ? (
+            <Tag
+              onClear={() => setState('clearThree', true)}
+              clearLabel={'Clear "Three"'}
+            >
+              Three
+            </Tag>
+          ) : null}
+          <Text tone="secondary">
+            <TextLinkButton
+              weight="weak"
+              hitArea="large"
+              onClick={() => {
+                setState('clearTwo', false);
+                setState('clearThree', false);
+              }}
+            >
+              Reset
+            </TextLinkButton>
+          </Text>
+        </Inline>,
+      ),
   },
 ];
