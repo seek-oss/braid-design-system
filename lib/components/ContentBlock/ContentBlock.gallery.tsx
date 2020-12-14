7c473883f1d6ec69f597874bc1bed59d5a0d4f3b
Michael Taranto <mtaranto@seek.com.au>
2020-12-14 11:53:56 +1100

    Update ContentBlock

--- lib/components/ContentBlock/ContentBlock.gallery.tsx
+++ lib/components/ContentBlock/ContentBlock.gallery.tsx
@@ -2,46 +2,52 @@ import React from 'react';
 import { ComponentExample } from '../../../site/src/types';
 import { Placeholder } from '../private/Placeholder/Placeholder';
 import { ContentBlock } from '../';
+import source from '../../utils/source.macro';
 
 export const galleryItems: ComponentExample[] = [
   {
-    label: 'Default Content Block',
-    Example: () => (
-      <ContentBlock>
-        <Placeholder height={100} />
-      </ContentBlock>
-    ),
+    label: 'Default width',
+    Example: () =>
+      source(
+        <ContentBlock>
+          <Placeholder height={100} />
+        </ContentBlock>,
+      ),
   },
   {
-    label: 'Xsmall Content Block',
-    Example: () => (
-      <ContentBlock width="xsmall">
-        <Placeholder height={100} />
-      </ContentBlock>
-    ),
+    label: 'Xsmall width',
+    Example: () =>
+      source(
+        <ContentBlock width="xsmall">
+          <Placeholder height={100} />
+        </ContentBlock>,
+      ),
   },
   {
-    label: 'Small Content Block',
-    Example: () => (
-      <ContentBlock width="small">
-        <Placeholder height={100} />
-      </ContentBlock>
-    ),
+    label: 'Small width',
+    Example: () =>
+      source(
+        <ContentBlock width="small">
+          <Placeholder height={100} />
+        </ContentBlock>,
+      ),
   },
   {
-    label: 'Medium Content Block',
-    Example: () => (
-      <ContentBlock width="medium">
-        <Placeholder height={100} />
-      </ContentBlock>
-    ),
+    label: 'Medium width',
+    Example: () =>
+      source(
+        <ContentBlock width="medium">
+          <Placeholder height={100} />
+        </ContentBlock>,
+      ),
   },
   {
-    label: 'Large Content Block',
-    Example: () => (
-      <ContentBlock width="large">
-        <Placeholder height={100} />
-      </ContentBlock>
-    ),
+    label: 'Large width',
+    Example: () =>
+      source(
+        <ContentBlock width="large">
+          <Placeholder height={100} />
+        </ContentBlock>,
+      ),
   },
 ];
