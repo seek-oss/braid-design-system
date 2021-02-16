f2f118dedc87e231c9b297ac57c5267800158712
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2021-02-16 15:35:54 +1100

    Forward refs in Playroom wrappers

--- lib/components/Button/Button.playroom.tsx
+++ lib/components/Button/Button.playroom.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { buttonVariants } from '../ButtonRenderer/ButtonRenderer';
 import { Button as BraidButton, ButtonProps } from './Button';
 
-export const Button = ({ variant, ...restProps }: ButtonProps) => (
-  <BraidButton
-    variant={
-      variant && buttonVariants.indexOf(variant) > -1 ? variant : 'solid'
-    }
-    {...restProps}
-  />
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, ...restProps }, ref) => (
+    <BraidButton
+      ref={ref}
+      variant={
+        variant && buttonVariants.indexOf(variant) > -1 ? variant : 'solid'
+      }
+      {...restProps}
+    />
+  ),
 );
+
+Button.displayName = 'Button';
--- lib/components/ButtonLink/ButtonLink.playroom.tsx
+++ lib/components/ButtonLink/ButtonLink.playroom.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { buttonVariants } from '../ButtonRenderer/ButtonRenderer';
 import { ButtonLink as BraidButtonLink, ButtonLinkProps } from './ButtonLink';
 
-export const ButtonLink = ({
-  href,
-  onClick,
-  variant,
-  ...restProps
-}: ButtonLinkProps) => (
-  <BraidButtonLink
-    href={href ?? ''}
-    onClick={onClick ? onClick : (event) => event?.preventDefault()}
-    variant={
-      variant && buttonVariants.indexOf(variant) > -1 ? variant : 'solid'
-    }
-    {...restProps}
-  />
+export const ButtonLink = forwardRef<HTMLAnchorElement, ButtonLinkProps>(
+  ({ href, onClick, variant, ...restProps }, ref) => (
+    <BraidButtonLink
+      ref={ref}
+      href={href ?? ''}
+      onClick={onClick ? onClick : (event) => event?.preventDefault()}
+      variant={
+        variant && buttonVariants.indexOf(variant) > -1 ? variant : 'solid'
+      }
+      {...restProps}
+    />
+  ),
 );
+
+ButtonLink.displayName = 'ButtonLink';
