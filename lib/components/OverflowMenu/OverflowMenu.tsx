c9a6c8e71e2801641fb3911462f40dac7187f6ce
Michael Taranto <mtaranto@seek.com.au>
2021-03-19 11:29:34 +1100

    More migrations

--- lib/components/OverflowMenu/OverflowMenu.tsx
+++ lib/components/OverflowMenu/OverflowMenu.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useStyles } from 'sku/react-treat';
 import { MenuRenderer, MenuRendererProps } from '../MenuRenderer/MenuRenderer';
 import { OverflowButton } from '../iconButtons/OverflowButton/OverflowButton';
 import { Box } from '../Box/Box';
-import * as styleRefs from './OverflowMenu.treat';
+import * as styles from './OverflowMenu.css';
 
 interface OverflowMenuProps
   extends Omit<MenuRendererProps, 'trigger' | 'align' | 'offsetSpace'> {
@@ -14,21 +13,17 @@ export const OverflowMenu = ({
   label,
   children,
   ...menuProps
-}: OverflowMenuProps) => {
-  const styles = useStyles(styleRefs);
-
-  return (
-    <MenuRenderer
-      trigger={(triggerProps, { open }) => (
-        <Box className={styles.triggerOffset}>
-          <OverflowButton label={label} active={open} {...triggerProps} />
-        </Box>
-      )}
-      align="right"
-      offsetSpace="small"
-      {...menuProps}
-    >
-      {children}
-    </MenuRenderer>
-  );
-};
+}: OverflowMenuProps) => (
+  <MenuRenderer
+    trigger={(triggerProps, { open }) => (
+      <Box className={styles.triggerOffset}>
+        <OverflowButton label={label} active={open} {...triggerProps} />
+      </Box>
+    )}
+    align="right"
+    offsetSpace="small"
+    {...menuProps}
+  >
+    {children}
+  </MenuRenderer>
+);
