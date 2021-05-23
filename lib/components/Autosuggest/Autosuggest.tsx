1d73f4af3ddb127b1d48e54c5cac809334e693c2
mattcompiles <mattjones701@gmail.com>
2021-05-24 09:35:12 +1000

    Autosuggest: Fix missing/invalid group headings in some cases (#937)

--- lib/components/Autosuggest/Autosuggest.tsx
+++ lib/components/Autosuggest/Autosuggest.tsx
@@ -197,6 +197,11 @@ function GroupHeading({ children }: GroupHeadingProps) {
           tone: 'formAccent',
         }),
       ]}
+      data-testid={
+        process.env.NODE_ENV !== 'production'
+          ? `group-heading-${children}`
+          : undefined
+      }
     >
       {children}
     </Box>
@@ -217,9 +222,9 @@ function normaliseSuggestions<Value>(
       item.suggestions.forEach((suggestion) => {
         groupHeadingForSuggestion.set(suggestion, item.label);
       });
-      index += normalisedSuggestions.push(...item.suggestions);
+      index = normalisedSuggestions.push(...item.suggestions);
     } else {
-      index += normalisedSuggestions.push(item);
+      index = normalisedSuggestions.push(item);
     }
   }
 
