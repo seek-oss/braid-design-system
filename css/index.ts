93cc53b57fd4fdaa953aa5a3d398c58a2036eef9
Matt Jones <mattjones701@gmail.com>
2021-06-03 08:40:30 +1000

    Remove background from public atoms API

--- css/index.ts
+++ css/index.ts
@@ -1,4 +1,5 @@
 import { vars as internalVars } from '../lib/themes/vars.css';
+import { atoms as internalAtoms, Atoms } from '../lib/atoms/atoms';
 
 const {
   grid,
@@ -24,7 +25,18 @@ export const vars = {
   borderWidth,
 };
 
-export { atoms } from '../lib/atoms/atoms';
+export function atoms(props: Omit<Atoms, 'background'>) {
+  if (process.env.NODE_ENV !== 'production') {
+    if ('background' in props) {
+      throw new Error(
+        `'background' is not available via 'atoms'. This is because Braid uses React context to dynamically apply matching tones to child elements. Please use \`<Box background="...">\` instead.`,
+      );
+    }
+  }
+
+  return internalAtoms(props);
+}
+
 export {
   breakpoints,
   Breakpoint,
