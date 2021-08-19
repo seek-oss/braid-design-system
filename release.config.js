ac7b409c883ef4ac397dfb86319a0d18d8d602ba
Michael Taranto <michaeltaranto@users.noreply.github.com>
2021-08-20 09:29:36 +1000

    Remove unused semantic-release config (#988)

--- release.config.js
+++
@@ -1,7 +0,0 @@
-// semantic-release configuration
-
-module.exports = {
-  // Avoid all default success plugins to avoid commenting on PRs
-  // https://github.com/semantic-release/semantic-release/blob/caribou/docs/usage/configuration.md#success
-  success: false,
-};
