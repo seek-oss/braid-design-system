3fd8f5a0e0760454423c9fe66d904938d45ff274
Ben <bjervis@seek.com.au>
2021-08-23 13:59:24 +1000

    Remove troublesome imports

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -5,9 +5,7 @@ export {
 import { LinkComponent as _LinkComponent } from './BraidProvider/BraidProvider';
 export type LinkComponent = _LinkComponent;
 
-export { BraidLoadableProvider } from './BraidLoadableProvider/BraidLoadableProvider';
 export { BraidPortal } from './BraidPortal/BraidPortal';
-export { BraidTestProvider } from './BraidTestProvider/BraidTestProvider';
 export { ThemeNameConsumer } from './ThemeNameConsumer/ThemeNameConsumer';
 export { useThemeName } from './useThemeName/useThemeName';
 export { useSpace } from './useSpace/useSpace';
