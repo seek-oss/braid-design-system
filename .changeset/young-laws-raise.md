c2835f3c2fc1fd82f75134ca44fabbd62d2ffd30
Michael Taranto <michaeltaranto@users.noreply.github.com>
2021-12-14 15:23:44 +1100

    Update experimental color mode flags (#1050)

---
+++ .changeset/young-laws-raise.md
@@ -0,0 +1,5 @@
+---
+'braid-design-system': patch
+---
+
+Update experimental color mode flags
--- color-mode/index.ts
+++ color-mode/index.ts
@@ -1,9 +1,13 @@
 import { darkMode } from '../lib/css/atoms/sprinkles.css';
-const flag = '_bdsdm=1';
+const flag = '_bdsdm';
+// VALUES
+// 0 = light mode
+// 1 = dark mode
+// 2 = OS Preference
 // TODO: COLORMODE RELEASE
 // Finalise import contract
-export const __experimentalDarkMode__ = `
-<script>
-((l,p)=>{try{r=new RegExp(\`[\?&]\${p}\`);if(r.test(l.search)){history.replaceState(null,null,l.pathname+l.search.replace(r,'').replace(/^&/,'?')+l.hash);if(matchMedia('(prefers-color-scheme:dark)').matches)document.documentElement.classList.add('${darkMode}')}}catch(e){}})(location,'${flag}')
-</script>
-`;
+export const __experimentalDarkMode__ = [
+  '<script>',
+  `((l)=>{try{r=/[?&]${flag}=(\\d)/;[,s]=l.search.match(r)||[];if(s){history.replaceState(null,'',l.pathname+l.search.replace(r,'').replace(/^&/,'?')+l.hash);if(s==1||(s==2&&matchMedia('(prefers-color-scheme:dark)').matches))document.documentElement.classList.add('${darkMode}')}}catch(e){}})(location)`,
+  '</script>',
+].join('');
