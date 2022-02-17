a0579740e6a5cc195e23a2870c31955b5130e73a
Michael Taranto <michaeltaranto@users.noreply.github.com>
2022-02-18 08:31:52 +1100

    Drawer: Support positioning on the left (#1067)

--- lib/components/Drawer/Drawer.tsx
+++ lib/components/Drawer/Drawer.tsx
@@ -5,35 +5,63 @@ import { ModalContent, ModalContentProps } from '../private/Modal/ModalContent';
 export { AllowCloseContext } from '../private/Modal/Modal';
 
 const validWidths = ['small', 'medium', 'large'] as const;
+const validPositions = ['left', 'right'] as const;
 
 const defaultWidth = 'medium';
+const defaultPosition = 'right';
 const modalStyle = {
-  position: 'right',
   headingLevel: '2',
   illustration: undefined,
 } as const;
 
 export interface DrawerProps
-  extends Omit<ModalProps, keyof typeof modalStyle | 'width'> {
+  extends Omit<ModalProps, keyof typeof modalStyle | 'width' | 'position'> {
   width?: typeof validWidths[number];
+  position?: typeof validPositions[number];
 }
 
-export const Drawer = ({ width = defaultWidth, ...restProps }: DrawerProps) => {
+export const Drawer = ({
+  width = defaultWidth,
+  position = defaultPosition,
+  ...restProps
+}: DrawerProps) => {
   assert(validWidths.indexOf(width) >= 0, `Invalid width: ${width}`);
+  assert(
+    validPositions.indexOf(position) >= 0,
+    `Invalid position: ${position}`,
+  );
 
-  return <Modal width={width} {...restProps} {...modalStyle} />;
+  return (
+    <Modal width={width} position={position} {...restProps} {...modalStyle} />
+  );
 };
 
 interface DrawerContentProps
-  extends Omit<ModalContentProps, keyof typeof modalStyle | 'width'> {
+  extends Omit<
+    ModalContentProps,
+    keyof typeof modalStyle | 'width' | 'position'
+  > {
   width?: typeof validWidths[number];
+  position?: typeof validPositions[number];
 }
 
 export const DrawerContent = ({
   width = defaultWidth,
+  position = defaultPosition,
   ...restProps
 }: DrawerContentProps) => {
   assert(validWidths.indexOf(width) >= 0, `Invalid width: ${width}`);
+  assert(
+    validPositions.indexOf(position) >= 0,
+    `Invalid position: ${position}`,
+  );
 
-  return <ModalContent width={width} {...restProps} {...modalStyle} />;
+  return (
+    <ModalContent
+      width={width}
+      position={position}
+      {...restProps}
+      {...modalStyle}
+    />
+  );
 };
