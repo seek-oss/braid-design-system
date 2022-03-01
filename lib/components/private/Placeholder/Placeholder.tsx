061ff8a62897cfef45beb4aaff6d15c235b128d4
Michael Taranto <mtaranto@seek.com.au>
2022-03-01 13:31:53 +1100

    Placeholder: Document and add support for images

--- lib/components/private/Placeholder/Placeholder.tsx
+++ lib/components/private/Placeholder/Placeholder.tsx
@@ -10,16 +10,54 @@ export interface PlaceholderProps {
   width?: string | number;
   label?: ReactNode;
   shape?: 'rectangle' | 'round';
+  image?: string;
+  imageSize?: string;
 }
 
 const resolveToPxIfUnitless = (value: string | number) =>
   typeof value === 'string' && /[0-9]$/.test(value) ? `${value}px` : value;
+
+const PlaceholderContent = ({
+  label,
+  image,
+}: Pick<PlaceholderProps, 'label' | 'image'>) => {
+  if (image) {
+    return null;
+  }
+
+  if (label) {
+    return (
+      <Box paddingX="xsmall" paddingY="xxsmall">
+        <Text size="small" weight="strong" align="center" baseline={false}>
+          <Box className={styles.label}>{label}</Box>
+        </Text>
+      </Box>
+    );
+  }
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={atoms({
+        reset: 'svg',
+        position: 'absolute',
+        width: 'full',
+        height: 'full',
+      })}
+    >
+      <line className={styles.line} x1={0} y1={0} x2="100%" y2="100%" />
+      <line className={styles.line} x1="100%" y1={0} x2={0} y2="100%" />
+    </svg>
+  );
+};
 
 export const Placeholder = ({
   label,
   width = 'auto',
   height = 120,
   shape = 'rectangle',
+  image,
+  imageSize = 'cover',
 }: PlaceholderProps) => {
   const lightness = useBackgroundLightness();
 
@@ -35,33 +73,16 @@ export const Placeholder = ({
         wireframe.vanillaTheme,
         styles.lightTheme[lightness.lightMode],
         styles.darkTheme[lightness.darkMode],
-        styles.box,
+        !image ? styles.box : undefined,
       ]}
       style={{
         width: resolveToPxIfUnitless(width),
         height: resolveToPxIfUnitless(height),
+        background: image ? `url(${image}) no-repeat center center` : undefined,
+        backgroundSize: imageSize,
       }}
     >
-      {label ? (
-        <Box paddingX="xsmall" paddingY="xxsmall">
-          <Text size="small" weight="strong" align="center" baseline={false}>
-            <Box className={styles.label}>{label}</Box>
-          </Text>
-        </Box>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={atoms({
-            reset: 'svg',
-            position: 'absolute',
-            width: 'full',
-            height: 'full',
-          })}
-        >
-          <line className={styles.line} x1={0} y1={0} x2="100%" y2="100%" />
-          <line className={styles.line} x1="100%" y1={0} x2={0} y2="100%" />
-        </svg>
-      )}
+      <PlaceholderContent label={label} image={image} />
     </Box>
   );
 };
