7b5cfce036293482ef6a997578e4acd05e01f876
askoufis <askoufis@seek.com.au>
2022-08-10 14:50:23 +1000

    Get playroom working

--- site/playroom.config.js
+++ site/playroom.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const SkuWebpackPlugin = require('sku/webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const browserslist = require('./browserslist');
+const browserslist = require('../browserslist');
 
 module.exports = {
-  components: './lib/playroom/components.ts',
-  outputPath: './site/dist/playroom',
+  components: '../packages/braid-design-system/lib/playroom/components.ts',
+  outputPath: './dist/playroom',
   title: 'BRAID',
-  snippets: './lib/playroom/snippets.ts',
-  themes: './lib/themes/index.ts',
-  frameComponent: './lib/playroom/FrameComponent.tsx',
-  scope: './lib/playroom/useScope.ts',
-  typeScriptFiles: ['lib/**/*.{ts,tsx}', '!**/node_modules'],
+  snippets: '../packages/braid-design-system/lib/playroom/snippets.ts',
+  themes: '../packages/braid-design-system/lib/themes/index.ts',
+  frameComponent:
+    '../packages/braid-design-system/lib/playroom/FrameComponent.tsx',
+  scope: '../packages/braid-design-system/lib/playroom/useScope.ts',
+  typeScriptFiles: [
+    '../packages/braid-design-system/lib/**/*.{ts,tsx}',
+    '!**/node_modules',
+  ],
   widths: [320, 768, 1024, 1400],
   openBrowser: false,
   port: 8082,
