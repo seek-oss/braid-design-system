c4bffd9ef09d9d41be0d0fb789029bff3535da43
Remus Mate <rmate@seek.com.au>
2022-11-16 16:29:18 +1100

    rewrite default imports

--- packages/codemod/src/plugin-deprecate/plugin-import-update.test.ts
+++ packages/codemod/src/plugin-deprecate/plugin-import-update.test.ts
@@ -6,32 +6,32 @@ import plugin from './plugin-import-update';
 const tests: Parameters<typeof pluginTester>[0]['tests'] = [
   {
     title: 'Updates component import path with single import',
-    code: dedent`
+    code: dedent/* ts */ `
       import { BraidTestProvider } from 'braid-design-system';
       import { foo } from 'other-package';
     `,
-    output: dedent`
+    output: dedent/* ts */ `
       import { BraidTestProvider } from 'braid-design-system/test';
       import { foo } from 'other-package';
     `,
   },
   {
     title: 'Skip matching component import from non-Braid package',
-    code: dedent`
+    code: dedent/* ts */ `
       import { BraidTestProvider } from 'other-package';
     `,
-    output: dedent`
+    output: dedent/* ts */ `
       import { BraidTestProvider } from 'other-package';
     `,
   },
   {
     title:
       'Moves component to separate import when multiple things are imported',
-    code: dedent`
+    code: dedent/* ts */ `
       import { vars, BraidTestProvider } from 'braid-design-system';
       import { foo } from 'other-package';
     `,
-    output: dedent`
+    output: dedent/* ts */ `
       import { vars } from 'braid-design-system';
       import { BraidTestProvider } from 'braid-design-system/test';
       import { foo } from 'other-package';
@@ -39,14 +39,31 @@ const tests: Parameters<typeof pluginTester>[0]['tests'] = [
   },
   {
     title: 'Handle import aliasing of component',
-    code: dedent`
+    code: dedent/* ts */ `
       import { vars, BraidTestProvider as TestProvider } from 'braid-design-system';
       import { foo } from 'other-package';
     `,
-    output: dedent`
+    output: dedent/* ts */ `
       import { vars } from 'braid-design-system';
       import { BraidTestProvider as TestProvider } from 'braid-design-system/test';
       import { foo } from 'other-package';
+    `,
+  },
+  {
+    title: 'Updates Playroom imports',
+    code: dedent/* ts */ `
+      import { foo } from 'some-package';
+      import FrameComponent from 'braid-design-system/lib/playroom/FrameComponent';
+      import components from 'braid-design-system/lib/playroom/components';
+      import snippets from 'braid-design-system/lib/playroom/snippets';
+      import { bar } from 'other-package';
+    `,
+    output: dedent/* ts */ `
+      import { foo } from 'some-package';
+      import FrameComponent from 'braid-design-system/playroom/FrameComponent';
+      import components from 'braid-design-system/playroom/components';
+      import snippets from 'braid-design-system/playroom/snippets';
+      import { bar } from 'other-package';
     `,
   },
 ];
--- packages/codemod/src/plugin-deprecate/plugin-import-update.ts
+++ packages/codemod/src/plugin-deprecate/plugin-import-update.ts
@@ -3,8 +3,16 @@ import { types as t } from '@babel/core';
 
 const packageName = 'braid-design-system';
 
-const relocationMap = {
-  BraidTestProvider: `${packageName}/test`,
+const relocationMaps = {
+  [`${packageName}$`]: {
+    BraidTestProvider: `${packageName}/test`,
+  },
+  [`${packageName}/lib/playroom/.*`]: {
+    FrameComponent: `${packageName}/playroom/FrameComponent`,
+    components: `${packageName}/playroom/components`,
+    snippets: `${packageName}/playroom/snippets`,
+    useScope: `${packageName}/playroom/useScope`,
+  },
 } as const;
 
 export default function (): PluginObj<PluginPass> {
@@ -18,42 +26,55 @@ export default function (): PluginObj<PluginPass> {
         enter(path) {
           const bodyPath = path.get('body');
 
-          for (const statement of bodyPath) {
-            if (
-              t.isImportDeclaration(statement.node) &&
-              /braid-design-system$/.test(statement.node.source.value)
-            ) {
-              const relocations = Object.keys(relocationMap);
+          for (const [matcher, relocationMap] of Object.entries(
+            relocationMaps,
+          )) {
+            for (const statement of bodyPath) {
+              if (
+                t.isImportDeclaration(statement.node) &&
+                new RegExp(matcher).test(statement.node.source.value)
+              ) {
+                const relocations = Object.keys(relocationMap);
 
-              statement.node.specifiers = statement.node.specifiers.filter(
-                (specifier) => {
-                  if (
-                    t.isImportSpecifier(specifier) &&
-                    t.isIdentifier(specifier.imported) &&
-                    relocations.includes(specifier.imported.name)
-                  ) {
-                    statement.insertAfter(
-                      t.importDeclaration(
-                        [specifier],
-                        t.stringLiteral(
-                          relocationMap[
-                            specifier.imported
-                              .name as keyof typeof relocationMap
-                          ],
+                statement.node.specifiers = statement.node.specifiers.filter(
+                  (specifier) => {
+                    const rewriteImport = (name: string) => {
+                      statement.insertAfter(
+                        t.importDeclaration(
+                          [specifier],
+                          t.stringLiteral(
+                            relocationMap[name as keyof typeof relocationMap]!,
+                          ),
                         ),
-                      ),
-                    );
-                    // @ts-expect-error
-                    this.file.metadata.hasChanged = true;
+                      );
+                      // @ts-expect-error
+                      this.file.metadata.hasChanged = true;
 
-                    return false;
-                  }
-                  return true;
-                },
-              );
+                      return false;
+                    };
 
-              if (statement.node.specifiers.length === 0) {
-                statement.remove();
+                    if (
+                      t.isImportSpecifier(specifier) &&
+                      t.isIdentifier(specifier.imported) &&
+                      relocations.includes(specifier.imported.name)
+                    ) {
+                      return rewriteImport(specifier.imported.name);
+                    }
+
+                    if (
+                      t.isImportDefaultSpecifier(specifier) &&
+                      relocations.includes(specifier.local.name)
+                    ) {
+                      return rewriteImport(specifier.local.name);
+                    }
+
+                    return true;
+                  },
+                );
+
+                if (statement.node.specifiers.length === 0) {
+                  statement.remove();
+                }
               }
             }
           }
