52c4a9bb7349d353a9faeb8cee5c386d785cdb8b
Remus Mate <rmate@seek.com.au>
2022-11-18 15:45:26 +1100

    clean up some types/checks

--- packages/braid-design-system/lib/components/private/Snippets.ts
+++ packages/braid-design-system/lib/components/private/Snippets.ts
@@ -3,9 +3,9 @@ import type { Snippets as PlayroomSnippets } from 'playroom';
 import type { Optional } from 'utility-types';
 import type { Source } from '../../utils/source.macro';
 
-export interface BraidSnippet
+interface BraidSnippet
   extends Omit<Optional<PlayroomSnippets[number], 'group'>, 'code'> {
   code: Source<ReactElement>;
 }
 
-export type Snippets = Array<BraidSnippet>;
+export type Snippets = BraidSnippet[];
