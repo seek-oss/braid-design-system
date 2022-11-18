0f948cb314cbfb92f83df4bc7e7f0e8ef6b92ea1
Remus Mate <rmate@seek.com.au>
2022-11-18 15:45:55 +1100

    throw an error when importing Playroom components

--- packages/braid-design-system/lib/css/reset/resetTracker.ts
+++ packages/braid-design-system/lib/css/reset/resetTracker.ts
@@ -10,12 +10,12 @@ export const ensureResetImported = () => {
   if (!resetImported) {
     throw new Error(dedent`
       Braid components imported before reset.
-
-      Make sure to import the Braid reset module before importing any components. 
-      This ensures the CSS reset does not override the component styles. 
-    
+      ‎
+      Make sure to import the Braid reset module before importing any components.
+      This ensures the CSS reset does not override the component styles.
+      ‎
       e.g.
-
+      ‎
       import 'braid-design-system/reset'; // <-- Must be first
       import { BraidProvider, Box } from 'braid-design-system';
     `);
--- packages/braid-design-system/playroom/components.ts
+++ packages/braid-design-system/playroom/components.ts
@@ -1,1 +1,15 @@
+import dedent from 'dedent';
+
+declare const global: any;
+if (global?.__IS_PLAYROOM_ENVIRONMENT__ !== 'clearly') {
+  throw new Error(dedent`
+    Playroom components were imported instead of Braid components.
+    ‎
+    Make sure to import from the correct entry point.
+    ‎
+    -import { Component } from 'braid-design-system/playroom/components';
+    +import { Component } from 'braid-design-system';
+  `);
+}
+
 export * from '../lib/playroom/components';
