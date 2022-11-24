29f8a8690db7ec868c8a291c9f102d2e927de1f3
askoufis <askoufis@seek.com.au>
2022-11-24 14:34:17 +1100

    Delete theme treat files

--- packages/braid-design-system/lib/themes/catho/index.ts
+++ packages/braid-design-system/lib/themes/catho/index.ts
@@ -1,7 +1,5 @@
-import { theme as treatTheme } from './theme.treat';
 import vanillaTheme from './cathoTheme.css';
 
 export default {
-  ...treatTheme,
   vanillaTheme,
 };
--- packages/braid-design-system/lib/themes/occ/index.ts
+++ packages/braid-design-system/lib/themes/occ/index.ts
@@ -1,7 +1,5 @@
-import { theme as treatTheme } from './theme.treat';
 import vanillaTheme from './occTheme.css';
 
 export default {
-  ...treatTheme,
   vanillaTheme,
 };
