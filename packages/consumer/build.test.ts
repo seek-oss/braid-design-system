65d28799cf4528c6d9832a20f1a8fb2aad3fb194
Remus Mate <rmate@seek.com.au>
2023-01-10 11:00:26 +1100

    add a test for side-effects

---
+++ packages/consumer/build.test.ts
@@ -0,0 +1,42 @@
+/** @jest-environment node */
+import path from 'path';
+import glob from 'fast-glob';
+import webpack from 'webpack';
+import braidPkg from 'braid-design-system/package.json'; // eslint-disable-line no-restricted-imports
+
+const { SideEffectsFlagPlugin } = webpack.optimize;
+
+describe('build', () => {
+  const cache = new Map();
+  const checkSideEffectts = (filePath: string): boolean => {
+    // format the path the way Webpack does it internally
+    const relativePath = `./${filePath}`;
+    // https://github.com/webpack/webpack/blob/v5.72.1/lib/optimize/SideEffectsFlagPlugin.js#L89-L94
+    const hasSideEffects = SideEffectsFlagPlugin.moduleHasSideEffects(
+      relativePath,
+      braidPkg.sideEffects,
+      cache,
+    );
+    return hasSideEffects;
+  };
+
+  test('side-effects', async () => {
+    const srcFiles = await glob(
+      [
+        '**/*.{js,ts,tsx}',
+        '!**/*.{docs,gallery,playroom,screenshots,snippets,stories,test}.{ts,tsx}',
+        '!node_modules',
+      ],
+      {
+        cwd: path.dirname(require.resolve('braid-design-system/package.json')),
+      },
+    );
+
+    const filesWithSideEffects = srcFiles
+      .sort()
+      .map((filePath) => checkSideEffectts(filePath) && filePath)
+      .filter(Boolean);
+
+    expect(filesWithSideEffects).toMatchSnapshot();
+  });
+});
