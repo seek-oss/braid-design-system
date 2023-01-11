76bfeac21b11791a6ad509d4360d0ef98cb0d223
Remus Mate <rmate@seek.com.au>
2023-01-11 11:48:26 +1100

    fix storybook build

--- packages/braid-design-system/sku.config.js
+++ packages/braid-design-system/sku.config.js
@@ -3,7 +3,6 @@ const browserslist = require('../../browserslist');
 module.exports = {
   clientEntry: './src/lib/components/index.ts',
   rootResolution: false,
-  target: './dist',
   supportedBrowsers: browserslist,
   displayNamesProd: true,
   skipPackageCompatibilityCompilation: ['lodash', 'prettier'],
