45760ff141aa55d751b22f79d460851fb2493c7c
Michael Taranto <michaeltaranto@users.noreply.github.com>
2023-01-25 16:07:47 +1100

    Update packages/braid-design-system/color-mode/index.ts
    Co-authored-by: Remus Mate <rmate@seek.com.au>

--- packages/braid-design-system/color-mode/index.ts
+++ packages/braid-design-system/color-mode/index.ts
@@ -6,7 +6,7 @@ const flag = '_bdsdm';
 // 2 = OS Preference
 // TODO: COLORMODE RELEASE
 // Finalise import contract
-/** @deprecate Use `import { colorModeQueryParamCheck } from 'braid-design-system/color-mode/query-param'` instead. */
+/** @deprecated Use `import { colorModeQueryParamCheck } from 'braid-design-system/color-mode/query-param'` instead. */
 export const __experimentalDarkMode__ = [
   '<script>',
   `((l,w)=>{try{r=/[?&]${flag}=(\\d)/;[,s]=l.search.match(r)||[];w.${flag}=s;if(s){history.replaceState(null,'',l.pathname+l.search.replace(r,'').replace(/^&/,'?')+l.hash);if(s==1||(s==2&&matchMedia('(prefers-color-scheme:dark)').matches))document.documentElement.classList.add('${darkMode}')}}catch(e){}})(location,window)`,
