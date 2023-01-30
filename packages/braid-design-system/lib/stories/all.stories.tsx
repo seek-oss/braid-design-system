aae451a577621e458d130271e35f323b6ee1de48
Remus Mate <rmate@seek.com.au>
2023-01-31 10:40:01 +1100

    Disable transitions in Chromatic (#1207)

--- packages/braid-design-system/lib/stories/all.stories.tsx
+++ packages/braid-design-system/lib/stories/all.stories.tsx
@@ -138,30 +138,30 @@ Object.keys(allStories)
           <BrowserRouter>
             <BraidProvider theme={theme}>
               <ToastProvider>
-                <style type="text/css">
-                  {`
-              .noAnimation * {
-                animation-delay: -0.0001s !important;
-                animation-duration: 0s !important;
-                animation-play-state: paused !important;
-              }
-              .artboard {
-                --deepColor: ${
-                  theme.name === 'apacDark'
-                    ? `rgba(255, 255, 255, .1)`
-                    : `rgba(0, 0, 0, .05)`
-                };
-                --cubeSize: 12px;
-                background-color: ${
-                  theme.name === 'apacDark' ? `black` : `white`
-                };
-                background-image: linear-gradient(45deg, var(--deepColor) 25%, transparent 25%, transparent 75%, var(--deepColor) 75%, var(--deepColor)),
-                  linear-gradient(45deg, var(--deepColor) 25%, transparent 25%, transparent 75%, var(--deepColor) 75%, var(--deepColor));
-                background-size: calc(var(--cubeSize) * 2) calc(var(--cubeSize) * 2);
-                background-position: 0 0, var(--cubeSize) var(--cubeSize);
-              }
-              `}
-                </style>
+                <style type="text/css">{`
+                  .noAnimation * {
+                    animation-delay: -0.0001s !important;
+                    animation-duration: 0s !important;
+                    animation-play-state: paused !important;
+                    transition-delay: 0s !important;
+                    transition-duration: 0s !important;
+                  }
+                  .artboard {
+                    --deepColor: ${
+                      theme.name === 'apacDark'
+                        ? `rgba(255, 255, 255, .1)`
+                        : `rgba(0, 0, 0, .05)`
+                    };
+                    --cubeSize: 12px;
+                    background-color: ${
+                      theme.name === 'apacDark' ? `black` : `white`
+                    };
+                    background-image: linear-gradient(45deg, var(--deepColor) 25%, transparent 25%, transparent 75%, var(--deepColor) 75%, var(--deepColor)),
+                      linear-gradient(45deg, var(--deepColor) 25%, transparent 25%, transparent 75%, var(--deepColor) 75%, var(--deepColor));
+                    background-size: calc(var(--cubeSize) * 2) calc(var(--cubeSize) * 2);
+                    background-position: 0 0, var(--cubeSize) var(--cubeSize);
+                  }
+                `}</style>
                 <div className="noAnimation artboard">
                   {docs.examples.map((example, i) => (
                     <PlayroomStateProvider key={i}>
