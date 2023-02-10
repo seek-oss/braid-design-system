78143c9b417cbfbb10b0ed66a6542c92db599b83
Michael Taranto <michaeltaranto@users.noreply.github.com>
2023-02-10 15:19:24 +1100

    Remove legacy themes (#1217)

--- packages/braid-design-system/src/lib/themes/index.ts
+++ packages/braid-design-system/src/lib/themes/index.ts
@@ -1,7 +1,4 @@
 export { default as apac } from './apac';
-export { default as seekAnz } from './seekAnz';
 export { default as seekBusiness } from './seekBusiness';
-export { default as catho } from './catho';
-export { default as occ } from './occ';
 export { default as wireframe } from './wireframe';
 export { default as docs } from './docs';
