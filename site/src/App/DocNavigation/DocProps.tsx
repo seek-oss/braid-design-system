850bc0f0ba40213ca7fea2cd20ea87969fec3c58
Michael Taranto <michaeltaranto@users.noreply.github.com>
2023-02-27 08:45:17 +1100

    site: Fix source and migration links (#1233)

--- site/src/App/DocNavigation/DocProps.tsx
+++ site/src/App/DocNavigation/DocProps.tsx
@@ -181,7 +181,7 @@ export const DocProps = () => {
   }
 
   const subfolder = /^Icon/.test(docsName) ? 'icons' : undefined;
-  const componentFolder = `packages/braid-design-system/lib/components/${
+  const componentFolder = `packages/braid-design-system/src/lib/components/${
     subfolder ? `${subfolder}/` : ''
   }${docsName}`;
   const sourceUrl = `${sourceUrlPrefix}/${componentFolder}`;
