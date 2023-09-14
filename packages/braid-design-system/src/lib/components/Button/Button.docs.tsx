5fbc790c374f1ab6e077244e3d6859adf5f7b207
Michael Taranto <michael.taranto@gmail.com>
2023-09-14 16:23:39 +1000

    Format fix for suggestions

--- packages/braid-design-system/src/lib/components/Button/Button.docs.tsx
+++ packages/braid-design-system/src/lib/components/Button/Button.docs.tsx
@@ -60,8 +60,8 @@ const choosingRightButtonDoc = [
         <Text>
           To compliment this, when a <Strong>tone</Strong> is purposefully
           applied to a button, the default variant becomes{' '}
-          <Strong>solid</Strong> to maximise its impact — allowing the
-          visual prominence to be reduced as needed.
+          <Strong>solid</Strong> to maximise its impact — allowing the visual
+          prominence to be reduced as needed.
         </Text>
       </>
     ),
