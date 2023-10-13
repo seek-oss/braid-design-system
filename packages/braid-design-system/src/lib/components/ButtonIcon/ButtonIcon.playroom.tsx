3929c5a20da26e0be5b5cc0a1197f49a18f6c7bd
Michael Taranto <michael.taranto@gmail.com>
2023-10-13 15:50:09 +1100

    Playroom: Guard tone on Button & ButtonIcon

--- packages/braid-design-system/src/lib/components/ButtonIcon/ButtonIcon.playroom.tsx
+++ packages/braid-design-system/src/lib/components/ButtonIcon/ButtonIcon.playroom.tsx
@@ -4,18 +4,21 @@ import {
   type ButtonIconProps,
   ButtonIcon as BraidButtonIcon,
   buttonIconVariants,
+  buttonIconTones,
 } from './ButtonIcon';
 
 export const ButtonIcon = forwardRef<HTMLButtonElement, ButtonIconProps>(
-  ({ variant, id, ...restProps }, ref) => {
+  ({ variant, id, tone, ...restProps }, ref) => {
     const fallbackId = useFallbackId();
     const isValidVariant = variant && buttonIconVariants.indexOf(variant) > -1;
+    const isValidTone = tone && buttonIconTones.indexOf(tone) > -1;
 
     return (
       <BraidButtonIcon
         ref={ref}
         id={id ?? fallbackId}
         variant={isValidVariant ? variant : undefined}
+        tone={isValidTone ? tone : undefined}
         {...restProps}
       />
     );
