7e48dcccbb7d89ff0a8998238549b7b663a973fd
Michael Taranto <michael.taranto@gmail.com>
2023-11-22 16:05:16 +1100

    Use workspace braid

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
       files: ['*.{js,ts,tsx}'],
       excludedFiles: [
         '*.{docs,gallery,screenshots,stories}.tsx',
+        'packages/documentation-design/**/*.{ts,tsx}',
         'site/**/*.{ts,tsx}',
       ],
       rules: {
@@ -21,6 +22,17 @@ module.exports = {
               'site/**',
               '**/site/**',
             ],
+          },
+        ],
+      },
+    },
+    {
+      files: ['packages/documentation-design/**/*.{js,ts,tsx}'],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: ['braid-src/**'],
           },
         ],
       },
