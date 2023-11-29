7651671c3dcabc175dd3ff84af70919d6034e8ec
Remus Mate <rmate@seek.com.au>
2023-11-29 16:49:42 +1100

    ensure the circular dependency plugin doesn't complain about compiled Braid code

--- site/sku.config.js
+++ site/sku.config.js
@@ -29,7 +29,7 @@ module.exports = {
 
     config.plugins.push(
       new CircularDependencyPlugin({
-        exclude: /node_modules/,
+        exclude: /node_modules|braid-design-system\/dist.*/,
         failOnError: true,
       }),
     );
