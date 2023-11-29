3cc7b2bc2ab983a6df1182b88c33c6eef5bcc16c
Remus Mate <rmate@seek.com.au>
2023-11-29 17:25:47 +1100

    dummy commit to test Chromatic

--- packages/braid-design-system/src/lib/components/Tabs/Tabs.tsx
+++ packages/braid-design-system/src/lib/components/Tabs/Tabs.tsx
@@ -61,6 +61,8 @@ const getActiveTabLinePosition = (
   return { left: button.offsetLeft + paddingLeft, width };
 };
 
+// dummy comment
+
 export const Tabs = (props: TabsProps) => {
   const tabsContext = useContext(TabsContext);
   const tabsRef = useRef<HTMLElement>(null);
