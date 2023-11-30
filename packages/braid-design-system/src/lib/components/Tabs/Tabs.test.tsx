5edf4bd8214f3bca3d2f51b144102e7fafa704be
Remus Mate <rmate@seek.com.au>
2023-11-30 11:54:20 +1100

    tweak comment on `jest.mock`

--- packages/braid-design-system/src/lib/components/Tabs/Tabs.test.tsx
+++ packages/braid-design-system/src/lib/components/Tabs/Tabs.test.tsx
@@ -16,8 +16,8 @@ import {
 } from '..';
 
 // We have the test environment set to `jsdom`, but the actual code runs in Node.js.
-// So, we will get the error that "useLayoutEffect does nothing on the server".
-// To avoid the error, we need to manually switch to `useEffect`.
+// That means we will get the error that "useLayoutEffect does nothing on the server".
+// To avoid the error we need to manually switch to `useEffect`.
 jest.mock('../../hooks/useIsomorphicLayoutEffect', () => ({
   useIsomorphicLayoutEffect: jest.requireActual('react').useEffect,
 }));
