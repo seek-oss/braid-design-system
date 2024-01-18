c766eeefde07e811792fc28721c0e29460ceb9bf
Felix Habib <fhabib@seek.com.au>
2024-01-18 11:30:47 +1100

    Add WIP HeaderNavigation component

---
+++ packages/docs-ui/src/components/HeaderNavigation/HeaderNavigation.tsx
@@ -0,0 +1,44 @@
+import type React from 'react';
+import { Box, Hidden, HiddenVisually, Link, Text } from 'braid-design-system';
+import { MenuButton } from '../MenuButton/MenuButton';
+
+interface HeaderNavigationProps {
+  menuOpen: boolean;
+  menuClick: () => void;
+  logo: React.ReactNode;
+  themeToggle: React.ReactNode;
+}
+
+export const HeaderNavigation = ({
+  menuOpen,
+  menuClick,
+  logo,
+  themeToggle,
+}: HeaderNavigationProps) => (
+  // Todo - fix padding sizes
+  <Box paddingY="small" paddingX="small">
+    <Box display="flex" alignItems="center">
+      <Hidden print>
+        <Box
+          paddingRight="medium"
+          display={{
+            mobile: 'flex',
+            wide: 'none',
+          }}
+          alignItems="center"
+        >
+          <MenuButton open={menuOpen} onClick={menuClick} />
+        </Box>
+      </Hidden>
+      <Box paddingRight="medium">
+        <Text component="div" baseline={false}>
+          <Link href="/" tabIndex={menuOpen ? -1 : undefined}>
+            {logo}
+            <HiddenVisually>Braid Logo</HiddenVisually>
+          </Link>
+        </Text>
+      </Box>
+      {themeToggle}
+    </Box>
+  </Box>
+);
