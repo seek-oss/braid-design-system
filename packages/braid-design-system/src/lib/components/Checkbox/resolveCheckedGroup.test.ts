a4b097c16674c29d499f5a32416abe2458240d23
Michael Taranto <michael.taranto@gmail.com>
2024-02-22 11:08:17 +1100

    Fix type cast in test

--- packages/braid-design-system/src/lib/components/Checkbox/resolveCheckedGroup.test.ts
+++ packages/braid-design-system/src/lib/components/Checkbox/resolveCheckedGroup.test.ts
@@ -3,7 +3,10 @@ import { resolveCheckedGroup } from './resolveCheckedGroup';
 type ResolveCheckedGroup = typeof resolveCheckedGroup;
 
 describe('resolveCheckedGroup', () => {
-  const data = [
+  const data: Array<{
+    input: Parameters<ResolveCheckedGroup>[0];
+    result: ReturnType<ResolveCheckedGroup>;
+  }> = [
     { input: [], result: false },
     { input: [false], result: false },
     { input: [false, false], result: false },
@@ -11,10 +14,7 @@ describe('resolveCheckedGroup', () => {
     { input: [false, true], result: 'mixed' },
     { input: [true], result: true },
     { input: [true, true], result: true },
-  ] as Array<{
-    input: Parameters<ResolveCheckedGroup>[0];
-    result: ReturnType<ResolveCheckedGroup>;
-  }>;
+  ];
 
   test.each(data)(`$input returns \`$result\``, ({ input, result }) => {
     expect(resolveCheckedGroup(input)).toEqual(result);
