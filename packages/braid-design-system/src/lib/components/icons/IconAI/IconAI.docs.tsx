09b6f96e5ade3b4bccdd506f5dec534daeb6aa05
Michael Taranto <michael.taranto@gmail.com>
2024-04-04 15:49:32 +1100

    Fix lint

--- packages/braid-design-system/src/lib/components/icons/IconAI/IconAI.docs.tsx
+++ packages/braid-design-system/src/lib/components/icons/IconAI/IconAI.docs.tsx
@@ -6,7 +6,6 @@ import { IconAI, Heading, Stack } from '../..';
 
 const docs: ComponentDocs = {
   category: 'Icon',
-  migrationGuide: true,
   Example: () =>
     source(
       <Stack space="none" align="center">
