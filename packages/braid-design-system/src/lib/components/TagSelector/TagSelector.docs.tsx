e9c6a3142e8bddee2e106afb69299b6ef5973d29
Felix Habib <fhabib@seek.com.au>
2024-04-18 12:26:52 +1000

    Format docs page

--- packages/braid-design-system/src/lib/components/TagSelector/TagSelector.docs.tsx
+++ packages/braid-design-system/src/lib/components/TagSelector/TagSelector.docs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import source from '@braid-design-system/source.macro';
 import type { ComponentDocs } from 'site/types';
 import { type Tag, TagSelector } from './TagSelector';
@@ -7,18 +7,16 @@ import { Heading, Stack } from '../';
 const docs: ComponentDocs = {
   category: 'Content',
   Example: () => {
-    const [standardSelectedTags, setStandardSelectedTags] = React.useState<
-      Tag[]
-    >([]);
+    const [standardSelectedTags, setStandardSelectedTags] = useState<Tag[]>([]);
     const standardOptions = [
       { description: 'this', id: 'thisStandardOptions' },
       { description: 'that', id: 'thatStandardOptions' },
       { description: 'the other', id: 'theOtherStandardOptions' },
       { description: 'another one', id: 'anotherOneStandardOptions' },
     ];
-    const [standardValue, setStandardValue] = React.useState<string>('');
+    const [standardValue, setStandardValue] = useState<string>('');
 
-    const [preSelectedTags, setPreSelectedTags] = React.useState<Tag[]>([
+    const [preSelectedTags, setPreSelectedTags] = useState<Tag[]>([
       { description: 'first', id: 'firstPreSelected' },
       { description: 'second', id: 'secondPreSelected' },
       { description: 'third', id: 'thirdPreSelected' },
@@ -29,7 +27,7 @@ const docs: ComponentDocs = {
       { description: 'the other', id: 'theOtherPreOptions' },
       { description: 'another one', id: 'anotherOnePreOptions' },
     ];
-    const [preValue, setPreValue] = React.useState<string>('');
+    const [preValue, setPreValue] = useState<string>('');
 
     return source(
       <Stack space="xxlarge">
