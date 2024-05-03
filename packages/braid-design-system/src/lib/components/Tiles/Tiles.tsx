15c8b6310b66d8350ff907ebb39f74a72c34f31e
Felix Habib <fhabib@seek.com.au>
2024-05-03 14:16:18 +1000

    Get Tiles working Make minor progress Fix dividers Refactor Refactor and update testing contract

--- packages/braid-design-system/src/lib/components/Tiles/Tiles.tsx
+++ packages/braid-design-system/src/lib/components/Tiles/Tiles.tsx
@@ -1,16 +1,11 @@
-import React, { Children } from 'react';
-import flattenChildren from '../../utils/flattenChildren';
-import { Box } from '../Box/Box';
-import { type DividerProps, Divider } from '../Divider/Divider';
-import type { Space } from '../../css/atoms/atoms';
-import { negativeMargin } from '../../css/negativeMargin/negativeMargin';
-import { resolveResponsiveProp } from '../../utils/resolveResponsiveProp';
-import * as styles from './Tiles.css';
 import type { ReactNodeNoStrings } from '../private/ReactNodeNoStrings';
-import {
-  type RequiredResponsiveValue,
-  mapResponsiveValue,
-} from '../../css/atoms/sprinkles.css';
+import { Box } from '../Box/Box';
+import type { RequiredResponsiveValue } from '../../css/atoms/sprinkles.css';
+import type { Space } from '../../css/atoms/atoms';
+import { Divider, type DividerProps } from '../Divider/Divider';
+import flattenChildren from '../../utils/flattenChildren';
+import { Children } from 'react';
+import { mapResponsiveValue } from '../../css/atoms/sprinkles.css';
 import buildDataAttributes, {
   type DataAttributeMap,
 } from '../private/buildDataAttributes';
@@ -32,49 +27,33 @@ export const Tiles = ({
   ...restProps
 }: TilesProps) => (
   <Box
-    className={negativeMargin('top', space)}
+    display="grid"
+    gridTemplateColumns={columns}
+    gap={space}
     {...buildDataAttributes({ data, validateRestProps: restProps })}
   >
-    <Box
-      display="flex"
-      flexWrap="wrap"
-      className={negativeMargin('left', space)}
-    >
-      {Children.map(flattenChildren(children), (child, i) => (
-        <Box
-          minWidth={0}
-          className={resolveResponsiveProp(
-            columns,
-            styles.columnsMobile,
-            styles.columnsTablet,
-            styles.columnsDesktop,
-            styles.columnsWide,
-          )}
-        >
-          <Box
-            height="full"
-            // This needs to be a separate element to support IE11.
-            paddingTop={space}
-            paddingLeft={space}
-          >
-            {dividers && i > 0 ? (
-              <Box
-                paddingBottom={space}
-                display={mapResponsiveValue(columns, (column) =>
-                  column === 1 ? 'block' : 'none',
-                )}
-              >
-                {typeof dividers === 'string' ? (
-                  <Divider weight={dividers} />
-                ) : (
-                  <Divider />
-                )}
-              </Box>
-            ) : null}
+    {Children.map(flattenChildren(children), (child, index) => {
+      if (dividers && index > 0) {
+        return (
+          <Box>
+            <Box
+              paddingBottom={space}
+              display={mapResponsiveValue(columns, (column) =>
+                column === 1 ? 'block' : 'none',
+              )}
+            >
+              {typeof dividers === 'string' ? (
+                <Divider weight={dividers} />
+              ) : (
+                <Divider />
+              )}
+            </Box>
             {child}
           </Box>
-        </Box>
-      ))}
-    </Box>
+        );
+      }
+
+      return child;
+    })}
   </Box>
 );
