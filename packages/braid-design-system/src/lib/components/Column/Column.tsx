63eb3a37af599de13b7f9011da71f965eee2965d
Felix Habib <fhabib@seek.com.au>
2024-05-06 10:06:24 +1000

    Remove offset padding from Column component

--- packages/braid-design-system/src/lib/components/Column/Column.tsx
+++ packages/braid-design-system/src/lib/components/Column/Column.tsx
@@ -1,5 +1,4 @@
 import React, { type ReactNode, useContext } from 'react';
-import { optimizeResponsiveArray } from '../../utils/optimizeResponsiveArray';
 import { Box } from '../Box/Box';
 import { ColumnsContext } from '../Columns/ColumnsContext';
 import buildDataAttributes, {
@@ -19,17 +18,8 @@ export const Column = ({
   width,
   ...restProps
 }: ColumnProps) => {
-  const {
-    collapseMobile,
-    collapseTablet,
-    collapseDesktop,
-    mobileSpace,
-    tabletSpace,
-    desktopSpace,
-    wideSpace,
-    collapsibleAlignmentChildProps,
-    component,
-  } = useContext(ColumnsContext);
+  const { collapsibleAlignmentChildProps, component } =
+    useContext(ColumnsContext);
 
   return (
     <Box
@@ -47,22 +37,6 @@ export const Column = ({
     >
       <Box
         component={component}
-        paddingLeft={optimizeResponsiveArray([
-          collapseMobile ? 'none' : mobileSpace,
-          collapseTablet ? 'none' : tabletSpace,
-          collapseDesktop ? 'none' : desktopSpace,
-          wideSpace,
-        ])}
-        paddingTop={
-          collapseMobile || collapseTablet || collapseDesktop
-            ? optimizeResponsiveArray([
-                collapseMobile ? mobileSpace : 'none',
-                collapseTablet ? tabletSpace : 'none',
-                collapseDesktop ? desktopSpace : 'none',
-                'none',
-              ])
-            : undefined
-        }
         height="full"
         {...collapsibleAlignmentChildProps}
         className={styles.columnContent}
