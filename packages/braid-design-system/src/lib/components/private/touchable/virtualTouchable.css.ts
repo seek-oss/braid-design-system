86b5ffe75d5b2a259c6192511de0c1b73bd9c17a
Michael Taranto <michael.taranto@gmail.com>
2024-05-16 10:33:57 +1000

    Revert atoms optimisation

--- packages/braid-design-system/src/lib/components/private/touchable/virtualTouchable.css.ts
+++ packages/braid-design-system/src/lib/components/private/touchable/virtualTouchable.css.ts
@@ -2,11 +2,10 @@ import { style } from '@vanilla-extract/css';
 
 import { debugTouchable } from './debugTouchable';
 import { virtualTouchableRules } from './virtualTouchableRules';
-import { atoms } from '../../../css/atoms/atoms';
 
 export const virtualTouchable = style([
-  atoms({ position: 'relative' }),
   {
+    position: 'relative',
     '::after': {
       content: '""',
       position: 'absolute',
