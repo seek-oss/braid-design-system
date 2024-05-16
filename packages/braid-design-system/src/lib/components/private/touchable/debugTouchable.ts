88a4ab43800b92e0ccfd11d2756ca14af3136260
Michael Taranto <michael.taranto@gmail.com>
2024-05-16 15:44:12 +1000

    Stop stripping debug style

--- packages/braid-design-system/src/lib/components/private/touchable/debugTouchable.ts
+++ packages/braid-design-system/src/lib/components/private/touchable/debugTouchable.ts
@@ -2,16 +2,11 @@ import type { StyleRule } from '@vanilla-extract/css';
 
 export const debugTouchableAttrForDataProp = 'braid-debug';
 
-export const debugTouchable = ({ after = false } = {}): StyleRule =>
-  process.env.NODE_ENV === 'production'
-    ? {}
-    : {
-        selectors: {
-          [`[data-${debugTouchableAttrForDataProp}] &${
-            after ? '::after' : ''
-          }`]: {
-            background: 'red',
-            opacity: 0.2,
-          },
-        },
-      };
+export const debugTouchable = ({ after = false } = {}): StyleRule => ({
+  selectors: {
+    [`[data-${debugTouchableAttrForDataProp}] &${after ? '::after' : ''}`]: {
+      background: 'red',
+      opacity: 0.2,
+    },
+  },
+});
