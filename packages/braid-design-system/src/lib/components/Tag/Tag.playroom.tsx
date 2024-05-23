c5edd65512482573ccc0b0d79e54bc1673bc5987
Michael Taranto <michaeltaranto@users.noreply.github.com>
2024-05-23 16:09:54 +1000

    Tag: Add `small` size (#1497)

--- packages/braid-design-system/src/lib/components/Tag/Tag.playroom.tsx
+++ packages/braid-design-system/src/lib/components/Tag/Tag.playroom.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useFallbackId } from '../../playroom/utils';
-import { type TagProps, Tag as BraidTag } from './Tag';
+import { type TagProps, Tag as BraidTag, tagSizes } from './Tag';
 
-export const Tag = ({ icon, id, ...restProps }: TagProps) => {
+export const Tag = ({ icon, id, size, ...restProps }: TagProps) => {
   const fallbackId = useFallbackId();
   return (
     <BraidTag
       id={id ?? fallbackId}
       icon={typeof icon !== 'boolean' ? icon : undefined}
+      size={size && tagSizes.includes(size) ? size : undefined}
       {...restProps}
     />
   );
