854658f07681527c0a8c32e0e06a99642d71ed1a
Felix Habib <fhabib@seek.com.au>
2024-06-19 16:50:56 +1000

    Refactor

--- packages/braid-design-system/src/lib/components/Toggle/Toggle.tsx
+++ packages/braid-design-system/src/lib/components/Toggle/Toggle.tsx
@@ -77,6 +77,132 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
       (align === 'left' && appliedTogglePosition === 'trailing') ||
       (align !== 'left' && appliedTogglePosition === 'leading');
 
+    const ToggleInput = () => (
+      <Box position="relative">
+        <Box
+          component="input"
+          type="checkbox"
+          id={id}
+          checked={on}
+          onChange={handleChange(onChange)}
+          position="absolute"
+          zIndex={1}
+          cursor="pointer"
+          opacity={0}
+          className={[
+            styles.realField,
+            styles.realFieldPosition[size],
+            styles.fieldSize[size],
+          ]}
+          ref={forwardedRef}
+        />
+        <Box
+          position="relative"
+          display="flex"
+          alignItems="center"
+          flexShrink={0}
+          className={[
+            styles.slideContainer,
+            styles.slideContainerSize[size],
+            styles.fieldSize[size],
+          ]}
+        >
+          <Box
+            position="absolute"
+            width="full"
+            overflow="hidden"
+            borderRadius="full"
+            className={[
+              styles.slideTrack[size],
+              styles.slideTrackMask,
+              styles.slideTrackBgLightMode[lightness.lightMode],
+              styles.slideTrackBgDarkMode[lightness.darkMode],
+            ]}
+          >
+            <Box
+              position="absolute"
+              width="full"
+              height="full"
+              background="formAccent"
+              transition="fast"
+              className={styles.slideTrackSelected}
+            />
+          </Box>
+          <Box
+            position="absolute"
+            background="surface"
+            transition="fast"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="full"
+            className={styles.slider[size]}
+          >
+            <FieldOverlay
+              variant={on ? 'formAccent' : 'default'}
+              borderRadius="full"
+              visible
+              className={{
+                [styles.hideBorderOnDarkBackgroundInLightMode]:
+                  lightness.lightMode === 'dark',
+              }}
+            />
+            <FieldOverlay className={styles.icon}>
+              <IconTick tone="formAccent" size="fill" />
+            </FieldOverlay>
+            <FieldOverlay
+              variant="focus"
+              borderRadius="full"
+              className={styles.focusOverlay}
+            />
+            <FieldOverlay
+              variant="formAccent"
+              borderRadius="full"
+              className={!on ? styles.hoverOverlay : undefined}
+            />
+          </Box>
+        </Box>
+      </Box>
+    );
+
+    const ToggleLabel = () => (
+      <Box
+        component="label"
+        htmlFor={id}
+        // Todo - Replace paddings with flex-gap after browser policy change
+        /*
+      Apply padding by default to prevent padding disappearing
+      during partial completion of togglePosition and align props in Playroom
+      */
+        paddingLeft={
+          appliedTogglePosition === 'trailing' ? undefined : 'xsmall'
+        }
+        paddingRight={
+          appliedTogglePosition === 'leading' ? undefined : 'xsmall'
+        }
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        flexGrow={align === 'justify' ? 1 : undefined}
+        userSelect="none"
+        cursor="pointer"
+        className={[styles.label[size], virtualTouchable]}
+      >
+        <Text
+          baseline={false}
+          weight={on ? 'strong' : undefined}
+          size={size}
+          align={
+            align === 'justify' && appliedTogglePosition === 'leading'
+              ? 'right'
+              : undefined
+          }
+        >
+          {label}
+        </Text>
+      </Box>
+    );
+
     const content = (
       <Box
         position="relative"
@@ -89,126 +215,8 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
         className={styles.root}
         {...buildDataAttributes({ data, validateRestProps: restProps })}
       >
-        <Box position="relative">
-          <Box
-            component="input"
-            type="checkbox"
-            id={id}
-            checked={on}
-            onChange={handleChange(onChange)}
-            position="absolute"
-            zIndex={1}
-            cursor="pointer"
-            opacity={0}
-            className={[
-              styles.realField,
-              styles.realFieldPosition[size],
-              styles.fieldSize[size],
-            ]}
-            ref={forwardedRef}
-          />
-          <Box
-            position="relative"
-            display="flex"
-            alignItems="center"
-            flexShrink={0}
-            className={[
-              styles.slideContainer,
-              styles.slideContainerSize[size],
-              styles.fieldSize[size],
-            ]}
-          >
-            <Box
-              position="absolute"
-              width="full"
-              overflow="hidden"
-              borderRadius="full"
-              className={[
-                styles.slideTrack[size],
-                styles.slideTrackMask,
-                styles.slideTrackBgLightMode[lightness.lightMode],
-                styles.slideTrackBgDarkMode[lightness.darkMode],
-              ]}
-            >
-              <Box
-                position="absolute"
-                width="full"
-                height="full"
-                background="formAccent"
-                transition="fast"
-                className={styles.slideTrackSelected}
-              />
-            </Box>
-            <Box
-              position="absolute"
-              background="surface"
-              transition="fast"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              borderRadius="full"
-              className={styles.slider[size]}
-            >
-              <FieldOverlay
-                variant={on ? 'formAccent' : 'default'}
-                borderRadius="full"
-                visible
-                className={{
-                  [styles.hideBorderOnDarkBackgroundInLightMode]:
-                    lightness.lightMode === 'dark',
-                }}
-              />
-              <FieldOverlay className={styles.icon}>
-                <IconTick tone="formAccent" size="fill" />
-              </FieldOverlay>
-              <FieldOverlay
-                variant="focus"
-                borderRadius="full"
-                className={styles.focusOverlay}
-              />
-              <FieldOverlay
-                variant="formAccent"
-                borderRadius="full"
-                className={!on ? styles.hoverOverlay : undefined}
-              />
-            </Box>
-          </Box>
-        </Box>
-        <Box
-          component="label"
-          htmlFor={id}
-          // Todo - Replace paddings with flex-gap after browser policy change
-          /*
-          Apply padding by default to prevent padding disappearing
-          during partial completion of togglePosition and align props in Playroom
-          */
-          paddingLeft={
-            appliedTogglePosition === 'trailing' ? undefined : 'xsmall'
-          }
-          paddingRight={
-            appliedTogglePosition === 'leading' ? undefined : 'xsmall'
-          }
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          flexGrow={align === 'justify' ? 1 : undefined}
-          userSelect="none"
-          cursor="pointer"
-          className={[styles.label[size], virtualTouchable]}
-        >
-          <Text
-            baseline={false}
-            weight={on ? 'strong' : undefined}
-            size={size}
-            align={
-              align === 'justify' && appliedTogglePosition === 'leading'
-                ? 'right'
-                : undefined
-            }
-          >
-            {label}
-          </Text>
-        </Box>
+        <ToggleInput />
+        <ToggleLabel />
       </Box>
     );
 
