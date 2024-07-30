6bceddd824667128e218c43da2e2f02f563c520c
Michael Taranto <michael.taranto@gmail.com>
2024-07-30 14:42:21 +1000

    Badge: Enable usage inside typographic components

---
+++ packages/braid-design-system/src/lib/components/Badge/Badge.css.ts
@@ -0,0 +1,15 @@
+import { style } from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
+import { vars } from '../../themes/vars.css';
+
+export const verticalPadding = 'xxsmall';
+
+export const inline = style({
+  display: 'inline-flex',
+  verticalAlign: 'middle',
+  marginBottom: calc(vars.space[verticalPadding]).negate().toString(),
+  marginTop: calc(vars.space[verticalPadding])
+    .add('.25em') // subtle offset to improve alignment across all text sizes and themes
+    .negate()
+    .toString(),
+});
--- packages/braid-design-system/src/lib/components/Badge/Badge.screenshots.tsx
+++ packages/braid-design-system/src/lib/components/Badge/Badge.screenshots.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import type { ComponentScreenshot } from 'site/types';
-import { Badge, Inline, Heading, List } from '../';
+import { Badge, Inline, Heading, List, Text, Stack, Box } from '../';
+import { heading, textSizeUntrimmed } from '../../css/typography.css';
+
+const textSizes = Object.keys(textSizeUntrimmed) as Array<
+  keyof typeof textSizeUntrimmed
+>;
+const headingLevels = Object.keys(heading).sort() as Array<
+  keyof typeof heading
+>;
 
 export const screenshots: ComponentScreenshot = {
   screenshotWidths: [320],
@@ -117,6 +125,57 @@ export const screenshots: ComponentScreenshot = {
             Critical
           </Badge>
         </List>
+      ),
+    },
+    {
+      label: 'Test: Badge should not impact line height of text',
+      Example: () => (
+        <Text>
+          Incididunt consequat id nisi est dolore ipsum culpa eiusmod aliquip
+          est deserunt incididunt aliqua culpa.{' '}
+          <Badge tone="critical" weight="strong">
+            Critical
+          </Badge>{' '}
+          Incididunt consequat id nisi est dolore ipsum culpa eiusmod aliquip
+          est deserunt incididunt aliqua culpa.
+        </Text>
+      ),
+    },
+    {
+      label: 'Test: Badge should not impact line height of text',
+      Example: () => (
+        <Stack space="large">
+          {textSizes.map((size) => (
+            <Box key={size} position="relative">
+              <Box style={{ border: '1px solid red' }} />
+              <Text size={size}>
+                <Badge tone="positive" weight="strong">
+                  Badge
+                </Badge>{' '}
+                Lorem impsum{' '}
+                <Badge tone="positive" weight="strong">
+                  Badge
+                </Badge>
+              </Text>
+              <Box style={{ border: '1px solid red' }} />
+            </Box>
+          ))}
+          {headingLevels.reverse().map((level) => (
+            <Box key={level} position="relative">
+              <Box style={{ border: '1px solid red' }} />
+              <Heading level={level}>
+                <Badge tone="positive" weight="strong">
+                  Badge
+                </Badge>{' '}
+                Lorem{' '}
+                <Badge tone="positive" weight="strong">
+                  Badge
+                </Badge>
+              </Heading>
+              <Box style={{ border: '1px solid red' }} />
+            </Box>
+          ))}
+        </Stack>
       ),
     },
   ],
