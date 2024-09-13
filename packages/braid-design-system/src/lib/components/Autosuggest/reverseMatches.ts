e73f71111555979531179aa7d0cdeee235489962
Felix Habib <fhabib@seek.com.au>
2024-09-13 14:43:58 +1000

    Add comment explaining matches format

--- packages/braid-design-system/src/lib/components/Autosuggest/reverseMatches.ts
+++ packages/braid-design-system/src/lib/components/Autosuggest/reverseMatches.ts
@@ -1,3 +1,12 @@
+/*
+  In each Match,
+  - First number: index of the first highlighted character in the match
+  - Second number: index of the last highlighted character in the match *plus one*
+
+  This matches the format expected by the parse function
+  See https://github.com/moroshko/autosuggest-highlight?tab=readme-ov-file#parsetext-matches
+*/
+
 export type Match = [number, number];
 
 export function reverseMatches(suggestion: string, matches: Match[]): Match[] {
