f1fe217e7df84e0a7625e7e6647f1d3551d8ad82
Michael Taranto <michaeltaranto@users.noreply.github.com>
2024-09-19 15:34:25 +1000

    site: Add `component` and missing `reverse` docs (#1589)

--- packages/braid-design-system/src/lib/components/Column/Column.screenshots.tsx
+++ packages/braid-design-system/src/lib/components/Column/Column.screenshots.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import type { ComponentScreenshot } from 'site/types';
 import { Columns, Column, Box, Stack } from '../';
 import { Placeholder } from '../private/Placeholder/Placeholder';
@@ -49,77 +49,70 @@ export const screenshots: ComponentScreenshot = {
     {
       label: 'Gutter align',
       Example: () => (
-        <Fragment>
-          <Box marginBottom="medium">
-            <Columns space="small">
-              <Column>
-                <Placeholder height={60} label="Fluid" />
-              </Column>
-              <Column>
-                <Placeholder height={60} label="Fluid" />
-              </Column>
-            </Columns>
-          </Box>
-          <Box marginBottom="medium">
-            <Columns space="small">
-              <Column width="1/2">
-                <Placeholder height={60} label="&#189;" />
-              </Column>
-              <Column>
-                <Placeholder height={60} label="Fluid" />
-              </Column>
-            </Columns>
-          </Box>
-          <Box marginBottom="medium">
-            <Columns space="small">
-              <Column width="1/2">
-                <Placeholder height={60} label="&#189;" />
-              </Column>
-              <Column width="1/2">
-                <Placeholder height={60} label="&#189;" />
-              </Column>
-            </Columns>
-          </Box>
-          <Box marginBottom="medium">
-            <Columns space="small">
-              <Column width="1/4">
-                <Placeholder height={60} label="&#188;" />
-              </Column>
-              <Column width="1/4">
-                <Placeholder height={60} label="&#188;" />
-              </Column>
-              <Column>
-                <Placeholder height={60} label="Fluid" />
-              </Column>
-            </Columns>
-          </Box>
-          <Box marginBottom="medium">
-            <Columns space="small">
-              <Column width="1/3">
-                <Placeholder height={60} label="&#8531;" />
-              </Column>
-              <Column width="1/4">
-                <Placeholder height={60} label="&#188;" />
-              </Column>
-              <Column width="content">
-                <Placeholder height={60} label="Content" />
-              </Column>
-            </Columns>
-          </Box>
-          <Box marginBottom="medium">
-            <Columns space="small" reverse>
-              <Column width="1/3">
-                <Placeholder height={60} label="&#8531;" />
-              </Column>
-              <Column width="1/4">
-                <Placeholder height={60} label="&#188;" />
-              </Column>
-              <Column width="content">
-                <Placeholder height={60} label="Content" />
-              </Column>
-            </Columns>
-          </Box>
-        </Fragment>
+        <Stack space="medium">
+          <Columns space="small">
+            <Column>
+              <Placeholder height={60} label="Fluid" />
+            </Column>
+            <Column>
+              <Placeholder height={60} label="Fluid" />
+            </Column>
+          </Columns>
+
+          <Columns space="small">
+            <Column width="1/2">
+              <Placeholder height={60} label="&#189;" />
+            </Column>
+            <Column>
+              <Placeholder height={60} label="Fluid" />
+            </Column>
+          </Columns>
+
+          <Columns space="small">
+            <Column width="1/2">
+              <Placeholder height={60} label="&#189;" />
+            </Column>
+            <Column width="1/2">
+              <Placeholder height={60} label="&#189;" />
+            </Column>
+          </Columns>
+
+          <Columns space="small">
+            <Column width="1/4">
+              <Placeholder height={60} label="&#188;" />
+            </Column>
+            <Column width="1/4">
+              <Placeholder height={60} label="&#188;" />
+            </Column>
+            <Column>
+              <Placeholder height={60} label="Fluid" />
+            </Column>
+          </Columns>
+
+          <Columns space="small">
+            <Column width="1/3">
+              <Placeholder height={60} label="&#8531;" />
+            </Column>
+            <Column width="1/4">
+              <Placeholder height={60} label="&#188;" />
+            </Column>
+            <Column width="content">
+              <Placeholder height={60} label="Content" />
+            </Column>
+          </Columns>
+
+          <Columns space="small" reverse>
+            <Column width="1/3">
+              <Placeholder height={60} label="&#8531;" />
+            </Column>
+            <Column width="1/4">
+              <Placeholder height={60} label="&#188;" />
+            </Column>
+            <Column width="content">
+              <Placeholder height={60} label="Content" />
+            </Column>
+          </Columns>
+        </Stack>
       ),
     },
     {
