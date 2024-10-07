3cf83021080733311de0942dc76935cc6372e48a
Michael Taranto <michaeltaranto@users.noreply.github.com>
2024-10-07 16:21:50 +1100

    useColor: Align `background` tokens with `Box` and `vars` (#1616)

--- packages/braid-design-system/src/lib/components/useColor/useColor.ts
+++ packages/braid-design-system/src/lib/components/useColor/useColor.ts
@@ -1,3 +1,15 @@
 import { useBraidTheme } from '../BraidProvider/BraidThemeContext';
 
-export const useColor = () => useBraidTheme().color;
+export const useColor = () => {
+  const {
+    foreground,
+    // TODO: COLORMODE RELEASE
+    // Release new backgrounds
+    background: { surfaceDark: _, bodyDark: __, ...background },
+  } = useBraidTheme().color;
+
+  return {
+    foreground,
+    background,
+  };
+};
