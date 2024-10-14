55a0343b59663a34c3469478c78534cefdf3b14f
Felix Habib <fhabib@seek.com.au>
2024-10-14 11:48:08 +1100

    Add spacing section to docs

--- packages/braid-design-system/src/lib/components/List/List.docs.tsx
+++ packages/braid-design-system/src/lib/components/List/List.docs.tsx
@@ -155,6 +155,52 @@ const docs: ComponentDocs = {
         ),
     },
     {
+      label: 'Spacing',
+      description: (
+        <Text>
+          By default, Lists will use <Strong>small</Strong> spacing, or{' '}
+          <Strong>xsmall</Strong> spacing if the <Strong>size</Strong> prop is
+          set to either <Strong>small</Strong> or <Strong>xsmall</Strong>. This
+          can be changed via the <Strong>space</Strong> prop, which supports the
+          same the same spacing as{' '}
+          <TextLink href="/components/Stack">Stack</TextLink>.
+        </Text>
+      ),
+      Example: () =>
+        source(
+          <Columns space="large" collapseBelow="desktop">
+            <Column>
+              <List space="large">
+                <Text>Large</Text>
+                <Text>Large</Text>
+                <Text>Large</Text>
+              </List>
+            </Column>
+            <Column>
+              <List space="medium">
+                <Text>Standard</Text>
+                <Text>Standard</Text>
+                <Text>Standard</Text>
+              </List>
+            </Column>
+            <Column>
+              <List space="small">
+                <Text>Small</Text>
+                <Text>Small</Text>
+                <Text>Small</Text>
+              </List>
+            </Column>
+            <Column>
+              <List space="xsmall">
+                <Text>Xsmall</Text>
+                <Text>Xsmall</Text>
+                <Text>Xsmall</Text>
+              </List>
+            </Column>
+          </Columns>,
+        ),
+    },
+    {
       label: 'Rich content',
       description: (
         <Text>
