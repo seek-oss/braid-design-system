6e91012f2e78ac8d9b2c5adc801bb06610cefa52
Michael Taranto <michaeltaranto@users.noreply.github.com>
2024-10-21 13:45:47 +1100

    playroom: Fix Stack and Inline `component` prop (#1634)

--- packages/braid-design-system/src/lib/components/Inline/Inline.playroom.tsx
+++ packages/braid-design-system/src/lib/components/Inline/Inline.playroom.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { cleanSpaceValue } from '../../playroom/cleanSpaceValue';
 import { type InlineProps, Inline as BraidInline } from './Inline';
 
-export const Inline = ({
-  space,
-  align,
-  alignY,
-  component,
-  ...restProps
-}: InlineProps) => (
+export const Inline = ({ space, align, alignY, ...restProps }: InlineProps) => (
   <BraidInline
     space={cleanSpaceValue(space) || 'none'}
     align={typeof align !== 'boolean' ? align : undefined}
--- packages/braid-design-system/src/lib/components/Stack/Stack.playroom.tsx
+++ packages/braid-design-system/src/lib/components/Stack/Stack.playroom.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { cleanSpaceValue } from '../../playroom/cleanSpaceValue';
 import { type StackProps, Stack as BraidStack } from './Stack';
 
-export const Stack = ({
-  space,
-  align,
-  component,
-  ...restProps
-}: StackProps) => (
+export const Stack = ({ space, align, ...restProps }: StackProps) => (
   <BraidStack
     space={cleanSpaceValue(space) || 'none'}
     align={typeof align !== 'boolean' ? align : undefined}
