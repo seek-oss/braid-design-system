ea64129e535a27a70f17efc678a01033b6cc3a8c
Felix Habib <fhabib@seek.com.au>
2024-11-13 14:21:17 +1100

    WIP

---
+++ packages/braid-design-system/src/lib/components/useToast/Toaster.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const toaster = style({
+  bottom: '0',
+  left: '50%',
+  transform: 'translateX(-50%)',
+});
--- packages/braid-design-system/src/lib/components/useToast/Toaster.tsx
+++ packages/braid-design-system/src/lib/components/useToast/Toaster.tsx
@@ -1,17 +1,23 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import { Box } from '../Box/Box';
+import { ContentBlock } from '../ContentBlock/ContentBlock';
 
 import ToastComponent from './Toast';
 import type { InternalToast } from './ToastTypes';
+import { toastWidth } from './consts';
 import { useFlipList } from './useFlipList';
+
+import * as styles from './Toaster.css';
 
 interface ToasterProps {
   toasts: InternalToast[];
   removeToast: (key: string) => void;
 }
+
 export const Toaster = ({ toasts, removeToast }: ToasterProps) => {
-  const { itemRef, remove } = useFlipList();
+  const [expanded, setExpanded] = useState(false);
+  const { itemRef, remove } = useFlipList(expanded);
 
   const onClose = useCallback(
     (dedupeKey: string, toastKey: string) => {
@@ -24,23 +30,35 @@ export const Toaster = ({ toasts, removeToast }: ToasterProps) => {
 
   return (
     <Box
+      className={styles.toaster}
       position="fixed"
       zIndex="notification"
       width="full"
-      pointerEvents="none"
-      paddingX="gutter"
-      bottom={0}
     >
-      {toasts.map(({ toastKey, ...rest }) => (
-        <Box key={toastKey} paddingBottom="small">
-          <ToastComponent
-            ref={itemRef(toastKey)}
-            toastKey={toastKey}
-            onClose={onClose}
-            {...rest}
-          />
+      <ContentBlock width={toastWidth}>
+        <Box
+          pointerEvents={toasts.length === 0 ? 'none' : undefined}
+          boxShadow="borderCritical" // Todo - remove
+          marginX="gutter"
+          marginBottom="xsmall"
+          onMouseEnter={() => setExpanded(true)}
+          onMouseLeave={() => setExpanded(false)}
+          onFocus={() => setExpanded(true)}
+          onBlur={() => setExpanded(false)}
+          onClick={() => setExpanded(!expanded)}
+        >
+          {toasts.map(({ toastKey, ...rest }) => (
+            <ToastComponent
+              key={toastKey}
+              ref={itemRef(toastKey)}
+              toastKey={toastKey}
+              onClose={onClose}
+              expanded={expanded}
+              {...rest}
+            />
+          ))}
         </Box>
-      ))}
+      </ContentBlock>
     </Box>
   );
 };
