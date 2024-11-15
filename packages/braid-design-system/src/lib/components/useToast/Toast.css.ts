0bf1be2cfc5b378cf37fec519fc0aa565bd0b7fe
Felix Habib <fhabib@seek.com.au>
2024-11-15 16:22:01 +1100

    Disable animations for reduced motion

--- packages/braid-design-system/src/lib/components/useToast/Toast.css.ts
+++ packages/braid-design-system/src/lib/components/useToast/Toast.css.ts
@@ -2,4 +2,9 @@ import { style } from '@vanilla-extract/css';
 
 export const toast = style({
   pointerEvents: 'all',
+  '@media': {
+    'screen and (prefers-reduced-motion)': {
+      transition: 'none !important',
+    },
+  },
 });
