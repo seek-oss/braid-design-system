02ae1fe8691d0d6fe741e3613a7255962864feab
Felix Habib <fhabib@seek.com.au>
2024-12-06 10:27:30 +1100

    Refactor

--- packages/braid-design-system/src/lib/components/OverflowMenu/OverflowMenu.css.ts
+++ packages/braid-design-system/src/lib/components/OverflowMenu/OverflowMenu.css.ts
@@ -2,5 +2,4 @@ import { style } from '@vanilla-extract/css';
 
 export const wrapperPositioning = style({
   margin: '-1px -6px',
-  width: 'fit-content',
 });
--- packages/braid-design-system/src/lib/components/OverflowMenu/OverflowMenu.tsx
+++ packages/braid-design-system/src/lib/components/OverflowMenu/OverflowMenu.tsx
@@ -20,26 +20,28 @@ export const OverflowMenu = ({
   id,
   ...menuProps
 }: OverflowMenuProps) => (
-  <Box display="flex" justifyContent="flexEnd">
-    <Box className={styles.wrapperPositioning}>
-      <MenuRenderer
-        trigger={(triggerProps) => (
-          <ButtonIcon
-            // @ts-expect-error With no id, ButtonIcon will fallback from Tooltip to title internally.
-            // ID will no longer be required when React 18 has sufficient adoption and we can safely `useId()`
-            id={id}
-            icon={<IconOverflow />}
-            variant="transparent"
-            label={label}
-            {...triggerProps}
-          />
-        )}
-        align="right"
-        offsetSpace="small"
-        {...menuProps}
-      >
-        {children}
-      </MenuRenderer>
-    </Box>
+  <Box
+    className={styles.wrapperPositioning}
+    display="flex"
+    justifyContent="flexEnd"
+  >
+    <MenuRenderer
+      trigger={(triggerProps) => (
+        <ButtonIcon
+          // @ts-expect-error With no id, ButtonIcon will fallback from Tooltip to title internally.
+          // ID will no longer be required when React 18 has sufficient adoption and we can safely `useId()`
+          id={id}
+          icon={<IconOverflow />}
+          variant="transparent"
+          label={label}
+          {...triggerProps}
+        />
+      )}
+      align="right"
+      offsetSpace="small"
+      {...menuProps}
+    >
+      {children}
+    </MenuRenderer>
   </Box>
 );
