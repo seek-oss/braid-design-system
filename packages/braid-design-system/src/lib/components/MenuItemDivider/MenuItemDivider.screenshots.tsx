81dc545462509f98704126d430ec658a54e0aaf5
Felix Habib <fhabib@seek.com.au>
2024-12-19 14:21:02 +1100

    Update screenshot tests

--- packages/braid-design-system/src/lib/components/MenuItemDivider/MenuItemDivider.screenshots.tsx
+++ packages/braid-design-system/src/lib/components/MenuItemDivider/MenuItemDivider.screenshots.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { ComponentScreenshot } from 'site/types';
-import { Box, MenuItem, MenuItemCheckbox, MenuItemDivider } from '../';
+import { Box, Inline, MenuItem, MenuItemCheckbox, MenuItemDivider } from '../';
 import { Menu } from '../MenuRenderer/MenuRenderer';
 
 const defaultProps = {
@@ -23,55 +23,100 @@ export const screenshots: ComponentScreenshot = {
     {
       label: 'Default',
       Example: () => (
-        <Box display="flex">
-          <Menu {...defaultProps}>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItemDivider />
-            <MenuItemCheckbox checked={false} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-            <MenuItemCheckbox checked={true} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-          </Menu>
-        </Box>
+        <Inline space="medium">
+          <Box display="flex">
+            <Menu {...defaultProps}>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+          <Box display="flex">
+            <Menu {...defaultProps} size="small">
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+        </Inline>
       ),
     },
     {
       label: 'Highlighted before divider',
       Example: () => (
-        <Box display="flex">
-          <Menu {...defaultProps} highlightIndex={1}>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItemDivider />
-            <MenuItemCheckbox checked={false} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-            <MenuItemCheckbox checked={true} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-          </Menu>
-        </Box>
+        <Inline space="medium">
+          <Box display="flex">
+            <Menu {...defaultProps} highlightIndex={1}>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+          <Box display="flex">
+            <Menu {...defaultProps} size="small" highlightIndex={1}>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+        </Inline>
       ),
     },
     {
-      label: 'Highlighted before divider',
+      label: 'Highlighted after divider',
       Example: () => (
-        <Box display="flex">
-          <Menu {...defaultProps} highlightIndex={2}>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItem onClick={() => {}}>Item</MenuItem>
-            <MenuItemDivider />
-            <MenuItemCheckbox checked={false} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-            <MenuItemCheckbox checked={true} onChange={() => {}}>
-              Item
-            </MenuItemCheckbox>
-          </Menu>
-        </Box>
+        <Inline space="medium">
+          <Box display="flex">
+            <Menu {...defaultProps} highlightIndex={2}>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+          <Box display="flex">
+            <Menu {...defaultProps} size="small" highlightIndex={2}>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItem onClick={() => {}}>Item</MenuItem>
+              <MenuItemDivider />
+              <MenuItemCheckbox checked={false} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+              <MenuItemCheckbox checked={true} onChange={() => {}}>
+                Item
+              </MenuItemCheckbox>
+            </Menu>
+          </Box>
+        </Inline>
       ),
     },
   ],
