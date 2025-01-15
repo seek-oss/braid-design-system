983e56e42bcaa038b87297dd753910fdaa4bf879
Felix Habib <fhabib@seek.com.au>
2025-01-15 15:22:06 +1100

    Remove trigger as prop

--- packages/braid-design-system/src/lib/components/MenuRenderer/MenuRenderer.actions.ts
+++ packages/braid-design-system/src/lib/components/MenuRenderer/MenuRenderer.actions.ts
@@ -6,14 +6,17 @@ export const actionTypes = {
   MENU_ITEM_DOWN: 3,
   MENU_ITEM_ESCAPE: 4,
   MENU_ITEM_TAB: 5,
-  MENU_ITEM_TRIGGER_KEYBOARD: 6,
+  MENU_ITEM_ENTER: 6,
   MENU_ITEM_SPACE: 7,
-  MENU_ITEM_TRIGGER_CLICK: 8,
+  MENU_ITEM_CLICK: 8,
   MENU_ITEM_HOVER: 9,
-  MENU_TRIGGER_KEYBOARD: 10,
-  MENU_TRIGGER_CLICK: 11,
-  POPOVER_CLOSE: 12,
-  WINDOW_RESIZE: 13,
+  MENU_TRIGGER_ENTER: 10,
+  MENU_TRIGGER_SPACE: 11,
+  MENU_TRIGGER_CLICK: 12,
+  MENU_TRIGGER_TAB: 13,
+  MENU_TRIGGER_ESCAPE: 14,
+  BACKDROP_CLICK: 15,
+  WINDOW_RESIZE: 16,
 } as const;
 
 export type Action =
@@ -24,7 +27,7 @@ export type Action =
   | { type: typeof actionTypes.MENU_ITEM_ESCAPE }
   | { type: typeof actionTypes.MENU_ITEM_TAB }
   | {
-      type: typeof actionTypes.MENU_ITEM_TRIGGER_KEYBOARD;
+      type: typeof actionTypes.MENU_ITEM_ENTER;
       formElement: boolean;
       index: number;
       id?: string;
@@ -36,13 +39,16 @@ export type Action =
       id?: string;
     }
   | {
-      type: typeof actionTypes.MENU_ITEM_TRIGGER_CLICK;
+      type: typeof actionTypes.MENU_ITEM_CLICK;
       formElement: boolean;
       index: number;
       id?: string;
     }
   | { type: typeof actionTypes.MENU_ITEM_HOVER; value: number }
-  | { type: typeof actionTypes.MENU_TRIGGER_KEYBOARD }
+  | { type: typeof actionTypes.MENU_TRIGGER_ENTER }
+  | { type: typeof actionTypes.MENU_TRIGGER_SPACE }
   | { type: typeof actionTypes.MENU_TRIGGER_CLICK }
-  | { type: typeof actionTypes.POPOVER_CLOSE }
+  | { type: typeof actionTypes.MENU_TRIGGER_TAB }
+  | { type: typeof actionTypes.MENU_TRIGGER_ESCAPE }
+  | { type: typeof actionTypes.BACKDROP_CLICK }
   | { type: typeof actionTypes.WINDOW_RESIZE };
