6f7eb869d3c13e2699f298aa41a3a59527d95716
Michael Taranto <michael.taranto@gmail.com>
2025-01-29 14:23:19 +1100

    Set default playroom theme

--- site/playroom.config.js
+++ site/playroom.config.js
@@ -12,7 +12,7 @@ module.exports = {
   components: require.resolve('./src/playroom.components.ts'),
   snippets: resolveFromBraid('entries/playroom/snippets.ts'),
   themes: resolveFromBraid('lib/themes/index.ts'),
-  defaultVisibleThemes: ['apac', 'seekJobs'],
+  defaultVisibleThemes: ['gradConnection'],
   frameComponent: require.resolve('./src/playroom.frame.ts'),
   scope: require.resolve('./src/playroom.scope.ts'),
   typeScriptFiles: [resolveFromBraid('entries/playroom/components.ts')],
