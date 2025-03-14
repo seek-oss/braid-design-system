00e218211bea779d133628400cce1fa8ee928e1e
Felix Habib <fhabib@seek.com.au>
2025-03-14 17:16:28 +1100

    Fix triggerRef in TooltipRenderer

--- packages/braid-design-system/src/lib/components/TooltipRenderer/TooltipRenderer.tsx
+++ packages/braid-design-system/src/lib/components/TooltipRenderer/TooltipRenderer.tsx
@@ -103,9 +103,8 @@ export const TooltipRenderer = ({
   placement = 'top',
   children,
 }: TooltipRendererProps) => {
-  const triggerWrapperRef = useRef<HTMLElement>(null);
   const tooltipRef = useRef<HTMLDivElement>(null);
-  const triggerRef = useRef(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
   const [inferredPlacement, setInferredPlacement] =
     useState<PopoverProps['placement']>(placement);
@@ -143,8 +142,7 @@ export const TooltipRenderer = ({
 
     const setArrowPosition = () => {
       const tooltipPosition = tooltipRef.current?.getBoundingClientRect();
-      const triggerPosition =
-        triggerWrapperRef.current?.getBoundingClientRect();
+      const triggerPosition = triggerRef.current?.getBoundingClientRect();
 
       if (!tooltipPosition || !triggerPosition) {
         return;
@@ -174,7 +172,6 @@ export const TooltipRenderer = ({
   return (
     <>
       <Box
-        ref={triggerWrapperRef}
         tabIndex={-1}
         onClick={() => isMobile() && setOpen(!open)}
         onMouseEnter={() => !isMobile() && setOpen(true)}
