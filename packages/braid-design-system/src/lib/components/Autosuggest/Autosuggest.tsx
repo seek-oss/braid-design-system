ba3e3ca4d6064ca4a8ee35d6945f1e9f6d351cf3
Felix Habib <fhabib@seek.com.au>
2025-03-14 16:53:12 +1100

    Add `width` prop

--- packages/braid-design-system/src/lib/components/Autosuggest/Autosuggest.tsx
+++ packages/braid-design-system/src/lib/components/Autosuggest/Autosuggest.tsx
@@ -781,7 +781,7 @@ export const Autosuggest = forwardRef(function <Value>(
           <Popover
             triggerRef={fieldRef}
             open={isOpen}
-            align="full"
+            width="full"
             placement="bottom"
             lockPlacement
             offsetSpace="none"
