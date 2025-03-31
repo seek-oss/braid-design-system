3dc555a6f3dc6fb52ca53d1e9a424b20f8cb86df
Felix Habib <fhabib@seek.com.au>
2025-03-31 15:49:10 +1100

    Preserve portal for `Announcements`

--- packages/braid-design-system/src/lib/components/private/Announcement/Announcement.tsx
+++ packages/braid-design-system/src/lib/components/private/Announcement/Announcement.tsx
@@ -1,9 +1,11 @@
-import { useId } from 'react';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 
-import { Box } from '../../Box/Box';
+import { atoms } from '../../../css/atoms/atoms';
 
 import * as styles from '../../HiddenVisually/HiddenVisually.css';
 
+let announcementCounter = 0;
 export const containerPrefix = 'braid-announcement-container';
 
 interface AnnouncementProps {
@@ -11,18 +13,40 @@ interface AnnouncementProps {
 }
 
 export const Announcement = ({ children }: AnnouncementProps) => {
-  const announcementContainerId = `${containerPrefix}-${useId()}`;
+  const [announcementElement, setElement] = useState<HTMLElement | null>(null);
+  const className = [
+    atoms({
+      reset: 'div',
+      position: 'absolute',
+      bottom: 0,
+      overflow: 'hidden',
+    }),
+    styles.root,
+  ].join(' ');
 
-  return (
-    <Box
-      id={announcementContainerId}
-      aria-live="polite"
-      aria-atomic="true"
-      position="absolute"
-      overflow="hidden"
-      className={styles.root}
-    >
-      {children}
-    </Box>
-  );
+  useEffect(() => {
+    const announcementContainerId = `${containerPrefix}-${announcementCounter++}`;
+
+    const element = document.createElement('div');
+    element.setAttribute('id', announcementContainerId);
+    element.setAttribute('class', className);
+    element.setAttribute('aria-live', 'polite');
+    element.setAttribute('aria-atomic', 'true');
+
+    document.body.appendChild(element);
+
+    setElement(element);
+
+    return () => {
+      document.body.removeChild(element);
+    };
+  }, [className]);
+
+  if (!announcementElement) {
+    return null;
+  }
+
+  // Portal ensures screen reader announcements complete,
+  // even if the parent component unmounts
+  return createPortal(children, announcementElement);
 };
