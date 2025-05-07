f851b29328c82e677339cfa9131c95939baa97b4
Felix Habib <fhabib@seek.com.au>
2025-05-07 10:02:27 +1000

    docs: Fix `DocExample` theme in Snippets tabs

--- site/src/App/DocNavigation/DocSnippets.tsx
+++ site/src/App/DocNavigation/DocSnippets.tsx
@@ -1,15 +1,14 @@
+import docsTheme from 'braid-src/entries/themes/docs';
 import { Stack, Text, BraidProvider } from 'braid-src/lib/components';
 import { PlayroomStateProvider } from 'braid-src/lib/playroom/playroomState';
 import { useContext } from 'react';
 
 import { PageTitle } from '../Seo/PageTitle';
-import { useThemeSettings } from '../ThemeSetting';
 
 import { DocExample } from './DocExample';
 import { DocsContext } from './DocNavigation';
 
 export const DocSnippets = () => {
-  const { theme } = useThemeSettings();
   const { docsName, snippets } = useContext(DocsContext);
 
   return (
@@ -21,7 +20,7 @@ export const DocSnippets = () => {
           snippets.map(({ group, name, code }) => (
             <Stack space="medium" key={`${group}_${name}`}>
               <Text tone="secondary">{name}</Text>
-              <BraidProvider styleBody={false} theme={theme}>
+              <BraidProvider styleBody={false} theme={docsTheme}>
                 <PlayroomStateProvider>
                   <DocExample Example={() => code} showCodeByDefault={false} />
                 </PlayroomStateProvider>
