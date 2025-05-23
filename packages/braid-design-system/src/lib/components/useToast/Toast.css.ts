9fd802cf4099a674c7142416616b69ad3fc324d1
Felix Habib <fhabib@seek.com.au>
2025-05-23 10:54:15 +1000

    Remove reduce-motion check

--- packages/braid-design-system/src/lib/components/useToast/Toast.css.ts
+++ packages/braid-design-system/src/lib/components/useToast/Toast.css.ts
@@ -2,11 +2,6 @@ import { style } from '@vanilla-extract/css';
 
 export const toast = style({
   pointerEvents: 'all',
-  '@media': {
-    'screen and (prefers-reduced-motion)': {
-      transition: 'none !important',
-    },
-  },
 });
 
 export const collapsed = style({});
