9f66ceab5feb4e6a2852058d4eb39ee956396843
William Lark <wlark@seek.com.au>
2025-06-26 13:59:27 +1000

    removing unneeded comments

--- packages/braid-design-system/vitest.config.ts
+++ packages/braid-design-system/vitest.config.ts
@@ -2,9 +2,6 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineProject } from 'vitest/config';
 
-/**
- * Separating the the configs so that vitest can pick up this packages version of react rather than adding it as a dependency to the workspace root
- */
 export default defineProject({
   test: {
     globals: true,
--- packages/source.macro/vitest.config.ts
+++ packages/source.macro/vitest.config.ts
@@ -1,8 +1,5 @@
 import { defineProject } from 'vitest/config';
 
-/**
- * Separating the the configs so that vitest can pick up this packages version of react rather than adding it as a dependency to the workspace root
- */
 export default defineProject({
   test: {
     globals: true,
