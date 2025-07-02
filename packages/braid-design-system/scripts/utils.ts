085067a127a5b628f1201cba8d9120a16aa3f24c
Aden Margheriti <amargheriti@seek.com.au>
2025-07-02 14:11:21 +1000

    fix formatting on utils file

--- packages/braid-design-system/scripts/utils.ts
+++ packages/braid-design-system/scripts/utils.ts
@@ -16,9 +16,15 @@ export const debugLog = (...args: Parameters<typeof console.log>) => {
 export const relativeTo = (baseDir: string, absPath: string) => {
   let relativePath = path.relative(baseDir, require.resolve(absPath));
   relativePath = relativePath.replace(path.extname(relativePath), '');
-  if (relativePath.endsWith('/index')) relativePath = path.dirname(relativePath);
-  if (relativePath.endsWith('..')) relativePath += '/';
-  if (!relativePath.startsWith('.')) relativePath = `./${relativePath}`;
+  if (relativePath.endsWith('/index')) {
+    relativePath = path.dirname(relativePath);
+  }
+  if (relativePath.endsWith('..')) {
+    relativePath += '/';
+  }
+  if (!relativePath.startsWith('.')) {
+    relativePath = `./${relativePath}`;
+  }
 
   return relativePath;
 };
