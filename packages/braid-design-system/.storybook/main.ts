62c49c9145500d60804c4dedc12bb414f6b41eb6
William Lark <wlark@seek.com.au>
2025-07-02 18:40:58 +1000

    using csf-tools as indexer

--- packages/braid-design-system/.storybook/main.ts
+++ packages/braid-design-system/.storybook/main.ts
@@ -1,46 +1,19 @@
-/* eslint-disable no-console */
-import fs from 'fs';
-import path from 'path';
+import { readFile } from 'fs/promises';
 
+import { loadCsf } from '@storybook/csf-tools';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import { babel, webpackFinal } from 'sku/config/storybook';
+import type { Indexer } from 'storybook/internal/types';
 
-const screenshotsIndexer = {
+const screenshotsIndexer: Indexer = {
   test: /\.screenshots\.[tj]sx?$/,
-  createIndex: async (fileName: string) => {
-    try {
-      const componentName = path
-        .basename(fileName)
-        .replace(/\.screenshots\.[tj]sx?$/, '');
+  createIndex: async (fileName: string, { makeTitle }) => {
+    const code = await readFile(fileName, { encoding: 'utf-8' });
 
-      const fileContent = await fs.promises.readFile(fileName, 'utf8');
-
-      const hasDefaultExport = fileContent.includes('export default meta');
-
-      if (!hasDefaultExport) {
-        console.log(
-          `Warning: ${fileName} doesn't appear to be in CSF format yet.`,
-        );
-        return [];
-      }
-
-      const storyExportMatches = fileContent.matchAll(/export const (\w+)/g);
-      const storyExports = [...storyExportMatches].map((match) => match[1]);
-
-      if (storyExports.length === 0) {
-        console.log(`Warning: No exported stories found in ${fileName}`);
-      }
-
-      return storyExports.map((exportName) => ({
-        type: 'story' as const,
-        title: `Components/${componentName}`,
-        importPath: fileName,
-        exportName,
-      }));
-    } catch (error) {
-      console.error(`Error indexing ${fileName}:`, error);
-      return [];
-    }
+    return loadCsf(code, {
+      makeTitle,
+      fileName,
+    }).parse().indexInputs;
   },
 };
 
