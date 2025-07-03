00ca7c401c6f48093a1e551a1f413dbd2c02bb9d
Felix Habib <fhabib@seek.com.au>
2025-07-03 13:40:33 +1000

    Migrate `TextLink` to new style

--- packages/braid-design-system/src/lib/components/TextLink/TextLink.css.ts
+++ packages/braid-design-system/src/lib/components/TextLink/TextLink.css.ts
@@ -35,6 +35,8 @@ export const base = style({
   textDecoration: vars.linkDecoration,
   textDecorationThickness,
   textUnderlineOffset: 3,
+  outlineOffset: '0.2em',
+  borderRadius: vars.borderRadius.small, // Ensures focus ring is rounded
   ':hover': {
     color: textLinkVars.colorHover,
     textDecoration: 'underline',
@@ -48,12 +50,6 @@ export const base = style({
       to browser support policy of Edge 16+.
     */
     textDecorationThickness,
-  },
-  ':focus-visible': {
-    color: textLinkVars.colorHover,
-    outline: `${vars.focusRingSize} solid ${vars.borderColor.focus}`,
-    outlineOffset: '0.2em',
-    borderRadius: vars.borderRadius.small,
   },
 });
 
