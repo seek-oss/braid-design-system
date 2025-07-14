73d9eb12e7b231f0b440d47db5237650eed0d38d
Adam Skoufis <askoufis@seek.com.au>
2025-07-14 16:44:17 +1000

    Revert "Test change to `Accordion`"
    This reverts commit 17261a62c87f9df501ca444ec687492b876b0a42.

--- packages/braid-design-system/src/lib/components/Accordion/Accordion.tsx
+++ packages/braid-design-system/src/lib/components/Accordion/Accordion.tsx
@@ -99,7 +99,6 @@ export const Accordion = ({
 
   return (
     <AccordionContext.Provider value={contextValue}>
-      hello
       <Stack space={space} data={data}>
         {!dividers ? (
           children
