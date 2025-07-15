f0c9388351eb217c9bcb4019ba4886c33e4e5ce7
William Lark <wlark@seek.com.au>
2025-07-15 12:12:38 +1000

    updating link to exact commit reference

--- packages/braid-design-system/src/lib/utils/isFragment.ts
+++ packages/braid-design-system/src/lib/utils/isFragment.ts
@@ -7,7 +7,7 @@ const REACT_FRAGMENT_TYPE = Symbol.for('react.fragment');
 /**
  * A subset of the logic from `react-is` that supports both React 18 and 19.
  * We only use this to check for React fragments so only that code has been used.
- * @see https://github.com/facebook/react/blob/main/packages/react-is/src/ReactIs.js#L40
+ * @see https://github.com/facebook/react/blob/97cdd5d3c33eda77be4f96a43f72d6916d3badbb/packages/react-is/src/ReactIs.js#L40}
  *
  * _Note: once we drop support for React 18 we should move back to using `react-is`_
  */
