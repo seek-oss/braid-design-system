2dbac8da5a895e7cd03b57755736e6da4805371b
Michael Taranto <michaeltaranto@users.noreply.github.com>
2025-07-15 10:29:31 +1000

    Test with 2s delay

--- packages/braid-design-system/src/lib/components/Tabs/Tabs.screenshots.tsx
+++ packages/braid-design-system/src/lib/components/Tabs/Tabs.screenshots.tsx
@@ -20,7 +20,7 @@ const meta = {
   parameters: {
     chromatic: {
       ...setChromatic({ viewports: ['mobile', 'wide'] }),
-      // delay: 15000,
+      delay: 2000,
     },
   },
 } satisfies Meta<typeof Tabs>;
