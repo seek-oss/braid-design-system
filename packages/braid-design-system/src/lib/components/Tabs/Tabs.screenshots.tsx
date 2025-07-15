a636ab4e6f574d564ee527c67d968abbbb6d83b8
Michael Taranto <michaeltaranto@users.noreply.github.com>
2025-07-15 10:20:30 +1000

    Remove delay to test

--- packages/braid-design-system/src/lib/components/Tabs/Tabs.screenshots.tsx
+++ packages/braid-design-system/src/lib/components/Tabs/Tabs.screenshots.tsx
@@ -20,7 +20,7 @@ const meta = {
   parameters: {
     chromatic: {
       ...setChromatic({ viewports: ['mobile', 'wide'] }),
-      delay: 15000,
+      // delay: 15000,
     },
   },
 } satisfies Meta<typeof Tabs>;
