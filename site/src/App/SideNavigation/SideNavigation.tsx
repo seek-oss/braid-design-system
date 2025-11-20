0c419ef0ca6b4696ade447190e3e41df47f82e44
Cheryl Paulsen <cpaulsen@seek.com.au>
2025-11-20 16:51:50 +1100

    Wrap sidebar sections in an accordion

--- site/src/App/SideNavigation/SideNavigation.tsx
+++ site/src/App/SideNavigation/SideNavigation.tsx
@@ -1,5 +1,10 @@
 import { SideNavigationSection } from '@braid-design-system/docs-ui';
-import { Stack } from 'braid-src/lib/components';
+import {
+  Accordion,
+  AccordionItem,
+  Stack,
+  Text,
+} from 'braid-src/lib/components';
 import { useMemo } from 'react';
 import { matchPath, useLocation } from 'react-router';
 
@@ -45,97 +50,115 @@ export const SideNavigation = ({ onSelect }: SideNavigationProps) => {
   );
 
   return (
-    <Stack space="large">
-      <SideNavigationSection
-        title="Resources"
-        hideTitle={true}
-        items={[
-          {
-            name: 'Releases',
-            path: '/releases',
-            active: isActive('/releases'),
+    <Accordion size="standard">
+      <AccordionItem label="Getting started">
+        <Stack space="large">
+          <SideNavigationSection
+            title="Resources"
+            hideTitle={true}
+            items={[
+              {
+                name: 'Releases',
+                path: '/releases',
+                active: isActive('/releases'),
+                onClick: onSelect,
+              },
+              {
+                name: 'Gallery',
+                path: '/gallery',
+              },
+              {
+                name: 'Playroom',
+                path: playroomUrl,
+              },
+              {
+                name: 'GitHub',
+                path: 'https://github.com/seek-oss/braid-design-system',
+              },
+            ]}
+          />
+
+          <SideNavigationSection
+            title="Guides"
+            items={Object.entries(guides).map(([path, guide]) => ({
+              name: guide.title,
+              badge: guide.badge,
+              path,
+              active: isActive(path),
+              onClick: onSelect,
+            }))}
+          />
+
+          <SideNavigationSection
+            title="Examples"
+            items={Object.entries(examples).map(([path, example]) => ({
+              name: example.title,
+              badge: example.badge,
+              path,
+              active: isActive(path),
+              onClick: onSelect,
+            }))}
+          />
+        </Stack>
+      </AccordionItem>
+
+      <AccordionItem label="Foundations">
+        <SideNavigationSection
+          title="Foundations"
+          hideTitle={true}
+          items={Object.entries(foundations).map(([path, foundation]) => ({
+            name: foundation.title,
+            badge: foundation.badge,
+            path,
+            active: isActive(path),
             onClick: onSelect,
-          },
-          {
-            name: 'Gallery',
-            path: '/gallery',
-          },
-          {
-            name: 'Playroom',
-            path: playroomUrl,
-          },
-          {
-            name: 'GitHub',
-            path: 'https://github.com/seek-oss/braid-design-system',
-          },
-        ]}
-      />
+          }))}
+        />
+      </AccordionItem>
 
-      <SideNavigationSection
-        title="Guides"
-        items={Object.entries(guides).map(([path, guide]) => ({
-          name: guide.title,
-          badge: guide.badge,
-          path,
-          active: isActive(path),
-          onClick: onSelect,
-        }))}
-      />
+      <AccordionItem label="Components">
+        <SideNavigationSection
+          title="Components"
+          hideTitle={true}
+          items={componentsList.map((docs) => ({
+            name: docs.name,
+            badge: getBadge(docs),
+            path: `/components/${docs.name}`,
+            active: isActive(`/components/${docs.name}`),
+            onClick: onSelect,
+          }))}
+        />
+      </AccordionItem>
 
-      <SideNavigationSection
-        title="Foundations"
-        items={Object.entries(foundations).map(([path, foundation]) => ({
-          name: foundation.title,
-          badge: foundation.badge,
-          path,
-          active: isActive(path),
-          onClick: onSelect,
-        }))}
-      />
+      <AccordionItem label="Patterns">
+        <Text>Coming soon!</Text>
+      </AccordionItem>
 
-      <SideNavigationSection
-        title="Examples"
-        items={Object.entries(examples).map(([path, example]) => ({
-          name: example.title,
-          badge: example.badge,
-          path,
-          active: isActive(path),
-          onClick: onSelect,
-        }))}
-      />
+      <AccordionItem label="Development tools">
+        <Stack space="large">
+          <SideNavigationSection
+            title="CSS"
+            items={documentedCss.map((doc) => ({
+              name: doc.name,
+              badge: getBadge(doc),
+              path: `/css/${doc.name}`,
+              active: isActive(`/css/${doc.name}`),
+              onClick: onSelect,
+            }))}
+          />
 
-      <SideNavigationSection
-        title="Components"
-        items={componentsList.map((docs) => ({
-          name: docs.name,
-          badge: getBadge(docs),
-          path: `/components/${docs.name}`,
-          active: isActive(`/components/${docs.name}`),
-          onClick: onSelect,
-        }))}
-      />
-
-      <SideNavigationSection
-        title="CSS"
-        items={documentedCss.map((doc) => ({
-          name: doc.name,
-          badge: getBadge(doc),
-          path: `/css/${doc.name}`,
-          active: isActive(`/css/${doc.name}`),
-          onClick: onSelect,
-        }))}
-      />
-
-      <SideNavigationSection
-        title="Logic"
-        items={categorisedComponents.Logic.map((docs) => ({
-          name: docs.name,
-          badge: getBadge(docs),
-          path: `/components/${docs.name}`,
-          active: isActive(`/components/${docs.name}`),
-          onClick: onSelect,
-        }))}
-      />
-    </Stack>
+          <SideNavigationSection
+            title="Logic"
+            items={categorisedComponents.Logic.map((docs) => ({
+              name: docs.name,
+              badge: getBadge(docs),
+              path: `/components/${docs.name}`,
+              active: isActive(`/components/${docs.name}`),
+              onClick: onSelect,
+            }))}
+          />
+        </Stack>
+      </AccordionItem>
+    </Accordion>
   );
 };
