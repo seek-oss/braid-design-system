79b7e56245d45b7c59fa268002f3dd3cb7a4ddf6
Felix Habib <33821218+felixhabib@users.noreply.github.com>
2025-12-05 14:08:40 +1100

    Box, atoms: Remove `outline` `none`, and `boxShadow` `outlineFocus` (#1931)

--- packages/braid-design-system/src/lib/css/atoms/atoms.ts
+++ packages/braid-design-system/src/lib/css/atoms/atoms.ts
@@ -1,4 +1,3 @@
-import dedent from 'dedent';
 import type React from 'react';
 
 import { type RequiredResponsiveValue, sprinkles } from './sprinkles.css';
@@ -17,42 +16,6 @@ export interface Atoms extends Sprinkles {
 export const atoms = ({ reset, ...rest }: Atoms) => {
   if (!reset) {
     return sprinkles(rest);
-  }
-
-  if (process.env.NODE_ENV !== 'production') {
-    if (rest.outline && rest.outline === 'none') {
-      // eslint-disable-next-line no-console
-      console.warn(
-        dedent`
-          \`outline='none'\` is deprecated and will be removed in a future release.
-
-          Braid manages the focus outlines of interactive elements as part of its
-          scoped CSS reset.
-
-          For a custom focus outlines on non-interactive elements, consider
-          \`outline='focus'\` or \`outlineStyle\` for more complex scenarios.
-
-          See documentation:
-          1. https://seek-oss.github.io/braid-design-system/components/Box#focus-outlines
-          2. https://seek-oss.github.io/braid-design-system/css/outlineStyle
-        `,
-      );
-    }
-    if (rest.boxShadow && rest.boxShadow === 'outlineFocus') {
-      // eslint-disable-next-line no-console
-      console.warn(
-        dedent`
-          \`outline='none'\` is deprecated and will be removed in a future release.
-
-          Braid no longer uses the \`box-shadow\` for focus outlines.
-          Instead, consider \`outline='focus'\` or the \`outlineStyle\` css utility.
-
-          See documentation:
-          1. https://seek-oss.github.io/braid-design-system/components/Box#focus-outlines
-          2. https://seek-oss.github.io/braid-design-system/css/outlineStyle
-        `,
-      );
-    }
   }
 
   const elementReset =
