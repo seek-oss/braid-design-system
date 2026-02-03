82032406e9dea93baf79db1ce5d2ea94c441c032
Cheryl Paulsen <cpaulsen@seek.com.au>
2026-02-03 11:57:09 +1100

    Reword copy to clarify responsive behaviour

--- packages/braid-design-system/src/lib/components/Table/Table.docs.tsx
+++ packages/braid-design-system/src/lib/components/Table/Table.docs.tsx
@@ -516,9 +516,9 @@ const docs: ComponentDocs = {
           <Text>
             Tables display at full width of their container with columns
             expanding to fill available space. When a table is wider than the
-            available space (such as on mobile or tablet), it scrolls
-            horizontally to retain its structure and allow easy comparison of
-            data.
+            available space, it scrolls horizontally to retain its structure and
+            allow easy comparison of data. This behaviour also applies to mobile
+            and tablet.
           </Text>
         </>
       ),
