55eebe57692b713f82b6f865076693aea2999350
Kilian Lippa <klippa@seek.com.au>
2026-04-22 15:39:13 +1000

    lint

--- packages/docs-ui/src/utils/useCopy.ts
+++ packages/docs-ui/src/utils/useCopy.ts
@@ -1,26 +1,26 @@
 import { useEffect, useState } from 'react';
 
 export const useCopy = () => {
-    const [copying, setCopying] = useState(false);
+  const [copying, setCopying] = useState(false);
 
-    useEffect(() => {
-        if (copying) {
-            setTimeout(() => {
-                setCopying(false);
-            }, 2000);
-        }
-    }, [copying]);
+  useEffect(() => {
+    if (copying) {
+      setTimeout(() => {
+        setCopying(false);
+      }, 2000);
+    }
+  }, [copying]);
 
-    const onCopyClick = async (content: string) => {
-        if (!copying) {
-            setCopying(true);
-            const clipboardItem = new ClipboardItem({ 'text/plain': content });
-            await navigator.clipboard.write([clipboardItem]);
-        }
-    };
+  const onCopyClick = async (content: string) => {
+    if (!copying) {
+      setCopying(true);
+      const clipboardItem = new ClipboardItem({ 'text/plain': content });
+      await navigator.clipboard.write([clipboardItem]);
+    }
+  };
 
-    return {
-        copying,
-        onCopyClick,
-    };
+  return {
+    copying,
+    onCopyClick,
+  };
 };
