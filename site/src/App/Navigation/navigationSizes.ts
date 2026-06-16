effa156f2608dce973b7ac9104c557a5e8a87b67
Cheryl Paulsen <cpaulsen@seek.com.au>
2026-06-16 13:40:32 +1000

    Remove gap below HeaderNavigation

--- site/src/App/Navigation/navigationSizes.ts
+++ site/src/App/Navigation/navigationSizes.ts
@@ -1,12 +1,11 @@
 import tokens from 'braid-src/lib/themes/docs/tokens';
 
-
-// What is this magive number?
-const menuButton = 49;
+// What is this magic number?
+const menuButton = 38;
 
 export const headerSpaceY = 'small';
 const headerPaddingY = tokens.grid * tokens.space[headerSpaceY];
 export const menuButtonSize = `${menuButton}px`;
-export const headerHeight = `${menuButton + (headerPaddingY * 2)}px`;
+export const headerHeight = `${menuButton + headerPaddingY * 2}px`;
 export const menuWidth = '260px';
 export const gutterSize = 'medium';
