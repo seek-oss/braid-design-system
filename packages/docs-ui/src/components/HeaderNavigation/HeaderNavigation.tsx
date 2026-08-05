9277c72718d85b9c24414d9a962ad509ed44d3fd
Pete Ryan <pryan@seek.com.au>
2026-08-05 14:58:41 +1000

    🐛 Fix docs search shortcut hydration mismatch (#2112)

--- packages/docs-ui/src/components/HeaderNavigation/HeaderNavigation.tsx
+++ packages/docs-ui/src/components/HeaderNavigation/HeaderNavigation.tsx
@@ -7,7 +7,7 @@ import {
   Link,
   Text,
 } from 'braid-design-system';
-import type { ReactNode } from 'react';
+import { type ReactNode, useLayoutEffect, useState } from 'react';
 
 import { KeyboardShortcut } from '../KeyboardShortcut/KeyboardShortcut';
 import { MenuButton } from '../MenuButton/MenuButton';
@@ -24,6 +24,8 @@ interface HeaderNavigationProps {
   themeToggle?: ReactNode;
 }
 
+const isApplePlatform = () => /Mac|iPhone|iPod|iPad/i.test(navigator.userAgent);
+
 export const HeaderNavigation = ({
   menuOpen = false,
   menuClick = () => {},
@@ -32,53 +34,55 @@ export const HeaderNavigation = ({
   logoLabel,
   logoHref = '/',
   themeToggle = null,
-}: HeaderNavigationProps) => (
-  <Box display="flex" alignItems="center">
-    <Hidden print>
-      <Box
-        paddingRight="medium"
-        display={{
-          mobile: 'flex',
-          wide: 'none',
-        }}
-        alignItems="center"
-      >
-        <MenuButton open={menuOpen} onClick={menuClick} />
-      </Box>
-    </Hidden>
-    <Box paddingRight="medium">
-      <Text component="div" baseline={false}>
-        <Link href={logoHref} tabIndex={menuOpen ? -1 : undefined}>
-          {logo}
-          <HiddenVisually>{logoLabel}</HiddenVisually>
-        </Link>
-      </Text>
-    </Box>
-    <div>
-      <>{themeToggle}</>
-      <Bleed horizontal="xxsmall" bottom="xxsmall">
+}: HeaderNavigationProps) => {
+  const [modifierKey, setModifierKey] = useState('⌘');
+
+  useLayoutEffect(() => {
+    if (!isApplePlatform()) {
+      setModifierKey('Ctrl');
+    }
+  }, []);
+
+  return (
+    <Box display="flex" alignItems="center">
+      <Hidden print>
         <Box
-          component="button"
-          padding="xxsmall"
-          paddingRight="xsmall"
-          borderRadius="standard"
-          className={searchButton}
-          onClick={onSearchClick}
+          paddingRight="medium"
+          display={{
+            mobile: 'flex',
+            wide: 'none',
+          }}
+          alignItems="center"
         >
-          <KeyboardShortcut
-            keys={[
-              navigator.platform.startsWith('Mac') ||
-              navigator.platform === 'iPhone' ||
-              navigator.platform === 'iPad' ||
-              navigator.platform === 'iPod'
-                ? '⌘'
-                : 'Ctrl',
-              'K',
-            ]}
-            shortcutLabel={<IconSearch />}
-          />
+          <MenuButton open={menuOpen} onClick={menuClick} />
         </Box>
-      </Bleed>
-    </div>
-  </Box>
-);
+      </Hidden>
+      <Box paddingRight="medium">
+        <Text component="div" baseline={false}>
+          <Link href={logoHref} tabIndex={menuOpen ? -1 : undefined}>
+            {logo}
+            <HiddenVisually>{logoLabel}</HiddenVisually>
+          </Link>
+        </Text>
+      </Box>
+      <div>
+        <>{themeToggle}</>
+        <Bleed horizontal="xxsmall" bottom="xxsmall">
+          <Box
+            component="button"
+            padding="xxsmall"
+            paddingRight="xsmall"
+            borderRadius="standard"
+            className={searchButton}
+            onClick={onSearchClick}
+          >
+            <KeyboardShortcut
+              keys={[modifierKey, 'K']}
+              shortcutLabel={<IconSearch />}
+            />
+          </Box>
+        </Bleed>
+      </div>
+    </Box>
+  );
+};
