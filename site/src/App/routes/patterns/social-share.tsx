4d944bc56ec1d24da7b121862d7fccebe67ac57e
Cheryl Paulsen <cpaulsen@seek.com.au>
2026-08-31 16:54:28 +1000

    Apply docSections to Social Share and tidy up content

--- site/src/App/routes/patterns/social-share.tsx
+++ site/src/App/routes/patterns/social-share.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   ButtonIcon,
+  Heading,
   IconLink,
   IconShare,
   IconSocialFacebook,
@@ -26,215 +27,213 @@ export const docs: PatternDocs = {
       social networks or with specific individuals.
     </Text>
   ),
-  additional: [
+  alternatives: [
     {
-      label: 'Web',
-      description: (
-        <>
-          <List space="large">
-            <Text>
-              It&rsquo;s recommended to present social share as a single menu.
-            </Text>
-            <Text>
-              Consider ordering your items alphabetically, and placing “Copy
-              link” at the bottom.
-            </Text>
-            <Text>Include logo icons next to each menu item.</Text>
-            <Text>
-              You may choose a menu trigger to meet your specific UI needs by
-              using{' '}
-              <TextLink href="/components/MenuRenderer">MenuRenderer</TextLink>.
-              Suggested triggers include Button or ButtonIcon, and you can
-              specify a variant and/or tone to meet your needs.
-            </Text>
-            <Text>
-              The menu can be aligned to the left or the right of the trigger.
-            </Text>
-            <Text>
-              When the user makes a selection from the menu, consider opening
-              the share link in a new tab and closing the menu.
-            </Text>
-            <Text>
-              When the user selects “Copy link”, consider closing the menu and
-              providing a positive{' '}
-              <TextLink href="/components/useToast">Toast</TextLink> that alerts
-              the user that the link has been copied successfully.
-            </Text>
-          </List>
-          <Text>
-            A right-aligned example triggered by a transparent Button:
-          </Text>
-        </>
-      ),
-      showCodeByDefault: true,
-      Example: ({ showToast }) =>
-        source(
-          <Box padding="small">
-            <Inline space="none" align="right">
-              <MenuRenderer
-                offsetSpace="small"
-                align="right"
-                width="small"
-                trigger={(triggerProps) => (
-                  <Button
-                    variant="transparent"
-                    icon={<IconShare />}
-                    {...triggerProps}
-                  >
-                    Share job
-                  </Button>
-                )}
-              >
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialFacebook />}
-                >
-                  Facebook
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialInstagram />}
-                >
-                  Instagram
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialLinkedIn />}
-                >
-                  LinkedIn
-                </MenuItemLink>
-                <MenuItemLink href="#" target="_blank" icon={<IconSocialX />}>
-                  Twitter
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  icon={<IconLink />}
-                  onClick={() =>
-                    showToast({
-                      message: 'Link copied',
-                      tone: 'positive',
-                      key: '1',
-                    })
-                  }
-                >
-                  Copy link
-                </MenuItemLink>
-              </MenuRenderer>
-            </Inline>
-          </Box>,
-        ),
+      name: 'MenuItem',
+      description: 'For displaying buttons and links within a menu',
     },
     {
-      description: (
-        <Text>A left-aligned example triggered by a ButtonIcon:</Text>
-      ),
-      showCodeByDefault: true,
-      Example: ({ showToast }) =>
-        source(
-          <Box padding="medium">
-            <Inline space="none">
-              <MenuRenderer
-                offsetSpace="small"
-                width="small"
-                trigger={(triggerProps) => (
-                  <ButtonIcon
-                    variant="transparent"
-                    icon={<IconShare />}
-                    label="Share job"
-                    {...triggerProps}
-                  />
-                )}
-              >
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialFacebook />}
-                >
-                  Facebook
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialInstagram />}
-                >
-                  Instagram
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  target="_blank"
-                  icon={<IconSocialLinkedIn />}
-                >
-                  LinkedIn
-                </MenuItemLink>
-                <MenuItemLink href="#" target="_blank" icon={<IconSocialX />}>
-                  Twitter
-                </MenuItemLink>
-                <MenuItemLink
-                  href="#"
-                  icon={<IconLink />}
-                  onClick={() =>
-                    showToast({
-                      message: 'Link copied',
-                      tone: 'positive',
-                      key: '1',
-                    })
-                  }
-                >
-                  Copy link
-                </MenuItemLink>
-              </MenuRenderer>
-            </Inline>
-          </Box>,
-        ),
+      name: 'MenuRenderer',
+      description: 'For custom menu components',
     },
     {
-      label: 'Mobile and apps',
-      description: (
-        <List space="large">
-          <Text>
-            For basic share options, it&rsquo;s recommended to utilise the
-            native OS share sheet.
-          </Text>
-          <Text>
-            If you require additional options not included in the native OS
-            share sheet, you may want to create a custom sheet. If you create a
-            custom sheet, consider including an option to open the native sheet
-            from within the custom menu.
-          </Text>
-          <Text>
-            Similarly to web, you may choose an appropriate trigger to meet your
-            needs, however the relevant OS share icon may be most appropriate.
-          </Text>
-        </List>
-      ),
+      name: 'Button',
+      description: 'For a semantic button',
     },
     {
-      label: 'Relevant components',
-      description: (
-        <List space="large">
-          <Text>
-            <TextLink href="/components/MenuItem">MenuItem</TextLink> — For
-            displaying buttons and links within a menu
-          </Text>
-          <Text>
-            <TextLink href="/components/MenuRenderer">MenuRenderer</TextLink> —
-            For custom menu components
-          </Text>
-          <Text>
-            <TextLink href="/components/Button">Button</TextLink> — For a
-            semantic button
-          </Text>
-          <Text>
-            <TextLink href="/components/ButtonIcon">ButtonIcon</TextLink> — For
-            buttons containing only an icon
-          </Text>
-        </List>
-      ),
+      name: 'ButtonIcon',
+      description: 'For buttons containing only an icon',
     },
   ],
+  docSections: {
+    appearance: [
+      {
+        label: 'Web',
+        Example: ({ showToast }) =>
+          source(
+            <Box padding="small">
+              <Inline space="none" align="right">
+                <MenuRenderer
+                  offsetSpace="small"
+                  align="right"
+                  width="small"
+                  trigger={(triggerProps) => (
+                    <Button
+                      variant="transparent"
+                      icon={<IconShare />}
+                      {...triggerProps}
+                    >
+                      Share job
+                    </Button>
+                  )}
+                >
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialFacebook />}
+                  >
+                    Facebook
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialInstagram />}
+                  >
+                    Instagram
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialLinkedIn />}
+                  >
+                    LinkedIn
+                  </MenuItemLink>
+                  <MenuItemLink href="#" target="_blank" icon={<IconSocialX />}>
+                    Twitter
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    icon={<IconLink />}
+                    onClick={() =>
+                      showToast({
+                        message: 'Link copied',
+                        tone: 'positive',
+                        key: '1',
+                      })
+                    }
+                  >
+                    Copy link
+                  </MenuItemLink>
+                </MenuRenderer>
+              </Inline>
+            </Box>,
+          ),
+      },
+      {
+        description: (
+          <>
+            <Heading level="4">Visual guidelines</Heading>
+            <List space="large">
+              <Text>
+                It&rsquo;s recommended to present social share as a single menu.
+              </Text>
+              <Text>
+                Consider ordering your items alphabetically, and placing “Copy
+                link” at the bottom.
+              </Text>
+              <Text>Include logo icons next to each menu item.</Text>
+              <Text>
+                You may choose a menu trigger to meet your specific UI needs by
+                using{' '}
+                <TextLink href="/components/MenuRenderer">
+                  MenuRenderer
+                </TextLink>
+                . Suggested triggers include Button or ButtonIcon, and you can
+                specify a variant and/or tone to meet your needs.
+              </Text>
+              <Text>
+                The menu can be aligned to the left or the right of the trigger.
+              </Text>
+              <Text>
+                When the user makes a selection from the menu, consider opening
+                the share link in a new tab and closing the menu.
+              </Text>
+              <Text>
+                When the user selects “Copy link”, consider closing the menu and
+                providing a positive{' '}
+                <TextLink href="/components/useToast">Toast</TextLink> that
+                alerts the user that the link has been copied successfully.
+              </Text>
+            </List>
+          </>
+        ),
+      },
+      {
+        label: 'Mobile and apps',
+        Example: ({ showToast }) =>
+          source(
+            <Box padding="medium">
+              <Inline space="none">
+                <MenuRenderer
+                  offsetSpace="small"
+                  width="small"
+                  trigger={(triggerProps) => (
+                    <ButtonIcon
+                      variant="transparent"
+                      icon={<IconShare />}
+                      label="Share job"
+                      {...triggerProps}
+                    />
+                  )}
+                >
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialFacebook />}
+                  >
+                    Facebook
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialInstagram />}
+                  >
+                    Instagram
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    target="_blank"
+                    icon={<IconSocialLinkedIn />}
+                  >
+                    LinkedIn
+                  </MenuItemLink>
+                  <MenuItemLink href="#" target="_blank" icon={<IconSocialX />}>
+                    Twitter
+                  </MenuItemLink>
+                  <MenuItemLink
+                    href="#"
+                    icon={<IconLink />}
+                    onClick={() =>
+                      showToast({
+                        message: 'Link copied',
+                        tone: 'positive',
+                        key: '1',
+                      })
+                    }
+                  >
+                    Copy link
+                  </MenuItemLink>
+                </MenuRenderer>
+              </Inline>
+            </Box>,
+          ),
+      },
+      {
+        description: (
+          <>
+            <Heading level="4">Visual guidelines</Heading>
+            <List space="large">
+              <Text>
+                For basic share options, it&rsquo;s recommended to utilise the
+                native OS share sheet.
+              </Text>
+              <Text>
+                If you require additional options not included in the native OS
+                share sheet, you may want to create a custom sheet. If you
+                create a custom sheet, consider including an option to open the
+                native sheet from within the custom menu.
+              </Text>
+              <Text>
+                Similarly to web, you may choose an appropriate trigger to meet
+                your needs, however the relevant OS share icon may be most
+                appropriate.
+              </Text>
+            </List>
+          </>
+        ),
+      },
+    ],
+  },
 };
 
 export default docs;
