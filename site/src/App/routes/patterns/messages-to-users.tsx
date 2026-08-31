cecb7b094d3af7dbee566c5be5c5c7fae68062b7
Cheryl Paulsen <cpaulsen@seek.com.au>
2026-08-31 15:08:30 +1000

    Structure content with tables

--- site/src/App/routes/patterns/messages-to-users.tsx
+++ site/src/App/routes/patterns/messages-to-users.tsx
@@ -1,4 +1,14 @@
-import { List, Stack, Strong, Text, TextLink } from 'braid-design-system';
+import {
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  Text,
+  TextLink,
+} from 'braid-design-system';
 
 import type { PatternDocs } from '../../../types';
 
@@ -13,84 +23,146 @@ export const docs: PatternDocs = {
     {
       label: 'Messaging components',
       description: (
-        <>
+        <Stack space="large">
           <Text>
             Braid includes a variety of messaging components, each with their
             own purpose and intended use.
           </Text>
-          <Stack space="large">
-            <Stack space="small">
-              <Text>
-                <Strong>
-                  <TextLink href="/components/Alert">Alert</TextLink> and{' '}
-                  <TextLink href="/components/Notice">Notice</TextLink>
-                </Strong>
-              </Text>
-              <Text>
-                Inform the user of an important status or condition relevant to
-                their current task. They sit within the context of the page,
-                close to the issue to which they relate.
-              </Text>
-            </Stack>
-            <Stack space="small">
-              <Text>
-                <Strong>
-                  <TextLink href="/components/useToast">useToast</TextLink>
-                </Strong>
-              </Text>
-              <Text>
-                Briefly acknowledges a user action without interrupting their
-                flow. Toasts float at the bottom of the screen and disappear
-                after a few seconds.
-              </Text>
-            </Stack>
-            <Stack space="small">
-              <Text>
-                <Strong>
-                  <TextLink href="/components/FieldMessage">
-                    FieldMessage
-                  </TextLink>
-                </Strong>
-              </Text>
-              <Text>
-                For displaying messages below a custom input field that
-                isn&rsquo;t provided by Braid.
-              </Text>
-            </Stack>
-          </Stack>
-        </>
+          <Table label="Messaging components" alignY="top">
+            <TableHeader>
+              <TableRow>
+                <TableHeaderCell wrap width="30%" minWidth={150}>
+                  <Text size="small">Component</Text>
+                </TableHeaderCell>
+                <TableHeaderCell wrap width="70%" minWidth={200}>
+                  <Text size="small">Purpose and use</Text>
+                </TableHeaderCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell wrap width="30%" minWidth={150}>
+                  <Text size="small">
+                    <TextLink href="/components/Alert">Alert</TextLink> and{' '}
+                    <TextLink href="/components/Notice">Notice</TextLink>
+                  </Text>
+                </TableCell>
+                <TableCell wrap width="70%" minWidth={200}>
+                  <Text size="small">
+                    Inform the user of an important status or condition relevant
+                    to their current task. They sit within the context of the
+                    page, close to the issue to which they relate.
+                  </Text>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell wrap width="30%" minWidth={150}>
+                  <Text size="small">
+                    <TextLink href="/components/useToast">useToast</TextLink>
+                  </Text>
+                </TableCell>
+                <TableCell wrap width="70%" minWidth={200}>
+                  <Text size="small">
+                    Briefly acknowledges a user action without interrupting
+                    their flow. Toasts float at the bottom of the screen and
+                    disappear after a few seconds.
+                  </Text>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell wrap width="30%" minWidth={150}>
+                  <Text size="small">
+                    <TextLink href="/components/FieldMessage">
+                      FieldMessage
+                    </TextLink>
+                  </Text>
+                </TableCell>
+                <TableCell wrap width="70%" minWidth={200}>
+                  <Text size="small">
+                    For displaying messages below a custom input field that
+                    isn&rsquo;t provided by Braid.
+                  </Text>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Stack>
       ),
     },
     {
       label: 'Messaging patterns',
       description: (
-        <List space="large">
-          <Text>
-            <TextLink href="/patterns/empty-state">Empty state</TextLink> —
-            Occurs when there is no data available at the present time, and
-            informs the user about what they might see when there is data,
-            and/or what they should do next.
-          </Text>
-          <Text>
-            <TextLink href="/patterns/error-state">Error state</TextLink> —
-            Occurs when the website or app fails to complete an expected action,
-            and informs the user that a problem has occurred.
-          </Text>
-          <Text>
-            <TextLink href="/patterns/nudge">Nudge</TextLink> — A prominent
-            message that encourages the user to take a specific action on
-            something relevant at that moment. A nudge is always actionable and
-            drives desired behaviour.
-          </Text>
-          <Text>
-            <TextLink href="/patterns/service-outage-banner">
-              Service outage banner
-            </TextLink>{' '}
-            — Inform users of upcoming interruptions to SEEK products and
-            services. This can include outages to the entire system or to
-            specific products.
-          </Text>
-        </List>
+        <Table label="Messaging patterns" alignY="top">
+          <TableHeader>
+            <TableRow>
+              <TableHeaderCell wrap width="30%" minWidth={150}>
+                <Text size="small">Pattern</Text>
+              </TableHeaderCell>
+              <TableHeaderCell wrap width="70%" minWidth={200}>
+                <Text size="small">Purpose and use</Text>
+              </TableHeaderCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell wrap width="30%" minWidth={150}>
+                <Text size="small">
+                  <TextLink href="/patterns/empty-state">Empty state</TextLink>
+                </Text>
+              </TableCell>
+              <TableCell wrap width="70%" minWidth={200}>
+                <Text size="small">
+                  Occurs when there is no data available at the present time,
+                  and informs the user about what they might see when there is
+                  data, and/or what they should do next.
+                </Text>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell wrap width="30%" minWidth={150}>
+                <Text size="small">
+                  <TextLink href="/patterns/error-state">Error state</TextLink>
+                </Text>
+              </TableCell>
+              <TableCell wrap width="70%" minWidth={200}>
+                <Text size="small">
+                  Occurs when the website or app fails to complete an expected
+                  action, and informs the user that a problem has occurred.
+                </Text>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell wrap width="30%" minWidth={150}>
+                <Text size="small">
+                  <TextLink href="/patterns/nudge">Nudge</TextLink>
+                </Text>
+              </TableCell>
+              <TableCell wrap width="70%" minWidth={200}>
+                <Text size="small">
+                  A prominent message that encourages the user to take a
+                  specific action on something relevant at that moment. A nudge
+                  is always actionable and drives desired behaviour.
+                </Text>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell wrap width="30%" minWidth={150}>
+                <Text size="small">
+                  <TextLink href="/patterns/service-outage-banner">
+                    Service outage banner
+                  </TextLink>
+                </Text>
+              </TableCell>
+              <TableCell wrap width="70%" minWidth={200}>
+                <Text size="small">
+                  Inform users of upcoming interruptions to SEEK products and
+                  services. This can include outages to the entire system or to
+                  specific products.
+                </Text>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       ),
     },
   ],
